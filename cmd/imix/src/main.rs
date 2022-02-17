e589c81412d1bc39e12f4ea6aee5fb78d6822c05
Nicholas OBrien <ndo9903@rit.edu>
2022-02-17 05:47:00 +0000

    added a ton fo logic for the initial call back, also errors!

--- cmd/imix/src/main.rs
+++ cmd/imix/src/main.rs
@@ -5,30 +5,45 @@ use std::fs::File;
 use std::path::Path;
 
 
-fn install(file_path: String) -> std::io::Result<()> {
-    println!("Installing with {} config...", file_path);
-    let config_file = File::open(file_path)?;
+async fn install(config_path: String) -> Result<(), imix::Error> {
+    let config_file = File::open(config_path)?;
     let config: imix::Config = serde_json::from_reader(config_file)?;
 
-    if cfg!(windows) {
-        return imix::windows::install(config);
-    }
-
-    if Path::new(imix::linux::SYSTEMD_DIR).is_dir() {
-        return imix::linux::install(config);
-    }
-
-    unimplemented!("The current OS/Service Manager is not supported")
+    #[cfg(target_os = "windows")]
+	    return imix::windows::install(config).await;
+	
+    #[cfg(target_os = "linux")]
+        if Path::new(imix::linux::SYSTEMD_DIR).is_dir() {
+            return imix::linux::install(config).await;
+        }
+	
+	unimplemented!("The current OS/Service Manager is not supported")
 }
 
-fn run() -> std::io::Result<()> {
-    println!("Running...");
+async fn run(config_path: String) -> Result<(), imix::Error> {
+    let config_file = File::open(config_path)?;
+    let config: imix::Config = serde_json::from_reader(config_file)?;
 
-    Ok(())
+    #[cfg(target_os = "windows")]
+        return imix::windows::run(config).await;
+
+    #[cfg(target_os = "linux")]
+        if Path::new(imix::linux::SYSTEMD_DIR).is_dir() {
+            return imix::linux::run(config).await;
+        }
+
+    unimplemented!("The current OS/Manager is not supported")
 }
 
-fn main() -> std::io::Result<()> {
+#[tokio::main]
+async fn main() -> Result<(), imix::Error> {
     let matches = App::new("imix")
+        .arg(
+            arg!(
+                -c --config <FILE> "Sets a custom config file"
+            )
+            .required(false)
+        )
         .subcommand(
             App::new("install")
                 .about("Run in install mode")
@@ -41,11 +56,15 @@ fn main() -> std::io::Result<()> {
         )
         .get_matches();
     
+    if let Some(config_path) = matches.value_of("config") {
+        return run(String::from(config_path)).await
+    }
+
     match matches.subcommand() {
         Some(("install", args)) => {
-            let file_path_str = args.value_of("config").unwrap();
-            install(String::from(file_path_str))
+            let config_path = args.value_of("config").unwrap();
+            install(String::from(config_path)).await
         },
-        _ => run(),
+        _ => Ok(())
     }
 }
--- cmd/imix/src/windows.rs
+++ cmd/imix/src/windows.rs
@@ -1,4 +1,8 @@
 
-pub fn install(_config: super::Config)-> std::io::Result<()> {
+pub async fn install(_config: super::Config)-> Result<(), super::Error> {
+    unimplemented!("Windows is currently not supported")
+}
+
+pub async fn run(_config: super::Config)-> Result<(), super::Error> {
     unimplemented!("Windows is currently not supported")
 }
