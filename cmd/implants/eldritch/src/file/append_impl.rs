84e6ffb6b9cbe1f1c5001ffb1456de9e39dde28a
Hulto <hulto@hul.to>
2022-03-01 03:17:17 +0000

    Added test existing.

--- cmd/implants/eldritch/src/file/append_impl.rs
+++ cmd/implants/eldritch/src/file/append_impl.rs
@@ -3,18 +3,14 @@ use std::fs::OpenOptions;
 use std::io::prelude::*;
 
 
-pub fn append(_path: String, _content: String) -> Result<()> {
+pub fn append(path: String, content: String) -> Result<()> {
     let mut file = OpenOptions::new()
-        .create_new(true)
-        .write(true)    //Do we want to create the file if it doesn't exist?
+        .create(true) //Do we want to create the file if it doesn't exist? - Yes!
+        .write(true)    
         .append(true)
-        .open(_path)
-        .unwrap();
+        .open(path)?;
 
-    if let Err(e) = writeln!(file, "{}", _content) {
-        eprintln!("Couldn't write to file: {}", e);
-        return Err(e)?
-    }
+    writeln!(file, "{}", content)?;
     Ok(())
 }
 
@@ -27,11 +23,11 @@ mod tests {
     use std::fs::remove_file;
 
     #[test]
-    fn test_append_existing() {
+    fn test_append_nonexisting() -> anyhow::Result<()> {
         //Remove the test file
-        let _rmfileres = remove_file(String::from("/tmp/win"));
+        let _ = remove_file(String::from("/tmp/win"));
         // Run  our code
-        let _res = append(String::from("/tmp/win"), String::from("Hi2!"));
+        append(String::from("/tmp/win"), String::from("Hi2!"))?;
         // Read the file
         let file = BufReader::new(File::open("/tmp/win").unwrap());
         // Reverse the file lines
@@ -48,6 +44,40 @@ mod tests {
             // Last line so just break.
             break;
         }
-        let _rmfileres = remove_file(String::from("/tmp/win"));
+        remove_file(String::from("/tmp/win"))?;
+        Ok(())
+    }    
+    #[test]
+    fn test_append_existing() -> anyhow::Result<()> {
+        //Remove the test file
+        let remove_res = remove_file(String::from("/tmp/win"));
+        match remove_res {
+            Ok(_res) => {
+                let mut file = File::create("/tmp/win")?;
+                file.write_all(b"Hello, world!\n")?;
+            },
+            Err(_) => {}
+        }
+
+        // Run  our code
+        append(String::from("/tmp/win"), String::from("Hi2!"))?;
+        // Read the file
+        let file = BufReader::new(File::open("/tmp/win").unwrap());
+        // Reverse the file lines
+        let mut lines: Vec<_> = file.lines().map(|line| { line.unwrap() }).collect();
+        lines.reverse();
+
+        //Make sure not empty
+        assert_eq!((lines.len() > 0), true);
+        
+        // Make sure the last line equals == Hi2!
+        for line in lines.iter() {
+            println!("{}", line);
+            assert_eq!(line, "Hi2!");
+            // Last line so just break.
+            break;
+        }
+        remove_file(String::from("/tmp/win"))?;
+        Ok(())
     }
 }
