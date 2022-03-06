8db5aaf1ba1edb8dc061c4dd7b899d9bd9837c73
Hulto <jack.m.mckenna@gmail.com>
2022-03-05 21:44:26 -0500

    Eldritch file hash function. (#8)
    * Added functions, docs, and testing.
    
    * Update cmd/implants/eldritch/src/file/hash_impl.rs
    
    Co-authored-by: Nicholas O'Brien <ndo9903@rit.edu>
    
    Co-authored-by: KCarretto <Kcarretto@gmail.com>
    Co-authored-by: Nicholas O'Brien <ndo9903@rit.edu>

--- cmd/implants/eldritch/src/file/hash_impl.rs
+++ cmd/implants/eldritch/src/file/hash_impl.rs
@@ -1,5 +1,33 @@
 use anyhow::Result;
+use sha256::digest_file;
 
-pub fn hash(_path: String) -> Result<String> {
-    unimplemented!("Method unimplemented")
+pub fn hash(path: String) -> Result<String> {
+    let val = digest_file(path)?;
+    Ok(val)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs::File;
+    use std::io::prelude::*;
+    use std::fs::remove_file;
+
+    #[test]
+    fn test_hash() -> anyhow::Result<()>{
+        let winfile = "/tmp/win_test_exists_does";
+        let _ = remove_file(String::from(winfile));
+        // Create file
+        let mut file = File::create(winfile)?;
+        // Write to file
+        file.write_all(b"aoeu")?;
+
+        // Run our code
+        let res = hash(String::from(winfile))?;
+
+        assert_eq!(res, "bc4c24181ed3ce6666444deeb95e1f61940bffee70dd13972beb331f5d111e9b");
+
+        remove_file(String::from(winfile))?;
+        Ok(())
+    }
 }
