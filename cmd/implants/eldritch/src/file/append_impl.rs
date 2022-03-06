d78d76b8ee8bdea1d37b2d3e3728356cf7c22fc2
Nicholas O'Brien <ndo9903@rit.edu>
2022-03-05 21:49:42 -0500

    Append fix (#10)
    * Added functions, docs, and testing.
    
    * fixed some of the append test code
    
    Co-authored-by: Hulto <hulto@hul.to>

--- cmd/implants/eldritch/src/file/append_impl.rs
+++ cmd/implants/eldritch/src/file/append_impl.rs
@@ -24,55 +24,48 @@ mod tests {
 
     #[test]
     fn test_append_nonexisting() -> anyhow::Result<()> {
-        //Remove the test file
+        // In Case Cleanup failed
         let _ = remove_file(String::from("/tmp/win"));
+
         // Run  our code
         append(String::from("/tmp/win"), String::from("Hi2!"))?;
-        // Read the file
-        let file = BufReader::new(File::open("/tmp/win").unwrap());
-        // Reverse the file lines
-        let mut lines: Vec<_> = file.lines().map(|line| { line.unwrap() }).collect();
-        lines.reverse();
 
-        //Make sure not empty
-        assert_eq!((lines.len() > 0), true);
-        
+        // Read the file
+        let file = BufReader::new(File::open("/tmp/win")?);
+
+        // Get Last Line
+        let last_line = file.lines().last().unwrap()?;
+
         // Make sure the last line equals == Hi2!
-        for line in lines.iter() {
-            println!("{}", line);
-            assert_eq!(line, "Hi2!");
-            // Last line so just break.
-            break;
-        }
+        assert_eq!(last_line, "Hi2!");
+
+        // Cleanup
         remove_file(String::from("/tmp/win"))?;
         Ok(())
     }    
     #[test]
     fn test_append_existing() -> anyhow::Result<()> {
-        //Remove the test file
-        let _remove_res = remove_file(String::from("/tmp/win"));
-        let mut file = File::create("/tmp/win").unwrap();
+        // In Case Cleanup failed
+        let _ = remove_file(String::from("/tmp/win"));
+
+        // Make New File
+        let mut file = File::create("/tmp/win")?;
         file.write_all(b"Hello, world!\n")?;
 
         // Run  our code
         append(String::from("/tmp/win"), String::from("Hi2!"))?;
-        // Read the file
-        let file = BufReader::new(File::open("/tmp/win").unwrap());
-        // Reverse the file lines
-        let mut lines: Vec<_> = file.lines().map(|line| { line.unwrap() }).collect();
-        lines.reverse();
 
-        //Make sure not empty
-        assert_eq!((lines.len() > 0), true);
-        
+        // Read the file
+        let file = BufReader::new(File::open("/tmp/win")?);
+
+        // Get Last Line
+        let last_line = file.lines().last().unwrap()?;
+
         // Make sure the last line equals == Hi2!
-        for line in lines.iter() {
-            println!("{}", line);
-            assert_eq!(line, "Hi2!");
-            // Last line so just break.
-            break;
-        }
-        remove_file(String::from("/tmp/win"))?;
+        assert_eq!(last_line, "Hi2!");
+
+        // Cleanup
+        let _ = remove_file(String::from("/tmp/win"));
         Ok(())
     }
 }
