6a7ae585493ec1fd262cd943e67412bcba59838b
kcarretto <kcarretto@gmail.com>
2022-03-07 04:08:56 +0000

    Refactored project

---
+++ tavern/graphql/mutation.implant_service_config_test.go
@@ -0,0 +1,272 @@
+package graphql_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/99designs/gqlgen/client"
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/kcarretto/realm/tavern/ent/enttest"
+	"github.com/kcarretto/realm/tavern/ent/implantserviceconfig"
+	"github.com/kcarretto/realm/tavern/graphql"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// TestCreateImplantServiceConfig ensures the createImplantServiceConfig mutation exhibits expected behavior.
+func TestCreateImplantServiceConfig(t *testing.T) {
+	// Initialize Test Context
+	ctx := context.Background()
+
+	// Initialize DB Backend
+	graph := enttest.Open(t, "sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+	defer graph.Close()
+
+	// Initialize sample data
+	existingConfig := graph.ImplantServiceConfig.Create().
+		SetName("meow-svc").
+		SetExecutablePath("/bin/meow-svc").
+		SaveX(ctx)
+
+	// Create a new GraphQL client
+	gqlClient := client.New(handler.NewDefaultServer(graphql.NewSchema(graph)))
+
+	// Run tests
+	t.Run("WithDefaults", newCreateImplantServiceConfigTest(
+		gqlClient,
+		graphql.CreateImplantServiceConfigInput{
+			Name:           "test-service",
+			ExecutablePath: "/bin/test-service",
+		},
+		func(t *testing.T, id int, err error) {
+			require.NoError(t, err)
+			assert.NotZero(t, id)
+
+			cfg := graph.ImplantServiceConfig.GetX(ctx, id)
+			assert.Equal(t, "test-service", cfg.Name)
+			assert.NotEmpty(t, cfg.Description)
+			assert.Equal(t, "/bin/test-service", cfg.ExecutablePath)
+		},
+	))
+	var (
+		expectedName           string = "new-svc"
+		expectedDescription           = "Some Info Here"
+		expectedExecutablePath        = `C:\new-svc`
+	)
+	t.Run("WithValues", newCreateImplantServiceConfigTest(
+		gqlClient,
+		graphql.CreateImplantServiceConfigInput{
+			Name:           expectedName,
+			Description:    &expectedDescription,
+			ExecutablePath: expectedExecutablePath,
+		},
+		func(t *testing.T, id int, err error) {
+			require.NoError(t, err)
+			assert.NotZero(t, id)
+
+			cfg := graph.ImplantServiceConfig.GetX(ctx, id)
+			assert.Equal(t, expectedName, cfg.Name)
+			assert.Equal(t, expectedDescription, cfg.Description)
+			assert.Equal(t, expectedExecutablePath, cfg.ExecutablePath)
+		},
+	))
+	t.Run("Duplicate", newCreateImplantServiceConfigTest(
+		gqlClient,
+		graphql.CreateImplantServiceConfigInput{
+			Name:           existingConfig.Name,
+			Description:    &expectedDescription,
+			ExecutablePath: expectedExecutablePath,
+		},
+		func(t *testing.T, id int, err error) {
+			assert.Zero(t, id)
+			assert.ErrorContains(t, err, "ent: constraint failed: UNIQUE constraint failed: implant_service_configs.name")
+		},
+	))
+}
+
+func newCreateImplantServiceConfigTest(gqlClient *client.Client, input graphql.CreateImplantServiceConfigInput, checks ...func(t *testing.T, id int, err error)) func(t *testing.T) {
+	return func(t *testing.T) {
+		// Define the mutatation for testing, taking the input as a variable
+		mut := `mutation CreateImplantServiceConfig($input: CreateImplantServiceConfigInput!) { createImplantServiceConfig(config:$input) { id } }`
+
+		// Make our request to the GraphQL API
+		var resp struct {
+			CreateImplantServiceConfig struct{ ID string }
+		}
+		err := gqlClient.Post(mut, &resp, client.Var("input", input))
+
+		// Run checks with error (if any) and resulting ImplantServiceConfig ID
+		for _, check := range checks {
+			check(t, convertID(resp.CreateImplantServiceConfig.ID), err)
+		}
+	}
+}
+
+// TestUpdateImplantServiceConfig ensures the updateImplantServiceConfig mutation exhibits expected behavior.
+func TestUpdateImplantServiceConfig(t *testing.T) {
+	// Initialize Test Context
+	ctx := context.Background()
+
+	// Initialize DB Backend
+	graph := enttest.Open(t, "sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+	defer graph.Close()
+
+	// Initialize sample data
+	existingConfig := graph.ImplantServiceConfig.Create().
+		SetName("meow-svc").
+		SetExecutablePath("/bin/meow-svc").
+		SaveX(ctx)
+	existingConfig2 := graph.ImplantServiceConfig.Create().
+		SetName("other-svc").
+		SetExecutablePath("/bin/other-svc").
+		SaveX(ctx)
+
+	// Create a new GraphQL client
+	gqlClient := client.New(handler.NewDefaultServer(graphql.NewSchema(graph)))
+
+	// Run tests
+	t.Run("NoUpdate", newUpdateImplantServiceConfigTest(
+		gqlClient,
+		graphql.UpdateImplantServiceConfigInput{
+			ID: existingConfig.ID,
+		},
+		func(t *testing.T, id int, err error) {
+			require.NoError(t, err)
+			assert.NotZero(t, id)
+			assert.Equal(t, existingConfig.ID, id)
+
+			cfg := graph.ImplantServiceConfig.GetX(ctx, id)
+			assert.Equal(t, existingConfig.Name, cfg.Name)
+			assert.Equal(t, existingConfig.Description, cfg.Description)
+			assert.Equal(t, existingConfig.ExecutablePath, cfg.ExecutablePath)
+		},
+	))
+	var (
+		expectedName           string = "meow-svc"
+		expectedDescription           = "Some Info Here"
+		expectedExecutablePath        = `C:\meow-svc`
+	)
+	t.Run("UpdateFields", newUpdateImplantServiceConfigTest(
+		gqlClient,
+		graphql.UpdateImplantServiceConfigInput{
+			ID:             existingConfig.ID,
+			Name:           &expectedName,
+			Description:    &expectedDescription,
+			ExecutablePath: &expectedExecutablePath,
+		},
+		func(t *testing.T, id int, err error) {
+			require.NoError(t, err)
+			assert.NotZero(t, id)
+			assert.Equal(t, existingConfig.ID, id)
+
+			cfg := graph.ImplantServiceConfig.GetX(ctx, id)
+			assert.Equal(t, expectedName, cfg.Name)
+			assert.Equal(t, expectedDescription, cfg.Description)
+			assert.Equal(t, expectedExecutablePath, cfg.ExecutablePath)
+		},
+	))
+	t.Run("NotExists", newUpdateImplantServiceConfigTest(
+		gqlClient,
+		graphql.UpdateImplantServiceConfigInput{
+			ID: -1,
+		},
+		func(t *testing.T, id int, err error) {
+			assert.Zero(t, id)
+			assert.ErrorContains(t, err, "ent: implant_service_config not found")
+		},
+	))
+	t.Run("Duplicate", newUpdateImplantServiceConfigTest(
+		gqlClient,
+		graphql.UpdateImplantServiceConfigInput{
+			ID:             existingConfig.ID,
+			Name:           &existingConfig2.Name,
+			Description:    &expectedDescription,
+			ExecutablePath: &expectedExecutablePath,
+		},
+		func(t *testing.T, id int, err error) {
+			assert.Zero(t, id)
+			assert.ErrorContains(t, err, "ent: constraint failed: UNIQUE constraint failed: implant_service_configs.name")
+		},
+	))
+}
+
+func newUpdateImplantServiceConfigTest(gqlClient *client.Client, input graphql.UpdateImplantServiceConfigInput, checks ...func(t *testing.T, id int, err error)) func(t *testing.T) {
+	return func(t *testing.T) {
+		// Define the mutatation for testing, taking the input as a variable
+		mut := `mutation UpdateImplantServiceConfig($input: UpdateImplantServiceConfigInput!) { updateImplantServiceConfig(config:$input) { id } }`
+
+		// Make our request to the GraphQL API
+		var resp struct {
+			UpdateImplantServiceConfig struct{ ID string }
+		}
+		err := gqlClient.Post(mut, &resp, client.Var("input", input))
+
+		// Run checks with error (if any) and resulting ImplantServiceConfig ID
+		for _, check := range checks {
+			check(t, convertID(resp.UpdateImplantServiceConfig.ID), err)
+		}
+	}
+}
+
+// TestDeleteImplantServiceConfig ensures the deleteImplantServiceConfig mutation exhibits expected behavior.
+func TestDeleteImplantServiceConfig(t *testing.T) {
+	// Initialize Test Context
+	ctx := context.Background()
+
+	// Initialize DB Backend
+	graph := enttest.Open(t, "sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+	defer graph.Close()
+
+	// Initialize sample data
+	existingConfig := graph.ImplantServiceConfig.Create().
+		SetName("meow-svc").
+		SetExecutablePath("/bin/meow-svc").
+		SaveX(ctx)
+
+	// Create a new GraphQL client
+	gqlClient := client.New(handler.NewDefaultServer(graphql.NewSchema(graph)))
+
+	// Run tests
+	t.Run("Existing", newDeleteImplantServiceConfigTest(
+		gqlClient,
+		existingConfig.ID,
+		func(t *testing.T, id int, err error) {
+			require.NoError(t, err)
+			assert.NotZero(t, id)
+			assert.Equal(t, existingConfig.ID, id)
+
+			exists := graph.ImplantServiceConfig.Query().
+				Where(implantserviceconfig.ID(id)).
+				ExistX(ctx)
+			assert.False(t, exists)
+		},
+	))
+	t.Run("NotExists", newDeleteImplantServiceConfigTest(
+		gqlClient,
+		-1,
+		func(t *testing.T, id int, err error) {
+			assert.Zero(t, id)
+			assert.ErrorContains(t, err, "ent: implant_service_config not found")
+		},
+	))
+}
+
+func newDeleteImplantServiceConfigTest(gqlClient *client.Client, id int, checks ...func(t *testing.T, id int, err error)) func(t *testing.T) {
+	return func(t *testing.T) {
+		// Define the mutatation for testing, taking the input as a variable
+		mut := `mutation DeleteImplantServiceConfig($input: ID!) { deleteImplantServiceConfig(id:$input) }`
+
+		// Make our request to the GraphQL API
+		var resp struct {
+			DeleteImplantServiceConfig string
+		}
+		err := gqlClient.Post(mut, &resp, client.Var("input", id))
+
+		// Run checks with error (if any) and resulting ImplantServiceConfig ID
+		for _, check := range checks {
+			check(t, convertID(resp.DeleteImplantServiceConfig), err)
+		}
+	}
+}
---
+++ tavern/internal/cdn/download_test.go
@@ -0,0 +1,108 @@
+package cdn_test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/kcarretto/realm/tavern/ent"
+	"github.com/kcarretto/realm/tavern/ent/enttest"
+	"github.com/kcarretto/realm/tavern/internal/cdn"
+	"github.com/kcarretto/realm/tavern/internal/errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/sha3"
+)
+
+// TestUpload asserts that the download handler exhibits expected behavior.
+func TestDownload(t *testing.T) {
+	graph := enttest.Open(t, "sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+	defer graph.Close()
+
+	expectedContent := []byte("file_content")
+	existingFile := newFile(graph, "ExistingTestFile", expectedContent)
+
+	t.Run("File", newDownloadTest(
+		graph,
+		newDownloadRequest(t, existingFile.Name),
+		func(t *testing.T, fileContent []byte, err *errors.HTTP) {
+			assert.Nil(t, err)
+			assert.Equal(t, string(expectedContent), string(fileContent))
+		},
+	))
+	t.Run("CachedFile", newDownloadTest(
+		graph,
+		newDownloadRequest(t, existingFile.Name, withIfNoneMatchHeader(existingFile.Hash)),
+		func(t *testing.T, fileContent []byte, err *errors.HTTP) {
+			require.NotNil(t, err)
+			assert.Equal(t, http.StatusNotModified, err.StatusCode)
+			assert.ErrorContains(t, err, cdn.ErrFileNotModified.Error())
+			assert.Empty(t, string(fileContent))
+		},
+	))
+	t.Run("NonExistentFile", newDownloadTest(
+		graph,
+		newDownloadRequest(t, "ThisFileDoesNotExist"),
+		func(t *testing.T, fileContent []byte, err *errors.HTTP) {
+			require.NotNil(t, err)
+			assert.Equal(t, http.StatusNotFound, err.StatusCode)
+			assert.ErrorContains(t, err, cdn.ErrFileNotFound.Error())
+			assert.Empty(t, string(fileContent))
+		},
+	))
+}
+
+// newDownloadTest initializes a new test case for the download handler.
+func newDownloadTest(graph *ent.Client, req *http.Request, checks ...func(t *testing.T, fileContent []byte, err *errors.HTTP)) func(*testing.T) {
+	return func(t *testing.T) {
+		// Initialize Download Handler
+		handler := cdn.NewDownloadHandler(graph)
+
+		// Send request and record response
+		w := httptest.NewRecorder()
+		handler.ServeHTTP(w, req)
+
+		// Attempt to read the response body
+		result := w.Result()
+		body, err := ioutil.ReadAll(result.Body)
+		require.NoError(t, err, "failed to parse response body")
+		defer result.Body.Close()
+
+		// If successful, ensure Etag was properly set and run checks on the file
+		if result.StatusCode == http.StatusOK {
+			// Ensure the ETag was properly set by the server on successful requests
+			hash := fmt.Sprintf("%x", sha3.Sum256(body))
+			assert.Equal(t, hash, result.Header.Get(cdn.HeaderEtag))
+
+			// Run Checks
+			for _, check := range checks {
+				check(t, body, nil)
+			}
+			return
+		}
+
+		// Parse Error from failed response and run checks
+		httpErr := errors.NewHTTP(string(body), result.StatusCode)
+		for _, check := range checks {
+			check(t, nil, &httpErr)
+		}
+	}
+}
+
+// newDownloadRequest is a helper to create an http.Request for a file download
+func newDownloadRequest(t *testing.T, fileName string, options ...func(*http.Request)) *http.Request {
+	req := httptest.NewRequest(http.MethodGet, "/download/"+fileName, nil)
+	for _, opt := range options {
+		opt(req)
+	}
+	return req
+}
+
+// withIfNoneMatchHeader is an option to set the If-None-Match header on an http.Request
+func withIfNoneMatchHeader(hash string) func(*http.Request) {
+	return func(req *http.Request) {
+		req.Header.Set(cdn.HeaderIfNoneMatch, hash)
+	}
+}
