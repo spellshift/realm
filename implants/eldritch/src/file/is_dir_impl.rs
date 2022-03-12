c3fc49aad4e996aa85a1bcc9dd1546e94cfb1b1d
Hulto <hulto@hul.to>
2022-03-12 05:21:25 +0000

    Updated with tempdir and tempfile.

--- implants/eldritch/src/file/is_dir_impl.rs
+++ implants/eldritch/src/file/is_dir_impl.rs
@@ -9,46 +9,44 @@ pub fn is_dir(path: String) -> Result<bool> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::fs::File;
     use std::io::prelude::*;
-    use std::fs::remove_file;
+    use tempfile::{NamedTempFile,tempdir};
 
     #[test]
     fn test_is_dir_true() -> anyhow::Result<()>{
-        let winfile = "/tmp/";
-
+        let tmp_dir = tempdir()?;
+        let path = String::from(tmp_dir.path().to_str().unwrap()).clone();
         // Run our code
-        let res = is_dir(String::from(winfile))?;
+
+        let res = is_dir(path)?;
 
         assert_eq!(res, true);
         Ok(())
     }
     #[test]
     fn test_is_dir_file() -> anyhow::Result<()>{
-        let winfile = "/tmp/win_test_is_dir_file";
-        let _ = remove_file(String::from(winfile));
-        // Create file
-        let mut file = File::create(winfile)?;
-        // Write to file
-        file.write_all(b"aoeu")?;
+        let mut tmp_file = NamedTempFile::new()?;
+        let path = String::from(tmp_file.path().to_str().unwrap());
+
+        // Write to New File
+        tmp_file.write_all(b"Hello, world!\n")?;
 
         // Run our code
-        let res = is_dir(String::from(winfile))?;
+        let res = is_dir(path)?;
 
         assert_eq!(res, false);
-
-        remove_file(String::from(winfile))?;
         Ok(())
     }
     // Make sure non-existent error is thrown
     #[test]
     fn test_is_dir_nonexistent() -> anyhow::Result<()>{
-        let winfile = "/tmp/win_test_is_dir_nonexistent";
-        let _ = remove_file(String::from(winfile));
+        let tmp_dir = tempdir()?;
+        let path = String::from(tmp_dir.path().join("win_test_is_dir_nonexistent").to_str().unwrap()).clone();
+        // Run our code
 
-        // Run our code. Don't bubble up errors since we know it will error.
-        let res = is_dir(String::from(winfile));
-        assert_eq!(res.unwrap(), false);
+        let res = is_dir(path)?;
+
+        assert_eq!(res, false);
         Ok(())
     }
 }
