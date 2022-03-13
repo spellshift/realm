8a40a3f76f994d698f54268589cee82c1879590b
Hulto <hulto@hul.to>
2022-03-13 19:02:42 -0400

    is_dir added impl, test, and docs. (#9)
    * is_dir added impl, test, and docs.
    
    * Clarifying is_dir documentation
    
    Correcting assumptions about is_dir behavior regarding errors. Should now reflect that is_dir coerces errors into false.
    
    * Updated new docs file.
    
    * Updated with tempdir and tempfile.

--- implants/eldritch/src/file/is_dir_impl.rs
+++ implants/eldritch/src/file/is_dir_impl.rs
@@ -1,5 +1,52 @@
 use anyhow::Result;
+use std::path::Path;
 
-pub fn is_dir(_path: String) -> Result<bool> {
-    unimplemented!("Method unimplemented")
+pub fn is_dir(path: String) -> Result<bool> {
+    let res = Path::new(&path);
+    return Ok(res.is_dir())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::prelude::*;
+    use tempfile::{NamedTempFile,tempdir};
+
+    #[test]
+    fn test_is_dir_true() -> anyhow::Result<()>{
+        let tmp_dir = tempdir()?;
+        let path = String::from(tmp_dir.path().to_str().unwrap()).clone();
+        // Run our code
+
+        let res = is_dir(path)?;
+
+        assert_eq!(res, true);
+        Ok(())
+    }
+    #[test]
+    fn test_is_dir_file() -> anyhow::Result<()>{
+        let mut tmp_file = NamedTempFile::new()?;
+        let path = String::from(tmp_file.path().to_str().unwrap());
+
+        // Write to New File
+        tmp_file.write_all(b"Hello, world!\n")?;
+
+        // Run our code
+        let res = is_dir(path)?;
+
+        assert_eq!(res, false);
+        Ok(())
+    }
+    // Make sure non-existent error is thrown
+    #[test]
+    fn test_is_dir_nonexistent() -> anyhow::Result<()>{
+        let tmp_dir = tempdir()?;
+        let path = String::from(tmp_dir.path().join("win_test_is_dir_nonexistent").to_str().unwrap()).clone();
+        // Run our code
+
+        let res = is_dir(path)?;
+
+        assert_eq!(res, false);
+        Ok(())
+    }
 }
