716f8e118e802fc6ca2a8c564d6448db201677ce
Hulto <hulto@hul.to>
2022-03-30 03:39:01 +0000

    Added additional fields and testing.

--- implants/eldritch/src/process/list_impl.rs
+++ implants/eldritch/src/process/list_impl.rs
@@ -8,26 +8,29 @@ pub struct ProcessRes {
     username:   String,
     path:       String,
     command:    String,
+    cwd:        String,
+    environ:    String,
+    name:       String,
 }
 
 impl ToString for ProcessRes {
     #[inline]
     fn to_string(&self) -> String {
-        return format!("{{pid:{},ppid:{},status:\"{}\",username:\"{}\",path:\"{}\",command:\"{}\"}}",
+        return format!("{{pid:{},ppid:{},status:\"{}\",username:\"{}\",path:\"{}\",\
+        command:\"{}\",cwd:\"{}\",environ:\"{}\",name:\"{}\"}}",
         &self.pid,
         &self.ppid,
         &self.status,
         &self.username,
         &self.path,
         &self.command,
+        &self.cwd,
+        &self.environ,
+        &self.name,
     );
     }
 }
 
-
-// Returns PID, PPID, status, user, cmd, command
-// REVIEW BLOCKER: Do we want to add environ? start_time? run_time? {cpu,memory,disk}_usage?
-// https://docs.rs/sysinfo/0.23.5/sysinfo/struct.Process.html
 pub fn list() -> Result<Vec<String>> {
     if !System::IS_SUPPORTED {
         return Err(anyhow::anyhow!("This OS isn't supported for process functions.
@@ -40,49 +43,62 @@ pub fn list() -> Result<Vec<String>> {
     sys.refresh_processes();
     sys.refresh_users_list();
     let user_list =  sys.users().clone();
-    // for user in user_list {
-    //     println!("{} is username {}", user.name(), *user.username());
-    // }    
 
-    println!("Here");
     for (pid, process) in sys.processes() {
         let mut tmp_ppid = 0;
         if  process.parent() != None {
             tmp_ppid = process.parent().unwrap().as_u32();
         }
 
-        let tmprow = ProcessRes{ 
+        let tmprow = ProcessRes{
             pid:        pid.as_u32(),
             ppid:       tmp_ppid,
             status:     process.status().to_string(),
-            username:   username_to_username(process.uid, user_list),
+            username:   uid_to_username(process.uid, user_list),
             path:       String::from(process.exe().to_str().unwrap()),
             command:    String::from(process.cmd().join(" ")),
+            cwd:        String::from(process.cwd().to_str().unwrap()),
+            environ:    String::from(process.environ().join(" ")),
+            name:       String::from(process.name())
         };
         res.push(tmprow.to_string());
     }
     Ok(res)
 }
 
-fn username_to_username(username: u32, user_list: &[User]) -> String {
+fn uid_to_username(username: u32, user_list: &[User]) -> String {
     for user in user_list {
         if *user.uid() == username {
             return String::from(user.name());
         }
     }
-    return String::from("");
+    return String::from("?");
 }
 
-// REVIEW BLOCKER: Not totally sure the right way to test this.
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::process::Command;
+
     #[test]
     fn test_process_list() -> anyhow::Result<()>{
+        let child = Command::new("sleep")
+            .arg("5")
+            .spawn()?;
+        
+        println!("{:?}", child.id());
         let res = list()?;
+        let searchstring = String::from(format!("pid:{}", child.id()));
         for proc in res{
-            println!("{:?}", proc.to_string());
+            if proc.as_str().contains(&searchstring) {
+                println!("{:?}", proc.to_string());
+                if proc.as_str().contains("command:\\\"sleep 5\\\"") {
+                    assert_eq!(true, true);
+                }
+                return Ok(())
+            }
         }
-        Ok(())
+        assert_eq!(true, false);
+        return Ok(())
     }
 }
