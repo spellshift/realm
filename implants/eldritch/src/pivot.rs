9505079dffec4f996d47ba17ad32ba957121bef2
Hulto <hulto@hul.to>
2022-04-03 01:18:36 +0000

    Starting to add pivot lib

--- implants/eldritch/src/pivot.rs
+++ implants/eldritch/src/pivot.rs
@@ -8,12 +8,12 @@ use starlark::values::none::NoneType;
 use starlark::{starlark_type, starlark_simple_value, starlark_module};
 
 #[derive(Copy, Clone, Debug, PartialEq, Display)]
-#[display(fmt = "FileLibrary")]
-pub struct FileLibrary();
-starlark_simple_value!(FileLibrary);
+#[display(fmt = "PivotLibrary")]
+pub struct PivotLibrary();
+starlark_simple_value!(PivotLibrary);
 
-impl<'v> StarlarkValue<'v> for FileLibrary {
-    starlark_type!("file_library");
+impl<'v> StarlarkValue<'v> for PivotLibrary {
+    starlark_type!("pivot_library");
 
     fn get_methods(&self) -> Option<&'static Methods> {
         static RES: MethodsStatic = MethodsStatic::new();
@@ -21,68 +21,68 @@ impl<'v> StarlarkValue<'v> for FileLibrary {
     }
 }
 
-impl<'v> UnpackValue<'v> for FileLibrary {
+impl<'v> UnpackValue<'v> for PivotLibrary {
     fn expected() -> String {
-        FileLibrary::get_type_value_static().as_str().to_owned()
+        PivotLibrary::get_type_value_static().as_str().to_owned()
     }
 
     fn unpack_value(value: Value<'v>) -> Option<Self> {
-        Some(*value.downcast_ref::<FileLibrary>().unwrap())
+        Some(*value.downcast_ref::<PivotLibrary>().unwrap())
     }
 }
 
 // This is where all of the "file.X" impl methods are bound
 #[starlark_module]
 fn methods(builder: &mut MethodsBuilder) {
-    fn append(_this: FileLibrary, path: String, content: String) -> NoneType {
+    fn revshell(_this: PivotLibrary, path: String, content: String) -> NoneType {
         append_impl::append(path, content)?;
         Ok(NoneType{})
     }
-    fn copy(_this: FileLibrary, src: String, dst: String) -> NoneType {
+    fn copy(_this: PivotLibrary, src: String, dst: String) -> NoneType {
         copy_impl::copy(src, dst)?;
         Ok(NoneType{})
     }
-    fn download(_this: FileLibrary, uri: String, dst: String) -> NoneType {
+    fn download(_this: PivotLibrary, uri: String, dst: String) -> NoneType {
         download_impl::download(uri, dst)?;
         Ok(NoneType{})
     }
-    fn exists(_this: FileLibrary, path: String) -> bool {
+    fn exists(_this: PivotLibrary, path: String) -> bool {
         exists_impl::exists(path)
     }
-    fn hash(_this: FileLibrary, path: String) -> String {
+    fn hash(_this: PivotLibrary, path: String) -> String {
         hash_impl::hash(path)
     }
-    fn is_dir(_this: FileLibrary, path: String) -> bool {
+    fn is_dir(_this: PivotLibrary, path: String) -> bool {
         is_dir_impl::is_dir(path)
     }
-    fn mkdir(_this: FileLibrary, path: String) -> NoneType {
+    fn mkdir(_this: PivotLibrary, path: String) -> NoneType {
         mkdir_impl::mkdir(path)?;
         Ok(NoneType{})
     }
-    fn read(_this: FileLibrary, path: String) -> String {
+    fn read(_this: PivotLibrary, path: String) -> String {
         read_impl::read(path)
     }
-    fn remove(_this: FileLibrary, path: String) -> NoneType {
+    fn remove(_this: PivotLibrary, path: String) -> NoneType {
         remove_impl::remove(path)?;
         Ok(NoneType{})
     }
-    fn rename(_this: FileLibrary, old: String, new: String) -> NoneType {
+    fn rename(_this: PivotLibrary, old: String, new: String) -> NoneType {
         rename_impl::rename(old, new)?;
         Ok(NoneType{})
     }
-    fn replace_all(_this: FileLibrary, path: String, pattern: String, value: String) -> NoneType {
+    fn replace_all(_this: PivotLibrary, path: String, pattern: String, value: String) -> NoneType {
         replace_all_impl::replace_all(path, pattern, value)?;
         Ok(NoneType{})
     }
-    fn replace(_this: FileLibrary, path: String, pattern: String, value: String) -> NoneType {
+    fn replace(_this: PivotLibrary, path: String, pattern: String, value: String) -> NoneType {
         replace_impl::replace(path, pattern, value)?;
         Ok(NoneType{})
     }
-    fn timestomp(_this: FileLibrary, src: String, dst: String) -> NoneType {
+    fn timestomp(_this: PivotLibrary, src: String, dst: String) -> NoneType {
         timestomp_impl::timestomp(src, dst)?;
         Ok(NoneType{})
     }
-    fn write(_this: FileLibrary, path: String, content: String) -> NoneType {
+    fn write(_this: PivotLibrary, path: String, content: String) -> NoneType {
         write_impl::write(path, content)?;
         Ok(NoneType{})
     }
