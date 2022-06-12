8df5e61cb5858808ebead79f3f13d9cc26e88138
Hulto <hulto@hul.to>
2022-06-12 05:00:24 +0000

    TCP and UDP tests work.

--- implants/eldritch/src/pivot/ncat_impl.rs
+++ implants/eldritch/src/pivot/ncat_impl.rs
@@ -1,7 +1,66 @@
-use anyhow::Result;
 
-pub fn ncat(_address: String, _port: i32, _data: String, _protocol: String, _timeout: i32) -> Result<String> {
-    unimplemented!("Method unimplemented")
+use std::net::Ipv4Addr;
+
+use anyhow::Result;
+use tokio::io::{AsyncWriteExt, AsyncReadExt};
+use tokio::net::{TcpStream, UdpSocket};
+
+// Since we cannot go from async (test) -> sync (ncat) `block_on` -> async (handle_ncat) without getting an error "cannot create runtime in current runtime since current thread is calling async code."
+async fn handle_ncat(address: String, port: i32, data: String, protocol: String) -> Result<String> {
+    let mut response_buffer = [0; 1024];
+    let result_string: String;
+    let  address_and_port = format!("{}:{}", address, port);
+    if protocol == "tcp" {
+        // Connect to remote host
+        let mut connection = TcpStream::connect(&address_and_port).await?;
+
+        // Write our meessage
+        connection.write_all(data.as_bytes()).await?;
+        // Read server response
+        let _bytes_read_count = connection.read(&mut response_buffer).await?;    
+
+        // We  need to take a buffer of bytes, turn it into a String but that string has null bytes.
+        // To remove the null bytes we're using trim_matches.
+        result_string = String::from(String::from_utf8((&response_buffer).to_vec()).unwrap().trim_matches(char::from(0)));
+        Ok(result_string)
+
+    } else if protocol == "udp" {
+        // Connect to remote host
+    
+        // Setting the bind address to unspecified should leave it up to the OS to decide.
+        // https://stackoverflow.com/a/67084977
+        let sock = UdpSocket::bind((Ipv4Addr::UNSPECIFIED, 0)).await?;
+        sock.connect(address_and_port.clone()).await?;
+
+        // Send bytes to remote host
+        let _bytes_sent = sock.send_to(data.as_bytes(), address_and_port.clone()).await?;
+    
+        // Recieve any response from remote host
+        let mut response_buffer = [0; 1024];
+        let (_bytes_copied, _addr) = sock.recv_from(&mut response_buffer).await?;
+
+        // We  need to take a buffer of bytes, turn it into a String but that string has null bytes.
+        // To remove the null bytes we're using trim_matches.
+        result_string = String::from(String::from_utf8((&response_buffer).to_vec()).unwrap().trim_matches(char::from(0)));
+        Ok(result_string)
+
+    } else {
+        return Err(anyhow::anyhow!("Protocol not supported please use: udp or tcp."));
+    }
+}
+
+// We do not want to make this async since it would require we make all of the starlark bindings async.
+pub fn ncat(address: String, port: i32, data: String, protocol: String) -> Result<String> {
+    let runtime = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    let response = runtime.block_on(
+        handle_ncat(address, port, data, protocol)
+    );
+    
+    Ok(String::from(response.unwrap()))
 }
 
 #[cfg(test)]
@@ -9,31 +68,44 @@ mod tests {
     use super::*;
 
     use tokio::net::TcpListener;
-    use tokio::net::TcpStream;
+    use tokio::net::UdpSocket;
     use tokio::task;
     use tokio::io::copy;
 
-
-    async fn setup_test_listener(address: String, port: i32, protocol: String, _timeout: i32) -> anyhow::Result<()> {
+    async fn setup_test_listener(address: String, port: i32, protocol: String) -> anyhow::Result<()> {
+        let mut i = 0;
         if protocol == "tcp" {
             let listener = TcpListener::bind(format!("{}:{}", address,  port)).await?;
-            let mut i = 0;
-
             while i < 100 {
-                println!("Accepting connections");
+                // Accept new connection
                 let (mut socket, _) = listener.accept().await?;
+                // Split reader and writer references
                 let (mut reader, mut writer) = socket.split();
+                // Copy from reader to writer to echo message back.
                 let bytes_copied = copy(&mut reader, &mut writer).await?;
+                // If message sent break loop
                 if bytes_copied > 1 {
                     break;
                 }
                 i = i + 1;
             }
-            
-    
         } else if protocol == "udp" {
+            let sock = UdpSocket::bind(format!("{}:{}", address,  port)).await?;
 
+            let mut buf = [0; 1024];
+            while i < 100 {
+                println!("Accepting connections");
+                let (bytes_copied, addr) = sock.recv_from(&mut buf).await?;
+
+                let bytes_copied = sock.send_to(&buf[..bytes_copied], addr).await?;
+
+                if bytes_copied > 1 {
+                    break;
+                }
+                i = i + 1;
+            }
         } else {
+            println!("Unrecognized protocol");
             panic!("Unrecognized protocol")
         }
         Ok(())    
@@ -43,25 +115,67 @@ mod tests {
     async fn test_ncat_send_tcp() -> anyhow::Result<()> {
         // Setup a test echo server
         let expected_response = String::from("Hello world!");
-        println!("Starting listener");
-        let listen_task = task::spawn(setup_test_listener(String::from("127.0.0.1"),
-            65432, String::from("tcp"), 3));
+        let listen_task = task::spawn(
+            setup_test_listener(String::from("127.0.0.1"),65432, String::from("tcp"))
+        );
 
-        // Send data
-        // let actual_response = ncat(String::from("127.0.0.1"),
-        //     65432, expected_response.clone(), String::from("tcp"), 3)?;
-        // println!("{}", actual_response);
+        // Setup a sender
+        let send_task = task::spawn(
+            handle_ncat(String::from("127.0.0.1"), 65432, expected_response.clone(), String::from("tcp"))
+        );
+
+        // Will this create a race condition where the sender sends before the listener starts?
+        // Run both
+        let (_a, actual_response) = tokio::join!(listen_task,send_task);
+
+
         // Verify our data
-        let _a = tokio::join!(listen_task);
-        // assert_eq!(expected_response, actual_response);
+        assert_eq!(expected_response, actual_response.unwrap().unwrap());
+        Ok(())
+    }
+    #[tokio::test]
+    async fn test_ncat_send_udp() -> anyhow::Result<()> {
+        // Setup a test echo server
+        let expected_response = String::from("Hello world!");
+        let listen_task = task::spawn(
+            setup_test_listener(String::from("127.0.0.1"),65432, String::from("udp"))
+        );
+
+        // Setup a sender
+        let send_task = task::spawn(
+            handle_ncat(String::from("127.0.0.1"), 65432, expected_response.clone(), String::from("udp"))
+        );
+
+        // Will this create a race condition where the sender sends before the listener starts?
+        // Run both
+        let (_a, actual_response) = tokio::join!(listen_task,send_task);
+
+
+        // Verify our data
+        assert_eq!(expected_response, actual_response.unwrap().unwrap());
         Ok(())
     }
     #[test]
-    fn test_ncat_send_udp() -> anyhow::Result<()> {
+    fn test_ncat_not_handle() -> anyhow::Result<()> {
+        let result = ncat(String::from("127.0.0.1"), 65431, String::from("No one can hear me!"), String::from("tcp"));
+        let response = match result {
+            Ok(_) => todo!(),
+            Err(_) => return Ok(()),
+        };
         Ok(())
-    }
-    #[test]
-    fn test_ncat_failure_timeout() -> anyhow::Result<()> {
-        Ok(())
+            //     panic!("No connection should be made. Expect connection refused.")
+        // } else {
+        //     println!(E)
+        // }
+        //  let response = match result {
+        //     Ok(res) => res,
+        //     Err(err) => return Err(err),
+        //   };
+
+        // match res {
+        //     Ok(_) => todo!(),
+        //     Err(_) => break,
+        // }
+        // Ok(())
     }
 }
