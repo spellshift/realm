c03e87606eea018041372edaeb2927aab9db7ff8
hulto <hulto@hul.to>
2022-10-20 01:02:48 +0000

    Poc works golem interpreter works.

--- implants/golem/src/lib.rs
+++ implants/golem/src/lib.rs
@@ -2,3 +2,9 @@
 pub enum Error {
     Io(std::io::Error),
 }
+
+impl From<std::io::Error> for Error {
+    fn from(error: std::io::Error) -> Self {
+        Error::Io(error)
+    }
+}
--- implants/golem/src/main.rs
+++ implants/golem/src/main.rs
@@ -1,60 +1,113 @@
 extern crate golem;
 extern crate eldritch;
 
-use clap::{Command, arg};
-use std::fs::File;
-use std::path::Path;
+use clap::{Command, Arg};
+use tokio::task;
+use std::fs;
 
-// async fn install(config_path: String) -> Result<(), imix::Error> {
-//     let config_file = File::open(config_path)?;
-//     let config: imix::Config = serde_json::from_reader(config_file)?;
+use starlark::{starlark_module};
+use starlark::environment::{GlobalsBuilder, Globals, Module};
+use starlark::syntax::{AstModule, Dialect};
+use starlark::eval::Evaluator;
+use starlark::values::Value;
 
-//     #[cfg(target_os = "windows")]
-// 	    return imix::windows::install(config).await;
-	
-//     #[cfg(target_os = "linux")]
-//         if Path::new(imix::linux::SYSTEMD_DIR).is_dir() {
-//             return imix::linux::install(config).await;
-//         }
-	
-// 	unimplemented!("The current OS/Service Manager is not supported")
-// }
+use eldritch::file::FileLibrary;
+use eldritch::process::ProcessLibrary;
+use eldritch::sys::SysLibrary;
 
-// async fn run(config_path: String) -> Result<(), imix::Error> {
-//     let config_file = File::open(config_path)?;
-//     let config: imix::Config = serde_json::from_reader(config_file)?;
+fn eldritch_run(tome_filename: String, tome_contents: String) -> Result<String, golem::Error> {
+    let ast: AstModule = AstModule::parse(
+        &tome_filename, 
+        tome_contents.as_str().to_owned(), 
+        &Dialect::Standard
+    ).unwrap();
 
-//     #[cfg(target_os = "windows")]
-//         return imix::windows::run(config).await;
+    #[starlark_module]
+    fn eldritch(builder: &mut GlobalsBuilder) {
+        const file: FileLibrary = FileLibrary();
+        const process: ProcessLibrary = ProcessLibrary();
+        const sys: SysLibrary = SysLibrary();
+    }
 
-//     #[cfg(target_os = "linux")]
-//         if Path::new(imix::linux::SYSTEMD_DIR).is_dir() {
-//             return imix::linux::run(config).await;
-//         }
+    let globals = GlobalsBuilder::extended().with(eldritch).build();
+    let module: Module = Module::new();
 
-//     unimplemented!("The current OS/Manager is not supported")
-// }
+    let mut eval: Evaluator = Evaluator::new(&module);
+
+    let res: Value = eval.eval_module(ast, &globals).unwrap();
+
+    println!("{:?}", res.unpack_str());
+
+    // let mut eld = Evaluator::eval_function(
+    //     &mut self, "dir(file)", positional, named
+    // );
+
+//     let mut a = Assert::new();
+//     a.globals_add(globals);
+//     a.all_true(
+//         r#"
+// dir(file) == ["append", "copy", "download", "exists", "hash", "is_dir", "mkdir", "read", "remove", "rename", "replace", "replace_all", "timestomp", "write"]
+// dir(process) == ["kill", "list", "name"]
+// dir(sys) == ["exec", "is_linux", "is_macos", "is_windows", "shell"]
+// "#,
+//     );
+    Ok("Ran tome".to_string())
+}
+
+async fn interactive() -> Result<(), golem::Error> {
+    Ok(())
+}
+
+async fn run(tome_path: String) -> Result<String, golem::Error> {
+    println!("Executing {}", tome_path);
+    // Read a tome script
+    let tome_contents = fs::read_to_string(tome_path.clone())?;
+    // Execute a tome script
+    let tome_results: String = eldritch_run(tome_path, tome_contents)?;
+    // Return script output
+    return Ok(tome_results)
+}
 
 #[tokio::main]
 async fn main() -> Result<(), golem::Error> {
-    // let matches = Command::new("imix")
-    //     .arg(
-    //         arg!(
-    //             -f --file <FILE> "Execute a specific tome file"
-    //         )
-    //         .required(false)
-    //     )
-    //     .get_matches();
-    Ok(())
-    // if let Some(tome_path) = matches.value_of("file") {
-    //     return run(String::from(tome_path)).await
-    // }
+    let matches = Command::new("golem")
+        .arg(Arg::with_name("INPUT")
+            .help("Set the tomes to run")
+            .multiple_occurrences(true)
+            .required(false)
+        ).get_matches();
 
-    // match matches.subcommand() {
-    //     Some(("install", args)) => {
-    //         let config_path = args.value_of("config").unwrap();
-    //         install(String::from(config_path)).await
-    //     },
-    //     _ => Ok(())
-    // }
+    if matches.contains_id("INPUT") {
+        // Get list of files
+        let res = matches.try_get_many::<String>("INPUT")
+            .unwrap()
+            .unwrap();
+
+
+        // Queue async tasks
+        let mut all_tome_futures: Vec<_> = vec![];
+        for tome in res{
+            println!("Queueing {}", tome.clone().to_string());
+            let tome_execution_task = run(tome.to_string());
+            all_tome_futures.push(task::spawn(tome_execution_task))
+        }
+
+        // Collect results
+        let mut result: Vec<String> = Vec::new();
+        for tome_task in all_tome_futures {
+            match tome_task.await.unwrap() {
+                Ok(res) => result.push(res),
+                Err(_err) => continue,
+            }
+        }
+
+        println!("{:?}", result);
+
+    } else {
+        match interactive().await {
+            Ok(_) => todo!(),
+            Err(_) => todo!(),
+        }
+    }
+    Ok(())
 }
