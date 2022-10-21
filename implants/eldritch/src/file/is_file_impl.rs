a491ff2be8374aa66cfab09f215bcf6d3939538e
hulto <hulto@hul.to>
2022-10-21 03:13:06 +0000

    Added is_file stubs.

---
+++ implants/eldritch/src/file/is_file_impl.rs
@@ -0,0 +1,16 @@
+use anyhow::Result;
+
+pub fn is_file(_path: String) -> Result<()> {
+    unimplemented!("Method unimplemented")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tempfile::{NamedTempFile,tempdir};
+
+    #[test]
+    fn is_() -> anyhow::Result<()>{
+        unimplemented!("Method unimplemented")
+    }
+}
