d6d51cd2ba146043e124f7e374812385a019779a
hulto <hulto@hul.to>
2022-10-22 19:01:57 +0000

    Cleaned up temp file premature closes.

--- implants/eldritch/src/file/compress_impl.rs
+++ implants/eldritch/src/file/compress_impl.rs
@@ -4,15 +4,12 @@ use anyhow::Result;
 use tar::{Builder, HeaderMode};
 use tempfile::NamedTempFile;
 
-fn tar_dir(src: String) -> Result<String> {
+fn tar_dir(src: String, dst: String) -> Result<String> {
     let src_path = Path::new(&src);
 
-    let tmp_tar_file_src = NamedTempFile::new()?;
-    let tmp_tar_file_src_path = format!("{}.tar", String::from(tmp_tar_file_src.path().to_str().unwrap()));
-    let _ = tmp_tar_file_src.close();
 
     // Create the tar bulider
-    let tmp_tar_file = File::create(tmp_tar_file_src_path.clone()).unwrap();
+    let tmp_tar_file = File::create(dst.clone()).unwrap();
     let mut tar_builder = Builder::new(tmp_tar_file);
     tar_builder.mode(
         HeaderMode::Deterministic
@@ -24,7 +21,7 @@ fn tar_dir(src: String) -> Result<String> {
     );
     let _ = tar_builder.finish();
 
-    Ok(tmp_tar_file_src_path.clone())
+    Ok(dst.clone())
 }
 
 pub fn compress(src: String, dst: String) -> Result<()> {
@@ -32,13 +29,15 @@ pub fn compress(src: String, dst: String) -> Result<()> {
     let mut tmp_src = src.clone();
     let src_path = Path::new(&tmp_src);
 
-    println!("Start");
+    let tmp_tar_file_src = NamedTempFile::new()?;
+    let tmp_tar_file_src_path = String::from(tmp_tar_file_src.path().to_str().unwrap());
+
     // If our source is a dir create a tarball and update the src file to the tar ball.
     if src_path.clone().is_dir() {
-        println!("IsDir");
-        tmp_src = tar_dir(tmp_src).unwrap();
+        tmp_src = tar_dir(tmp_src, tmp_tar_file_src_path).unwrap();
+    } else {
+        let _ = tmp_tar_file_src.close();
     }
-    println!("Readers");
     // Check if src is a dir.
     // if dir tar the directry up.
     let mut f_src = std::io::BufReader::new(std::fs::File::open(tmp_src.clone()).unwrap());
@@ -47,9 +46,9 @@ pub fn compress(src: String, dst: String) -> Result<()> {
             .create(true) //Do we want to create the file if it doesn't exist? - Yes!
             .write(true)    
             .open(dst.clone())?);
-    println!("Compress");
+
     lzma_rs::xz_compress(&mut f_src, &mut f_dst).unwrap();
-    println!("Done");
+
     Ok(())
 }
 
@@ -68,7 +67,7 @@ mod tests {
         let path_src = String::from(tmp_file_src.path().to_str().unwrap());
         let tmp_file_dst = NamedTempFile::new()?;
         let path_dst = String::from(tmp_file_dst.path().to_str().unwrap());
-        let _res = tmp_file_dst.close();
+        // let _res = tmp_file_dst.close();
 
         // Write to file
         tmp_file_src.write_all(b"Hello, world!")?;
@@ -90,14 +89,14 @@ mod tests {
         // Create files
         let mut tmp_dir_src = tempdir()?;
         let path_src = String::from(tmp_dir_src.path().to_str().unwrap());
-        // for (i,v) in ["Hello", "World", "Goodbye"].iter().enumerate() {
-        //     let tmp_file = format!("{}/{}.txt", path_src.clone(), i);
-        //     let _res = fs::write(tmp_file, v);
-        // }
+        for (i,v) in ["Hello", "World", "Goodbye"].iter().enumerate() {
+            let tmp_file = format!("{}/{}.txt", path_src.clone(), i);
+            let _res = fs::write(tmp_file, v);
+        }
 
         let tmp_file_dst = NamedTempFile::new()?;
-        let path_dst = format!("{}.tar.xz", String::from(tmp_file_dst.path().to_str().unwrap()));
-        let _res = tmp_file_dst.close();
+        let path_dst = String::from(tmp_file_dst.path().to_str().unwrap());
+        // let _res = tmp_file_dst.close();
 
         // Run our code 
         // Test with trailing slash.
