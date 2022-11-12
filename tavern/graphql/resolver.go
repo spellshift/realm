2abedc6a4e0c0ab92bdf6edb6a3823682dfa630d
Kyle Carretto <kcarretto@gmail.com>
2022-11-12 04:46:04 +0000

    inject ent client

--- tavern/graphql/resolver.go
+++ tavern/graphql/resolver.go
@@ -1,7 +1,17 @@
 package graphql
 
-// This file will not be regenerated automatically.
-//
-// It serves as dependency injection for your app, add any dependencies you require here.
+import (
+	"github.com/kcarretto/realm/tavern/ent"
 
-type Resolver struct{}
+	"github.com/99designs/gqlgen/graphql"
+)
+
+// Resolver is the resolver root.
+type Resolver struct{ client *ent.Client }
+
+// NewSchema creates a graphql executable schema.
+func NewSchema(client *ent.Client) graphql.ExecutableSchema {
+	return NewExecutableSchema(Config{
+		Resolvers: &Resolver{client},
+	})
+}
