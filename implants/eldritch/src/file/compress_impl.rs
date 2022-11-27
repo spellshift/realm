74edd3896839b086ad3b18a36b2978a27b1a87b3
hulto <hulto@hul.to>
2022-11-27 19:17:08 +0000

    Emulate buffered write.

--- implants/eldritch/src/file/compress_impl.rs
+++ implants/eldritch/src/file/compress_impl.rs
@@ -39,8 +39,8 @@ pub fn compress(src: String, dst: String) -> Result<()> {
     } else {
         let _ = tmp_tar_file_src.close();
     }
-    // Check if src is a dir.
-    // if dir tar the directry up.
+
+    // Setup buffered reader writer.
     let f_src = std::io::BufReader::new(std::fs::File::open(tmp_src.clone()).unwrap());
     let mut f_dst = std::io::BufWriter::new(
             OpenOptions::new()
@@ -49,39 +49,17 @@ pub fn compress(src: String, dst: String) -> Result<()> {
             .open(dst.clone())?
     );
 
-    // let compressed_file = File::create(dst.clone())?;
-    // let mut encoder = flate2::write::GzEncoder::new(compressed_file, Compression::fast());
-    // {
-    //     // Create tar archive and compress files 
-    //     let mut archive = Builder::new(&mut encoder);
-    //     archive.append_dir_all(
-    //         src_path.clone().file_name().unwrap().to_str().unwrap(),
-    //         src_path.clone(),
-    //     )?;
-    // }
-    // encoder.finish();
-
-    let mut deflater = flate2::bufread::GzEncoder::new(f_src, Compression::fast());
-    // let mut gz_engine = flate2::write::GzEncoder::new(f_dst, Compression::fast());
-    // let mut buf = Vec::with_capacity(1024*16);
-
-
-    // while let Err(e) = f_src.fill_buf() {
-    //     if e.kind() != io::ErrorKind::Interrupted {
-    //         return Err(e);
-    //     }
-    // }
-    // gz_engine.write(f_src);
-    // This is bad. Holding the entire compressed file seems unideal.
-    // Is it possible to compress a file directly to disk?
-    let mut buffer: Vec<u8> = vec![0; 1024*16];
-    deflater.read_to_end(&mut buffer)?;
-    let _ = f_dst.write_all(&mut buffer);
     
+    let mut deflater = flate2::bufread::GzEncoder::new(f_src, Compression::fast());
 
-    println!("{}", buffer.len());
-    // lzma_rs::lzma2_compress(&mut f_src, &mut f_dst).unwrap();
-    // let mut compressed = lzma::compress(test_string.as_bytes(), 6).unwrap();
+    // Write 
+    let read_buffer_size = 1024*50;
+    let mut bytes_read = read_buffer_size;
+    while bytes_read != 0 {
+        let mut buffer: Vec<u8> = vec![0; read_buffer_size];
+        bytes_read = deflater.read(&mut buffer)?;
+        let _ = f_dst.write_all(&mut buffer[0..bytes_read]);
+    }
 
     Ok(())
 }
@@ -92,7 +70,7 @@ mod tests {
     use std::{io::{prelude::*, BufReader}, fs};
     use sha256::digest_file;
     use tempfile::{tempdir, NamedTempFile};
-
+    
     #[test]
     fn test_compress_basic() -> anyhow::Result<()>{
         // Create files
@@ -112,30 +90,7 @@ mod tests {
         let hash = digest_file(path_dst.clone())?;
 
         // Compare
-        assert_eq!(hash, "2ebf88a2917afdf9e8b3d5e0573457ee03f0d37780de770e94da38f55f298d73");
-
-        Ok(())
-    }
-
-    #[test]
-    fn test_compress_dir() -> anyhow::Result<()>{
-        // Create files
-        let tmp_dir_src = tempdir()?;
-        let path_src = String::from(tmp_dir_src.path().to_str().unwrap());
-        for (i,v) in ["Hello", "World", "Goodbye"].iter().enumerate() {
-            let tmp_file = format!("{}/{}.txt", path_src.clone(), i);
-            let _res = fs::write(tmp_file, v);
-        }
-
-        let tmp_file_dst = NamedTempFile::new()?;
-        let path_dst = String::from(tmp_file_dst.path().to_str().unwrap());
-
-        // Run our code 
-        // Test with trailing slash.
-        compress(format!("{}/", path_src.clone()), path_dst.clone())?;
-
-        // Test that no errors were raised.
-        assert_eq!(true, true);
+        assert_eq!(hash, "a4a62449deb847be376f523c527ddb8e37eda2a4a71dd293d3ddcd4c4a81941f");
 
         Ok(())
     }
@@ -174,12 +129,4 @@ mod tests {
         assert_eq!(res, searchstrings.len());
         Ok(())
     }
-
-    // #[test]
-    // fn test_compress_manual() -> anyhow::Result<()>{
-    //     // Create files
-    //     compress("/tmp/bigfile".to_string(), "/tmp/bigfile.gz".to_string())?;
-    //     Ok(())
-    // }
-
 }
