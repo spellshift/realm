c4b6b742c7321bf4980cda999e73a45902661eac
Hulto <7121375+hulto@users.noreply.github.com>
2023-02-08 15:48:13 -0500

    63 feature eldritch filetemplate starlarkdict (#84)
    * Latest version of starlark rust throws issues.
    
    * Rebased is_file in
    
    * No more warnings. All tests pass.
    
    * Pinning git commit.
    
    * Build works, tests pass.
    
    * Rebased
    
    * Rebasing
    
    * Rebasing
    
    * Rebasing
    
    * Trying to get starlark dicts working. rebased.
    
    * rebase and working on nested dict
    
    * Merged
    
    * Removed merge artifact
    
    * Testing dictionary creation.
    
    * Cleaned up code.
    
    * Cleaning up
    
    * All tests pass
    
    * Latest version of starlark rust throws issues.
    
    * Rebased is_file in
    
    * No more warnings. All tests pass.
    
    * Rebased
    
    * Rebasing
    
    * Rebasing
    
    * Rebasing
    
    * Trying to get starlark dicts working. rebased.
    
    * rebase and working on nested dict
    
    * Merged
    
    * Removed merge artifact
    
    * Testing dictionary creation.
    
    * Cleaned up code.
    
    * Cleaning up
    
    * All tests pass
    
    * Reverted rust version
    
    * Resolving comments from nick.
    
    * CI again

--- implants/eldritch/src/file.rs
+++ implants/eldritch/src/file.rs
@@ -11,11 +11,13 @@ mod remove_impl;
 mod moveto_impl;
 mod replace_all_impl;
 mod replace_impl;
+mod template_impl;
 mod timestomp_impl;
 mod write_impl;
 
 use derive_more::Display;
 
+use starlark::collections::SmallMap;
 use starlark::environment::{Methods, MethodsBuilder, MethodsStatic};
 use starlark::values::none::NoneType;
 use starlark::values::{StarlarkValue, Value, UnpackValue, ValueLike, ProvidesStaticType};
@@ -121,6 +123,11 @@ fn methods(builder: &mut MethodsBuilder) {
     fn timestomp(this: FileLibrary, src: String, dst: String) -> anyhow::Result<NoneType> {
         if false { println!("Ignore unused this var. _this isn't allowed by starlark. {:?}", this); }
         timestomp_impl::timestomp(src, dst)?;
+        Ok(NoneType{})
+    }
+    fn template(this: FileLibrary, template_path: String, dst_path: String, args: SmallMap<String, Value>, autoescape: bool) -> anyhow::Result<NoneType> {
+        if false { println!("Ignore unused this var. _this isn't allowed by starlark. {:?}", this); }
+        template_impl::template(template_path, dst_path, args, autoescape)?;
         Ok(NoneType{})
     }
     fn write(this: FileLibrary, path: String, content: String) -> anyhow::Result<NoneType> {
---
+++ implants/eldritch/src/file/template_impl.rs
@@ -0,0 +1,113 @@
+use std::fs;
+
+use anyhow::Result;
+use starlark::{collections::SmallMap};
+use starlark::values::Value;
+use tera::{Context, Tera};
+
+fn build_context(dict_data: SmallMap<String, Value>) -> Result<Context> {
+    let mut context = Context::new();
+    for (key, value) in dict_data {
+        let tmp = value;
+        context.insert(key.as_str(), &tmp);
+    }
+    Ok(context)
+}
+
+pub fn template(template_path: String, dst_path: String, args: SmallMap<String, Value>, autoescape: bool) -> Result<()> {
+    let context = build_context(args)?;
+    let template_content = fs::read_to_string(template_path)?;
+    let res_content = Tera::one_off(template_content.as_str(), &context, autoescape)?;
+    fs::write(dst_path, res_content)?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+    use starlark::{collections::SmallMap, values::FrozenHeap};
+    use tempfile::NamedTempFile;
+    use starlark::const_frozen_string;
+
+    use super::*;
+
+    #[test]
+    fn test_template_build_context() -> anyhow::Result<()>{
+        let mut map: SmallMap<String, Value> = SmallMap::new();
+        map.insert("name".to_string(), const_frozen_string!("greg").to_value());
+        map.insert("age".to_string(), Value::new_int(29));
+        map.insert("admin".to_string(), Value::new_bool(true));
+
+        let res = build_context(map)?;
+        assert_eq!(format!("{:?}", res), r#"Context { data: {"admin": Bool(true), "age": Number(29), "name": String("greg")} }"#);
+        Ok(())
+    }
+
+    #[test]
+    fn test_template_basic() -> anyhow::Result<()>{
+        let tmp_file = NamedTempFile::new()?;
+        let path = String::from(tmp_file.path().to_str().unwrap());
+        let dst_tmp_file = NamedTempFile::new()?;
+        let dst_path = String::from(tmp_file.path().to_str().unwrap());
+        dst_tmp_file.close()?;
+
+        // Write out template
+        fs::write(path.clone(),
+r#"Hello {{ name }},
+welcome to the eldritch realm test.
+I see you're {{ age }} years old.
+Congratulations on making it that far.
+"#.as_bytes())?;
+        // Setup our args
+        let mut dict_data: SmallMap<String, Value> = SmallMap::new();
+        dict_data.insert("name".to_string(), const_frozen_string!("test123").to_value());
+        dict_data.insert("age".to_string(), Value::new_int(22));
+
+        // Run our code
+        template(path, dst_path.clone(), dict_data, true)?;
+
+        // Verify output
+        let res = fs::read_to_string(dst_path)?;
+        assert_eq!(res,
+r#"Hello test123,
+welcome to the eldritch realm test.
+I see you're 22 years old.
+Congratulations on making it that far.
+"#.to_string());
+        Ok(())
+    }
+
+    #[test]
+    fn test_template_loops_and_lists() -> anyhow::Result<()>{
+        let tmp_file = NamedTempFile::new()?;
+        let path = String::from(tmp_file.path().to_str().unwrap());
+        let dst_tmp_file = NamedTempFile::new()?;
+        let dst_path = String::from(tmp_file.path().to_str().unwrap());
+        dst_tmp_file.close()?;
+
+        // Write out template
+        fs::write(path.clone(),
+r#"All the animals on the farm:{% for animal in animals %}
+-{{ animal }}{% endfor %}
+"#.as_bytes())?;
+
+        // Setup our args
+        let mut dict_data: SmallMap<String, Value> = SmallMap::new();
+        let animals_list = [const_frozen_string!("pig").to_frozen_value(), const_frozen_string!("cow").to_frozen_value()];
+        let frozen_heap = FrozenHeap::new();
+        let animal_heap = frozen_heap.alloc_list(&animals_list[0..2]);
+        dict_data.insert("animals".to_string(), animal_heap.to_value());
+
+        // Run our code
+        template(path, dst_path.clone(), dict_data, true)?;
+
+        // Verify output
+        let res = fs::read_to_string(dst_path)?;
+        assert_eq!(res,
+r#"All the animals on the farm:
+-pig
+-cow
+"#.to_string());
+        Ok(())
+    }
+}
