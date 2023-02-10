c47396236e6a5bcfff0abd6e48e1cebc4f491ab2
Hulto <7121375+hulto@users.noreply.github.com>
2023-02-10 17:35:29 -0500

    64 feature eldritch filecompress (#85)
    * Added docs.
    
    * Add dir and file compression.
    
    * Added tar directory test.
    
    * Working compression with flate.
    
    * Emulate buffered write.

--- implants/eldritch/src/file.rs
+++ implants/eldritch/src/file.rs
@@ -1,5 +1,6 @@
 mod append_impl;
 mod copy_impl;
+mod compress_impl;
 mod download_impl;
 mod exists_impl;
 mod hash_impl;
@@ -68,6 +69,11 @@ fn methods(builder: &mut MethodsBuilder) {
     fn copy(this: FileLibrary, src: String, dst: String) -> anyhow::Result<NoneType> {
         if false { println!("Ignore unused this var. _this isn't allowed by starlark. {:?}", this); }
         copy_impl::copy(src, dst)?;
+        Ok(NoneType{})
+    }
+    fn compress(this: FileLibrary, src: String, dst: String) -> anyhow::Result<NoneType> {
+        if false { println!("Ignore unused this var. _this isn't allowed by starlark. {:?}", this); }
+        compress_impl::compress(src, dst)?;
         Ok(NoneType{})
     }
     fn download(this: FileLibrary, uri: String, dst: String) -> anyhow::Result<NoneType> {
