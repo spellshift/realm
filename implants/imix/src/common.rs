3d20459902d2882dee745d9e8abc669e7379ae08
hulto <7121375+hulto@users.noreply.github.com>
2023-02-10 01:01:56 +0000

    merging

--- implants/imix/src/common.rs
+++ implants/imix/src/common.rs
@@ -1,10 +1,9 @@
-use std::{time::{self, SystemTime, UNIX_EPOCH}, thread};
+use std::{time, thread};
 
-use crate::graphql::call;
+use anyhow::Error;
+use tokio::task::JoinHandle;
 
 use super::graphql;
-use tokio::task::{self, JoinHandle};
-use anyhow::{Result,Error};
 
 enum TaskStatus {
     Waiting,
@@ -24,55 +23,13 @@ struct Task {
     data: TaskData, //Not sure
 }
 
-async fn execute_tome(tome_path: String) {
+async fn execute_tome(tome_path: String) -> String {
     let ten_millis = time::Duration::from_secs(10);
-    thread::sleep(ten_millis);    
+    thread::sleep(ten_millis);
+    return "A String".to_string()
 }
 
-// async fn graphql_request_update() -> graphql::GraphQLResponse {
-//     unimplemented!("graphql will callback to the c2 server. Update the ")
-// }
-
-async fn pull_tome() {
-    unimplemented!("graphql will callback to the c2 server. Update the ")
-}
-
-pub async fn main_loop() -> ! {
-    let mut active_tasks: Vec<Task> = vec![];
-    let mut graphql_responses: Vec<graphql::TaskResult> = vec![];
-    loop {
-        for task in &active_tasks {
-            // Read off STDOUT 
-            let task_output = "Some string".to_string();
-
-            graphql_responses.push(graphql::TaskResult{
-                task_id: task.task_id.clone(),
-                exec_finished_at: SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs(),
-                output: task_output,
-                error: "No errors. Never.".to_string(),
-            })
-        }
-
-        match call("variables".to_string(), "uri".to_string(), 1234).await {
-            Ok(respone) => {
-                for new_task in [respone] {
-                    active_tasks.append(Task{
-                        task_id: new_task.,
-                        start_time: todo!(),
-                        status: todo!(),
-                        future_handle: todo!(),
-                        data: todo!(),
-                    });
-                }
-            },
-            Err(_) => todo!(),
-        }
-        // Get new tasks from the server.
-        // let new_tasks = graphql_request_update();
-
-        // let new_tasks = get_new_tasks();
-        // for task in new_tasks {
-        //     active_tasks.append()
-        // }
-    }
+pub async fn main_loop() {
+    graphql::gql_claim_task();
+    unimplemented!("Nothing here yet. ")
 }
--- implants/imix/src/lib.rs
+++ implants/imix/src/lib.rs
@@ -3,33 +3,12 @@ use serde::{Serialize, Deserialize};
 #[derive(Debug)]
 pub enum Error {
     Io(std::io::Error),
-    HyperInvalidUri(hyper::http::uri::InvalidUri),
-    HyperHttp(hyper::http::Error),
-    Hyper(hyper::Error),
     SerdeJson(serde_json::Error)
 }
 
 impl From<std::io::Error> for Error {
     fn from(error: std::io::Error) -> Self {
         Error::Io(error)
-    }
-}
-
-impl From<hyper::http::uri::InvalidUri> for Error {
-    fn from(error: hyper::http::uri::InvalidUri) -> Self {
-        Error::HyperInvalidUri(error)
-    }
-}
-
-impl From<hyper::http::Error> for Error {
-    fn from(error: hyper::http::Error) -> Self {
-        Error::HyperHttp(error)
-    }
-}
-
-impl From<hyper::Error> for Error {
-    fn from(error: hyper::Error) -> Self {
-        Error::Hyper(error)
     }
 }
 
@@ -70,5 +49,5 @@ pub struct Config {
 
 pub mod windows;
 pub mod linux;
-pub mod graphql;
 pub mod common;
+pub mod graphql;
