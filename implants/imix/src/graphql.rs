2301928f439797daa26577a0364cf696ab52d8a3
Hulto <7121375+hulto@users.noreply.github.com>
2023-02-24 04:15:58 +0000

    Removed bad test.

--- implants/imix/src/graphql.rs
+++ implants/imix/src/graphql.rs
@@ -294,32 +294,32 @@ mod tests {
 
         assert_eq!(response.id,"17179869186".to_string());
     }
-    #[test] // This works.
-    fn imix_graphql_ssl_test() {
-        let start_time = Utc::now();
+    // #[test] // This works.
+    // fn imix_graphql_ssl_test() {
+    //     let start_time = Utc::now();
 
-        let runtime = tokio::runtime::Builder::new_current_thread()
-            .enable_all()
-            .build()
-            .unwrap();
+    //     let runtime = tokio::runtime::Builder::new_current_thread()
+    //         .enable_all()
+    //         .build()
+    //         .unwrap();
 
-        let response = runtime.block_on(
-        {
-                let client = reqwest::Client::builder()
-                .timeout(Duration::from_secs(5))
-                .danger_accept_invalid_certs(true)
-                .build().unwrap();
+    //     let response = runtime.block_on(
+    //     {
+    //             let client = reqwest::Client::builder()
+    //             .timeout(Duration::from_secs(5))
+    //             .danger_accept_invalid_certs(true)
+    //             .build().unwrap();
         
-                client.get("https://google.com/")
-                .header("Content-Type", "application/json")
-                .header("X-Realm-Auth", "letmeinnn")
-                .body("")
-                .send()
-            }
-        ).unwrap();
+    //             client.get("https://google.com/")
+    //             .header("Content-Type", "application/json")
+    //             .header("X-Realm-Auth", "letmeinnn")
+    //             .body("")
+    //             .send()
+    //         }
+    //     ).unwrap();
 
-        assert_eq!(response.status(), reqwest::StatusCode::OK);
-    }
+    //     assert_eq!(response.status(), reqwest::StatusCode::OK);
+    // }
 
 }
 
