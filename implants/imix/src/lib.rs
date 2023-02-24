b0abbb90c3c6e0d2f708aee34b523b6bc3b425d8
Hulto <7121375+hulto@users.noreply.github.com>
2023-02-23 23:53:09 -0500

    92 feature imix callback and main loop (#110)
    * init
    
    * stashhingn
    
    * merged
    
    * merging
    
    * init
    
    * stashhingn
    
    * merged
    
    * merging
    
    * Saving
    
    * claim tasks works
    
    * Imixcallback json deserializes.
    
    * Struggling with private structs.
    
    * Callbacks deserialize
    
    * add header
    
    * Saving to merge.
    
    * Saving
    
    * Testing for imix callback.
    
    * Post task results works.
    
    * Some mainloop tasking.
    
    * Comments.
    
    * Merging main loops
    
    * Adding imix main tests.
    
    * MVP of imix callbacks.
    
    * Removed dead code.
    
    * Small fixups.
    
    * Cleanup.
    
    * Delete imix-test-config.json
    
    * Deque finished tasks. Ty Nick.
    
    * Delete dead code.
    
    * Removed (crate)
    
    * Moved claim_task_input.
    
    * ClaimTask input is real.
    
    * Not a test.
    
    * Cleaned up warnings.
    
    * Small bug fixes.
    
    * Added test config to .gitignore.
    
    * HTTPS request ignore errors.
    
    * Tested SSL with reqwest builder.
    
    * Added timeout.
    
    * Removed bad test.

--- implants/imix/src/lib.rs
+++ implants/imix/src/lib.rs
@@ -3,33 +3,12 @@ use serde::{Serialize, Deserialize};
 #[derive(Debug)]
 pub enum Error {
     Io(std::io::Error),
-    HyperInvalidUri(hyper::http::uri::InvalidUri),
-    HyperHttp(hyper::http::Error),
-    Hyper(hyper::Error),
     SerdeJson(serde_json::Error)
 }
 
 impl From<std::io::Error> for Error {
     fn from(error: std::io::Error) -> Self {
         Error::Io(error)
-    }
-}
-
-impl From<hyper::http::uri::InvalidUri> for Error {
-    fn from(error: hyper::http::uri::InvalidUri) -> Self {
-        Error::HyperInvalidUri(error)
-    }
-}
-
-impl From<hyper::http::Error> for Error {
-    fn from(error: hyper::http::Error) -> Self {
-        Error::HyperHttp(error)
-    }
-}
-
-impl From<hyper::Error> for Error {
-    fn from(error: hyper::Error) -> Self {
-        Error::Hyper(error)
     }
 }
 
@@ -41,31 +20,31 @@ impl From<serde_json::Error> for Error {
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct C2Config {
-    uri: String,
-    priority: u8,
+    pub uri: String,
+    pub priority: u8,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct ServiceConfig {
     name: String,
     description: String,
     executable_path: String
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct CallbackConfig {
-    interval: u64,
-    jitter: u64,
-    timeout: u64,
-    c2_configs: Vec<C2Config>,
+    pub interval: u64,
+    pub jitter: u64,
+    pub timeout: u64,
+    pub c2_configs: Vec<C2Config>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct Config {
-    target_name: String,
-    target_forward_connect_ip: String,
-    callback_config: CallbackConfig,
-    service_configs: Vec<ServiceConfig>,
+    pub target_name: String,
+    pub target_forward_connect_ip: String,
+    pub callback_config: CallbackConfig,
+    pub service_configs: Vec<ServiceConfig>,
 }
 
 pub mod windows;
