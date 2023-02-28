9c5cf659918e4f6c1dc231f311d21458533ccf37
Hulto <7121375+hulto@users.noreply.github.com>
2023-02-28 04:31:00 +0000

    Cleanup.

--- implants/eldritch/src/params.rs
+++ implants/eldritch/src/params.rs
@@ -11,23 +11,6 @@ pub struct Params  {
 }
 starlark_simple_value!(Params); 
 
-// impl<'v, V: ValueLike<'v>> StarlarkValue<'v> for ParamsGen<V>
-//     where Self: AnyLifetime<'v> {
-//     starlark_type!("params");
-
-//     // To implement methods which are work for both `One` and `FrozenOne`,
-//     // use the `ValueLike` trait.
-// }
-
-// impl<'v> Freeze for Params<'v> {
-//     type Frozen = FrozenParams;
-//     fn freeze(self, freezer: &Freezer) -> anyhow::Result<Self::Frozen> {
-//         Ok(ParamsGen{
-//             data: self.data
-//         })
-//     } 
-// }
-
 impl<'v> StarlarkValue<'v> for Params {
     starlark_type!("params");
 
@@ -52,7 +35,6 @@ impl<'v> UnpackValue<'v> for Params {
     }
 
     fn unpack_value(value: Value<'v>) -> Option<Self> {
-        // value.downcast_ref::<Params>()
         Some(value.downcast_ref::<Params>().unwrap().clone())
     }
 }
@@ -75,19 +57,5 @@ fn methods(builder: &mut MethodsBuilder) {
             None => return Err(anyhow::anyhow!("parameter is invalid type: {}\n", name)),
         };
         Ok(res.to_string())
-
-    //    let map = match serde_json::from_str::<serde_json::Map<String, serde_json::Value>>(this.data.as_str()){
-    //     Ok(val) => val,
-    //     Err(err) => return Err(anyhow::anyhow!("failed to parse parameter JSON: {}\n", err)),
-
-    //     map.get(name)
-    // };
-
-        // let res: String = this.data[&name];
-        // let res: String = match this.data.get(&name) {
-        //     Some(val) => val.to_string().clone(),
-        //     None => return Err(anyhow::anyhow!("No such parameter: {}\n", name)),
-        // };
-        // Ok(res)
     }
 }
