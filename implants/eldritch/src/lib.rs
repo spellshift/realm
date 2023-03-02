99dd298016dc57a881e5521d763c81aa9e98d594
Hulto <7121375+hulto@users.noreply.github.com>
2023-03-01 23:22:24 -0500

    Eldritch input parameters from JSON. (#121)
    * implemented a PoC for accessing params from eldritch
    
    * Add tome_parameter argument to eldritch_run.
    
    * Trying to pass vars into starlark_interpreter.
    
    * Cleanup
    
    * Clean up
    
    * Added imix test is failing.
    
    * Imix test passing.
    
    * JSON read into dict instead of struct.
    
    * Cleanup.
    
    * Update imix test.
    
    * Remove unused params struct.
    
    * Remove whitespace.
    
    * Add number and array support to eldritch params.
    
    * Added param type tests and cleanup.
    
    * Test i32 overflow.
    
    * Fixed windowns / nix whitespace difference.
    
    * Resoving change requests.
    
    * Updated var name.
    
    ---------
    
    Co-authored-by: KCarretto <Kcarretto@gmail.com>

--- implants/eldritch/src/lib.rs
+++ implants/eldritch/src/lib.rs
@@ -3,11 +3,14 @@ pub mod process;
 pub mod sys;
 pub mod pivot;
 
-use starlark::{starlark_module};
+use serde_json::Map;
+use starlark::collections::SmallMap;
+use starlark::starlark_module;
 use starlark::environment::{GlobalsBuilder, Module, Globals};
 use starlark::syntax::{AstModule, Dialect};
 use starlark::eval::Evaluator;
-use starlark::values::Value;
+use starlark::values::dict::Dict;
+use starlark::values::{Value, AllocValue};
 
 use file::FileLibrary;
 use process::ProcessLibrary;
@@ -25,7 +28,7 @@ pub fn get_eldritch() -> anyhow::Result<Globals> {
     return Ok(globals);
 }
 
-pub fn eldritch_run(tome_filename: String, tome_contents: String) -> anyhow::Result<String> {
+pub fn eldritch_run(tome_filename: String, tome_contents: String, tome_parameters: Option<String>) -> anyhow::Result<String> {
     let ast: AstModule;
     match AstModule::parse(
             &tome_filename,
@@ -36,8 +39,65 @@ pub fn eldritch_run(tome_filename: String, tome_contents: String) -> anyhow::Res
             Err(err) => return Err(err),
     }
 
+    let tome_params_str: String = match tome_parameters {
+        Some(param_string) => param_string,
+        None => "".to_string(),
+    };
+
     let globals = get_eldritch()?;
+
     let module: Module = Module::new();
+
+    let res: SmallMap<Value, Value> = SmallMap::new();
+    let mut input_params: Dict = Dict::new(res);
+
+    let parsed: serde_json::Value = serde_json::from_str(&tome_params_str)?;
+    let param_map: serde_json::Map<String, serde_json::Value> = match parsed.as_object() {
+        Some(tmp_param_map) => tmp_param_map.clone(),
+        None => Map::new(),
+    };
+    for (key, value) in param_map.iter() {
+        let mut new_value: Value = Value::new_none();
+        let new_key = module.heap().alloc_str(&key);
+
+        if value.is_string() {
+            let tmp_val = match value.as_str() {
+                Some(tmp_str_val) => tmp_str_val,
+                None => continue, // Failed to get value as str skip it.
+            };
+            new_value = module.heap().alloc_str(tmp_val).to_value();
+        }else if value.is_array() {
+            let mut tmp_list: Vec<Value> = Vec::new();
+            let val_array = match value.as_array() {
+                Some(val_array_vec) => val_array_vec,
+                None => continue, // Failed to get value as str skip it.
+            };
+            for sub_value in val_array {
+                if sub_value.is_string() {
+                    let tmp_sub_value = match sub_value.as_str() {
+                        Some(sub_value_str) => sub_value_str,
+                        None => continue, // Failed to get value as str skip it.
+                    };
+                    tmp_list.push(module.heap().alloc_str(tmp_sub_value).to_value());
+                }
+            }
+            new_value = module.heap().alloc_list(tmp_list.as_slice());
+        }else if value.is_u64() || value.is_i64() || value.is_f64() || value.is_number() {
+            // Down cast the number to i32. On failure return max i32.
+            let tmp_value: i32 = match value.as_i64() {
+                Some(tmp_i64) => match tmp_i64.try_into() {
+                    Ok(tmp_i32) => tmp_i32,
+                    Err(_) => i32::MAX.into(),
+                },
+                None => i32::MAX.into(),
+            };
+            new_value = Value::new_int(tmp_value);
+        }
+        let hashed_key = new_key.to_value().get_hashed()?;
+        input_params.insert_hashed(hashed_key, new_value);
+    }
+
+    module.set("input_params", input_params.alloc_value(module.heap()));
 
     let mut eval: Evaluator = Evaluator::new(&module);
     let res: Value = match eval.eval_module(ast, &globals) {
@@ -45,17 +105,12 @@ pub fn eldritch_run(tome_filename: String, tome_contents: String) -> anyhow::Res
         Err(eval_error) => return Err(anyhow::anyhow!("Eldritch eval_module failed:\n{}", eval_error)),
     };
 
-    // Ok(res.unpack_str().unwrap().to_string())
-    let res_str = match res.unpack_str() {
-        Some(res) => res.to_string(),
-        None => return Err(anyhow::anyhow!("Failed to unpack result as str")),
-    };
-
-    Ok(res_str)
+    Ok(res.to_str())
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use starlark::environment::{GlobalsBuilder};
     use starlark::{starlark_module};
     use starlark::assert::Assert;
@@ -87,4 +142,53 @@ dir(pivot) == ["arp_scan", "bind_proxy", "ncat", "port_forward", "port_scan", "s
 "#,
         );
     }
+    
+    #[test]
+    fn test_library_parameter_input_string() -> anyhow::Result<()>{
+        // Create test script
+        let test_content = format!(r#"
+sys.shell(input_params['cmd2'])
+"#);
+        let param_string = r#"{"cmd":"id","cmd2":"echo hello_world","cmd3":"ls -lah /tmp/"}"#.to_string();
+        let test_res = eldritch_run("test.tome".to_string(), test_content, Some(param_string));
+        assert_eq!(test_res.unwrap().trim(), "hello_world".to_string());
+        Ok(())
+    }
+    #[test]
+    fn test_library_parameter_input_number() -> anyhow::Result<()>{
+        // Create test script
+        let test_content = format!(r#"
+input_params["number"]
+"#);
+        let param_string = r#"{"number":1}"#.to_string();
+        let test_res = eldritch_run("test.tome".to_string(), test_content, Some(param_string));
+        assert_eq!(test_res.unwrap(), "1".to_string());
+        Ok(())
+    }
+
+    #[test]
+    fn test_library_parameter_input_max_u64() -> anyhow::Result<()>{
+        // Create test script
+        let test_content = format!(r#"
+x = input_params["number"] - 1
+x
+"#);
+        let param_string = format!("{{\"number\":{}}}", u64::MAX);
+        let test_res = eldritch_run("test.tome".to_string(), test_content, Some(param_string));
+        assert_eq!(test_res.unwrap(), "2147483646".to_string()); // i32::MAX-1
+        Ok(())
+    }
+
+    #[test]
+    fn test_library_parameter_input_str_array() -> anyhow::Result<()>{
+        // Create test script
+        let test_content = format!(r#"
+input_params
+"#);
+        let param_string = r#"{"list_key":["item1","item2","item3"]}"#.to_string();
+        let test_res = eldritch_run("test.tome".to_string(), test_content, Some(param_string));
+        assert_eq!(test_res.unwrap(), r#"{"list_key": ["item1", "item2", "item3"]}"#);
+        Ok(())
+    }
+
 }
