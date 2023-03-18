e3c18472ff17a1b21d60648d36a1abc0c3d90942
Hulto <7121375+hulto@users.noreply.github.com>
2023-03-18 03:54:11 +0000

    Kinda works. Manage the embedded struct better.

--- implants/eldritch/src/assets/copy_impl.rs
+++ implants/eldritch/src/assets/copy_impl.rs
@@ -1,10 +1,21 @@
-use anyhow::Result;
 use std::fs;
+use anyhow::Result;
+use rust_embed::RustEmbed;
+
+#[derive(RustEmbed)]
+#[folder = "../../tests/embedded_files_test"]
+pub struct Asset;
 
 pub fn copy(src: String, dst: String, ) -> Result<()> {
-    let index_html = Asset::get(src)?;
-    fs::copy(src, dst)?;
-    Ok(())
+    let src_file = match Asset::get(src.as_str()) {
+        Some(local_src_file) => local_src_file.data,
+        None => return Err(anyhow::anyhow!("Embedded file {src} not found.")),
+    };
+    
+    match fs::write(dst, src_file) {
+        Ok(_) => Ok(()),
+        Err(local_err) => Err(local_err.into()),
+    }
 }
 
 
@@ -13,17 +24,10 @@ mod tests {
     use super::*;
     use std::io::prelude::*;
     use tempfile::NamedTempFile;
-    use rust_embed::RustEmbed;
-
-    #[derive(RustEmbed)]
-    #[folder = "../../tests/embedded_files_test"]
-    pub struct Asset;
     
     #[test]
     fn test_embedded_copy() -> anyhow::Result<()>{
-        let my_script = Asset::get("exec_script/hello_word.sh")?;
-
-    
+   
         // Create files
         let mut tmp_file_dst = NamedTempFile::new()?;
         let path_dst = String::from(tmp_file_dst.path().to_str().unwrap());
@@ -36,7 +40,7 @@ mod tests {
         tmp_file_dst.read_to_string(&mut contents)?;
         // Compare
         assert_eq!(contents, r#"#!/bin/sh
-echo "hello from an embedded shell script"#);
+echo "hello from an embedded shell script""#);
 
         Ok(())
     }
