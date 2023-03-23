9c63e50713fe92fce0d45d038d11af109aa5dc99
Hulto <7121375+hulto@users.noreply.github.com>
2023-03-22 21:38:50 -0400

    156 feature golem add embedded files for dropper functionality (#162)
    * Assets compiled in.
    
    * Pivot lib loading and added test for regression.
    
    * eldritch assets.list function
    
    * eldritch assets.copy function
    
    * Consolidated extraneous test files.
    
    * Cleanup

---
+++ implants/eldritch/src/assets/list_impl.rs
@@ -0,0 +1,25 @@
+use anyhow::Result;
+
+pub fn list() -> Result<Vec<String>> {
+    let mut res: Vec<String> = Vec::new();
+    for file_path in super::Asset::iter() {
+        res.push(file_path.to_string());
+    }
+
+    Ok(res)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_assets_list() -> anyhow::Result<()>{
+        let res_all_embedded_files = list()?;
+
+        assert_eq!(res_all_embedded_files, ["exec_script/hello_word.sh", "exec_script/main.eld", "print/main.eld"]);
+
+        Ok(())
+    }
+}
--- implants/eldritch/src/lib.rs
+++ implants/eldritch/src/lib.rs
@@ -2,6 +2,7 @@ pub mod file;
 pub mod process;
 pub mod sys;
 pub mod pivot;
+pub mod assets;
 
 use serde_json::Map;
 use starlark::collections::SmallMap;
@@ -15,6 +16,8 @@ use starlark::values::{Value, AllocValue};
 use file::FileLibrary;
 use process::ProcessLibrary;
 use sys::SysLibrary;
+use assets::AssetsLibrary;
+use pivot::PivotLibrary;
 
 pub fn get_eldritch() -> anyhow::Result<Globals> {
     #[starlark_module]
@@ -22,6 +25,8 @@ pub fn get_eldritch() -> anyhow::Result<Globals> {
         const file: FileLibrary = FileLibrary();
         const process: ProcessLibrary = ProcessLibrary();
         const sys: SysLibrary = SysLibrary();
+        const pivot: PivotLibrary = PivotLibrary();
+        const assets: AssetsLibrary = AssetsLibrary();
     }
 
     let globals = GlobalsBuilder::extended().with(eldritch).build();
@@ -114,35 +119,22 @@ mod tests {
     use std::thread;
 
     use super::*;
-    use starlark::environment::{GlobalsBuilder};
-    use starlark::{starlark_module};
     use starlark::assert::Assert;
     use tempfile::NamedTempFile;
-
-    use super::file::FileLibrary;
-    use super::process::ProcessLibrary;
-    use super::sys::SysLibrary;
-    use super::pivot::PivotLibrary;
 
     // just checks dir...
     #[test]
     fn test_library_bindings() {
-        #[starlark_module]
-        fn globals(builder: &mut GlobalsBuilder) {
-            const file: FileLibrary = FileLibrary();
-            const process: ProcessLibrary = ProcessLibrary();
-            const sys: SysLibrary = SysLibrary();
-            const pivot: PivotLibrary = PivotLibrary();
-        }
-
+        let globals = get_eldritch().unwrap();
         let mut a = Assert::new();
-        a.globals_add(globals);
+        a.globals(globals);
         a.all_true(
             r#"
 dir(file) == ["append", "compress", "copy", "download", "exists", "hash", "is_dir", "is_file", "mkdir", "read", "remove", "rename", "replace", "replace_all", "template", "timestomp", "write"]
 dir(process) == ["kill", "list", "name"]
 dir(sys) == ["dll_inject", "exec", "is_linux", "is_macos", "is_windows", "shell"]
 dir(pivot) == ["arp_scan", "bind_proxy", "ncat", "port_forward", "port_scan", "smb_exec", "ssh_exec", "ssh_password_spray"]
+dir(assets) == ["copy","list"]
 "#,
         );
     }
