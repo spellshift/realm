ce104b812ad3315b2bb75043875eeca7545d7b91
hulto <7121375+hulto@users.noreply.github.com>
2023-04-06 02:26:14 +0000

    Custom printhandler works but can't pass attributes

--- implants/eldritch/src/lib.rs
+++ implants/eldritch/src/lib.rs
@@ -4,9 +4,16 @@ pub mod sys;
 pub mod pivot;
 pub mod assets;
 
+use std::cell::RefCell;
+use std::sync::mpsc::Sender;
+use std::sync::mpsc;
+use std::rc::Rc;
+
+use gazebo::prelude::Dupe;
 use serde_json::Map;
+use starlark::assert::Assert;
 use starlark::collections::SmallMap;
-use starlark::starlark_module;
+use starlark::{starlark_module, PrintHandler};
 use starlark::environment::{GlobalsBuilder, Module, Globals};
 use starlark::syntax::{AstModule, Dialect};
 use starlark::eval::Evaluator;
@@ -33,7 +40,29 @@ pub fn get_eldritch() -> anyhow::Result<Globals> {
     return Ok(globals);
 }
 
-pub fn eldritch_run(tome_filename: String, tome_contents: String, tome_parameters: Option<String>) -> anyhow::Result<String> {
+pub struct EldritchPrintHandler{
+    sender: Sender<String>,
+}
+
+impl PrintHandler for EldritchPrintHandler {
+    fn println(&self, text: &str) -> anyhow::Result<()> {
+        println!("[ELD]: {}", text.to_owned());
+        Ok(())
+    }
+}
+
+pub struct StdPrintHandler {
+}
+
+impl PrintHandler for StdPrintHandler {
+    fn println(&self, text: &str) -> anyhow::Result<()> {
+        println!("[STD]: {}", text.to_owned());
+        Ok(())
+    }
+}
+
+
+pub fn eldritch_run(tome_filename: String, tome_contents: String, tome_parameters: Option<String>, sender: Option<Sender<String>>) -> anyhow::Result<String> {
     // Boilder plate
     let ast: AstModule;
     match AstModule::parse(
@@ -106,6 +135,10 @@ pub fn eldritch_run(tome_filename: String, tome_contents: String, tome_parameter
     module.set("input_params", input_params.alloc_value(module.heap()));
 
     let mut eval: Evaluator = Evaluator::new(&module);
+    let binding = EldritchPrintHandler{ sender: sender.unwrap().clone() };
+    eval.set_print_handler(&binding);
+    // eval.set_print_handler(&StdPrintHandler{});
+
     let res: Value = match eval.eval_module(ast, &globals) {
         Ok(eval_val) => eval_val,
         Err(eval_error) => return Err(anyhow::anyhow!("Eldritch eval_module failed:\n{}", eval_error)),
@@ -116,7 +149,7 @@ pub fn eldritch_run(tome_filename: String, tome_contents: String, tome_parameter
 
 #[cfg(test)]
 mod tests {
-    use std::thread;
+    use std::{thread, sync::mpsc::channel};
 
     use super::*;
     use starlark::assert::Assert;
@@ -146,7 +179,7 @@ dir(assets) == ["copy","list"]
 sys.shell(input_params['cmd2'])
 "#);
         let param_string = r#"{"cmd":"id","cmd2":"echo hello_world","cmd3":"ls -lah /tmp/"}"#.to_string();
-        let test_res = eldritch_run("test.tome".to_string(), test_content, Some(param_string));
+        let test_res = eldritch_run("test.tome".to_string(), test_content, Some(param_string), None);
         assert_eq!(test_res.unwrap().trim(), "hello_world".to_string());
         Ok(())
     }
@@ -157,7 +190,7 @@ sys.shell(input_params['cmd2'])
 input_params["number"]
 "#);
         let param_string = r#"{"number":1}"#.to_string();
-        let test_res = eldritch_run("test.tome".to_string(), test_content, Some(param_string));
+        let test_res = eldritch_run("test.tome".to_string(), test_content, Some(param_string), None);
         assert_eq!(test_res.unwrap(), "1".to_string());
         Ok(())
     }
@@ -170,7 +203,7 @@ x = input_params["number"] - 1
 x
 "#);
         let param_string = format!("{{\"number\":{}}}", u64::MAX);
-        let test_res = eldritch_run("test.tome".to_string(), test_content, Some(param_string));
+        let test_res = eldritch_run("test.tome".to_string(), test_content, Some(param_string), None);
         assert_eq!(test_res.unwrap(), "2147483646".to_string()); // i32::MAX-1
         Ok(())
     }
@@ -182,7 +215,7 @@ x
 input_params
 "#);
         let param_string = r#"{"list_key":["item1","item2","item3"]}"#.to_string();
-        let test_res = eldritch_run("test.tome".to_string(), test_content, Some(param_string));
+        let test_res = eldritch_run("test.tome".to_string(), test_content, Some(param_string), None);
         assert_eq!(test_res.unwrap(), r#"{"list_key": ["item1", "item2", "item3"]}"#);
         Ok(())
     }
@@ -195,11 +228,32 @@ input_params
         let test_content = format!(r#"
 file.download("https://www.google.com/", "{path}")
 "#);
-        let test_res = thread::spawn(|| { eldritch_run("test.tome".to_string(), test_content, None) });
+        let test_res = thread::spawn(|| { eldritch_run("test.tome".to_string(), test_content, None, None) });
         let _test_val = test_res.join();
 
         assert!(tmp_file.as_file().metadata().unwrap().len() > 5);
 
         Ok(())
     }
+
+    #[tokio::test]
+    async fn test_library_custom_print_handler() -> anyhow::Result<()> {
+        // just using a temp file for its path
+        let tmp_file = NamedTempFile::new()?;
+        let path = String::from(tmp_file.path().to_str().unwrap()).clone().replace("\\", "\\\\");
+        let test_content = format!(r#"
+print("Hello")
+print("World")
+print("123")
+"#);
+        let (sender, receiver) = channel::<String>();
+
+        let test_res = thread::spawn(|| { eldritch_run("test.tome".to_string(), test_content, None, Some(sender)) });
+        let _test_val = test_res.join();
+
+        assert!(tmp_file.as_file().metadata().unwrap().len() > 5);
+
+        Ok(())
+    }
+
 }
