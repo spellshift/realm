d2537cde09a7b016dc318eca3463fcd2432fa54e
Hulto <7121375+hulto@users.noreply.github.com>
2023-06-14 22:13:35 -0400

    193 resolve docs issues (#195)
    * Updated docs.
    
    * Added golem to toc.
    
    * Fix typo start to sort by name.
    
    * Add assets docs.
    
    * Unified wording.
    
    * Clean up spelling.

--- implants/eldritch/src/pivot/port_forward_impl.rs
+++ implants/eldritch/src/pivot/port_forward_impl.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
 
-pub fn port_forward(_listen_address: String, _listen_port: i32, _forward_address: String, _forward_port: i32, _portocol: String) -> Result<()> {
+pub fn port_forward(_listen_address: String, _listen_port: i32, _forward_address: String, _forward_port: i32, _protocol: String) -> Result<()> {
     unimplemented!("Method unimplemented")
 }
