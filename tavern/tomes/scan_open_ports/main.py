778d6eb096a9fab4df52feb075eca0dd49924f2c
Hulto <7121375+hulto@users.noreply.github.com>
2023-07-02 18:18:37 -0700

    Removed unused tomes.

--- tavern/tomes/scan_open_ports/main.py
+++
@@ -1,9 +0,0 @@
-def main(target_cidrs):
-    print("Scanning {}".format(target_cidrs))
-    target_ports = [x for x in range(0,10000)]
-    for found_port in pivot.port_scan(target_cidrs, target_ports, "tcp", 2):
-        if found_port['status'] == "open":
-            print(found_port)
-            print(pivot.ncat(found_port['ip'], found_port['port'], "nomnom\nid", "tcp"))
-
-main(["10.10.0.129/32"])
