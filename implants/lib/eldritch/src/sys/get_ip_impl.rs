038ab75e440565b6a70d184fc6ab5dd1f421c1e4
Hulto <7121375+hulto@users.noreply.github.com>
2023-07-12 00:06:35 +0000

    Docs.

--- implants/lib/eldritch/src/sys/get_ip_impl.rs
+++ implants/lib/eldritch/src/sys/get_ip_impl.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use network_interface::{NetworkInterfaceConfig, NetworkInterface};
-use starlark::values::{dict::Dict, Heap};
+use starlark::{values::{dict::Dict, Heap, Value}, collections::SmallMap, const_frozen_string};
 
 const UNKNOWN: &str = "UNKNOWN";
 
@@ -34,8 +34,35 @@ fn handle_get_ip() -> Result<Vec<NetInterface>> {
     Ok(res)
 }
 
-pub fn get_ip<'v>(starlark_heap: &Heap) -> Result<Vec<Dict<'v>>> {
-    todo!();
+fn create_dict_from_interface(starlark_heap: &Heap, interface: NetInterface) -> Result<Dict>{
+    let res: SmallMap<Value, Value> = SmallMap::new();
+    let mut tmp_res = Dict::new(res);
+
+    let tmp_value1 = starlark_heap.alloc_str(&interface.name);
+    tmp_res.insert_hashed(const_frozen_string!("name").to_value().get_hashed().unwrap(), tmp_value1.to_value());
+
+    let mut tmp_value2_arr = Vec::<Value>::new();
+    for ip in interface.ips {
+        tmp_value2_arr.push(starlark_heap.alloc_str(&ip.to_string()).to_value());
+    }
+    let tmp_value2 = starlark_heap.alloc(tmp_value2_arr);
+    tmp_res.insert_hashed(const_frozen_string!("ips").to_value().get_hashed().unwrap(), tmp_value2);
+
+    let tmp_value3 = starlark_heap.alloc_str(&interface.mac);
+    tmp_res.insert_hashed(const_frozen_string!("mac").to_value().get_hashed().unwrap(), tmp_value3.to_value());
+
+
+    Ok(tmp_res)
+}
+
+
+pub fn get_ip(starlark_heap: &Heap) -> Result<Vec<Dict>> {
+    let mut final_res: Vec<Dict> = Vec::new();
+    for network_interface in handle_get_ip()? {
+        let tmp_res = create_dict_from_interface(starlark_heap, network_interface)?;
+        final_res.push(tmp_res);
+    }
+    Ok(final_res)
 }
 
 #[cfg(test)]
@@ -46,11 +73,9 @@ mod tests {
 
     #[test]
     fn test_sys_get_ip() {
-        let res = handle_get_ip().unwrap();
-        for interface in res {
-            if interface.name == "lo" {
-                assert!(interface.ips.contains(&IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1))));
-            }
-        }
+        let starlark_heap = Heap::new();
+        let res = get_ip(&starlark_heap).unwrap();
+        assert!(format!("{:?}", res).contains("127.0.0.1"));
+        assert!(!format!("{:?}", res).contains(UNKNOWN));
     }
 }
