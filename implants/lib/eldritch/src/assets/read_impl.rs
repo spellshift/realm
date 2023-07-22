24a7a0891566e4d35f79f3d28a0b3371c48b20cd
Hulto <7121375+hulto@users.noreply.github.com>
2023-07-22 17:55:13 -0400

    223 implement assetsread and assetsread bytes (#225)
    * Impl, test, and docs.
    
    * read binary impl, test, and docs.
    
    * update lib test.
    
    * Fix test?
    
    * Might be faster.

---
+++ implants/lib/eldritch/src/assets/read_impl.rs
@@ -0,0 +1,26 @@
+use anyhow::Result;
+
+pub fn read(src: String) -> Result<String> {
+    let src_file_bytes = match super::Asset::get(src.as_str()) {
+        Some(local_src_file) => local_src_file.data,
+        None => return Err(anyhow::anyhow!("Embedded file {src} not found.")),
+    };
+    let mut result = String::new();
+    for byte in src_file_bytes.iter() {
+        result.push(*byte as char);
+    }
+    Ok(result)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_assets_read() -> anyhow::Result<()>{
+        let res = read("print/main.eld".to_string())?;
+        assert_eq!(res, r#"print("This script just prints")"#);
+        Ok(())
+    }
+}
