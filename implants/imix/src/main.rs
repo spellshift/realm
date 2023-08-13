8eb0c2fbf00a44c8fa9892f124fbbe9aac7e939e
hulto <7121375+hulto@users.noreply.github.com>
2023-08-13 02:39:39 +0000

    Fix field accessor.

--- implants/imix/src/main.rs
+++ implants/imix/src/main.rs
@@ -454,7 +454,7 @@ mod tests {
                     description: "Execute a command in the default system shell".to_string(),
                     eldritch: r#"
 print("custom_print_handler_test")
-sys.shell(input_params["cmd"]).stdout
+sys.shell(input_params["cmd"]).stdout()
 "#.to_string(),
                     files: None,
                     param_defs: Some(r#"{"params":[{"name":"cmd","type":"string"}]}"#.to_string()),
