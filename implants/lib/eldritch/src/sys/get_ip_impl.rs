ede6b22bb2e2c2fb9ff2b3000d0c7a76682e88c0
Joe Abbate <40615740+jabbate19@users.noreply.github.com>
2023-08-14 20:38:47 -0700

    Differentiate IP Crate for Windows and Nix (#273)
    * Differentiate for Windows and Nix
    
    * Conditional Deps
    
    * Fix Cargo order

--- implants/lib/eldritch/src/sys/get_ip_impl.rs
+++ implants/lib/eldritch/src/sys/get_ip_impl.rs
@@ -1,16 +1,21 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
+#[cfg(target_os = "windows")]
 use network_interface::{NetworkInterfaceConfig, NetworkInterface};
+#[cfg(not(target_os = "windows"))]
+use pnet::datalink::{interfaces, NetworkInterface};
 use starlark::{values::{dict::Dict, Heap, Value}, collections::SmallMap, const_frozen_string};
 
 const UNKNOWN: &str = "UNKNOWN";
 
 #[derive(Debug)]
+#[cfg(target_os = "windows")]
 struct NetInterface {
     name: String,
     ips: Vec<std::net::IpAddr>, //IPv6 and IPv4 Addresses on the itnerface
     mac: String,
 }
 
+#[cfg(target_os = "windows")]
 fn handle_get_ip() -> Result<Vec<NetInterface>> {
     let mut res = Vec::new();
     for network_interface in NetworkInterface::show()? {
@@ -34,6 +39,12 @@ fn handle_get_ip() -> Result<Vec<NetInterface>> {
     Ok(res)
 }
 
+#[cfg(not(target_os = "windows"))]
+fn handle_get_ip() -> Result<Vec<NetworkInterface>> {
+    Ok(interfaces())
+}
+
+#[cfg(target_os = "windows")]
 fn create_dict_from_interface(starlark_heap: &Heap, interface: NetInterface) -> Result<Dict>{
     let res: SmallMap<Value, Value> = SmallMap::new();
     let mut tmp_res = Dict::new(res);
@@ -55,6 +66,27 @@ fn create_dict_from_interface(starlark_heap: &Heap, interface: NetInterface) -> 
     Ok(tmp_res)
 }
 
+#[cfg(not(target_os = "windows"))]
+fn create_dict_from_interface(starlark_heap: &Heap, interface: NetworkInterface) -> Result<Dict>{
+    let res: SmallMap<Value, Value> = SmallMap::new();
+    let mut tmp_res = Dict::new(res);
+
+    let tmp_value1 = starlark_heap.alloc_str(&interface.name);
+    tmp_res.insert_hashed(const_frozen_string!("name").to_value().get_hashed().unwrap(), tmp_value1.to_value());
+
+    let mut tmp_value2_arr = Vec::<Value>::new();
+    for ip in interface.ips {
+        tmp_value2_arr.push(starlark_heap.alloc_str(&ip.to_string()).to_value());
+    }
+    let tmp_value2 = starlark_heap.alloc(tmp_value2_arr);
+    tmp_res.insert_hashed(const_frozen_string!("ips").to_value().get_hashed().unwrap(), tmp_value2);
+
+    let tmp_value3 = starlark_heap.alloc_str(&interface.mac.map(|mac| mac.to_string()).unwrap_or(UNKNOWN.to_string()));
+    tmp_res.insert_hashed(const_frozen_string!("mac").to_value().get_hashed().unwrap(), tmp_value3.to_value());
+
+
+    Ok(tmp_res)
+}
 
 pub fn get_ip(starlark_heap: &Heap) -> Result<Vec<Dict>> {
     let mut final_res: Vec<Dict> = Vec::new();
