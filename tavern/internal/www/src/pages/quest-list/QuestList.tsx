1964ac84d96c0efb0807694db5d6c789e020ce1d
Squidli <catie@squidli.com>
2023-10-11 03:13:15 +0000

    feat(sorting): Add sorting to tables

--- tavern/internal/www/src/pages/quest-list/QuestList.tsx
+++ tavern/internal/www/src/pages/quest-list/QuestList.tsx
@@ -1,58 +1,13 @@
-import { gql, useQuery } from "@apollo/client";
-import React from "react";
+import { useQuery } from "@apollo/client";
 import { Link } from "react-router-dom";
 
-import { CreateQuestDrawer } from "../../components/create-quest-drawer/CreateQuestDrawer";
-import { FormSteps } from "../../components/form-steps";
 import { PageWrapper } from "../../components/page-wrapper";
-import { QuestProps, Task } from "../../utils/consts";
 import { GET_QUEST_QUERY } from "../../utils/queries";
 import { QuestTable } from "./quest-table";
 
 export const QuestList = () => {
-
-    const { loading, error, data } = useQuery(GET_QUEST_QUERY);
-
-    const questsTableData = data?.quests?.map( (quest: QuestProps) => {
-
-        const taskDetails = quest.tasks.reduce( (map:any, task: Task)=> {
-            const modMap = {...map};
-
-            if(task.execFinishedAt){
-                modMap.finished += 1;
-            }
-            else if(task.execStartedAt){
-                modMap.inprogress += 1;
-            }
-            else{
-                modMap.queued += 1;
-            }
-
-            if(new Date(task.lastModifiedAt) > new Date(modMap.lastUpdated) ){
-                modMap.lastUpdated = task.lastModifiedAt;
-            }
-
-            if(task.output != ""){
-                modMap.outputCount += 1;
-            }
-
-            return modMap
-        },
-            {
-            finished: 0,
-            inprogress: 0,
-            queued: 0,
-            outputCount: 0,
-            lastUpdated: null
-            }
-        );
-
-        return {
-            id: quest.id,
-            name: quest.name,
-            ...taskDetails
-        }
-    });
+    const { loading, data } = useQuery(GET_QUEST_QUERY);
+    console.log(loading);
 
     return (
         <PageWrapper>
@@ -69,9 +24,15 @@ export const QuestList = () => {
                     </Link>
                 </div>
             </div>
-            <div>
+            <div className="flex flex-col justify-center items-center">
+                {loading && 
+                    <div className="py-2 px-4 ">
+                        <svg className="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24"/>
+                            loading...
+                    </div>
+                }
                 {data?.quests?.length > 0 &&
-                    <QuestTable quests={questsTableData} />
+                    <QuestTable quests={data?.quests || []} />
                 }
             </div>
         </PageWrapper>
