027ae7625f3f8e16868d53cea55e9c21eaba9b21
hulto <7121375+hulto@users.noreply.github.com>
2023-10-20 17:32:02 -0700

    Build with core std.

--- bin/reflective_loader/src/lib.rs
+++ bin/reflective_loader/src/lib.rs
@@ -1,15 +1,19 @@
 #![cfg_attr(not(test), no_std)]
 #![cfg_attr(not(test), no_main)]
-// #![no_std]
-// #![no_main]
+
 use core::ffi::c_void;
 use windows_sys::Win32::Foundation::HINSTANCE;
+
 mod loader; 
 
 type DWORD = i32;
 type LPVOID = *mut c_void;
 type BOOL = i32;
 const TRUE: i32 = 1;
+
+// #[allow(non_upper_case_globals)]
+// #[export_name = "_fltused"]
+// pub static _fltused: i32 = 0;
 
 #[cfg(not(test))]
 #[panic_handler]
