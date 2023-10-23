d2b7767dd0d2bb65eefe0629629e25dec4a4980f
adm1npanda <dnc295@nyu.edu>
2023-10-23 19:21:20 -0400

    added implemented Process.Name function in Eldritch
    	modified:   docs/_docs/user-guide/eldritch.md
    	modified:   implants/lib/eldritch/src/process/name_impl.rs

--- implants/lib/eldritch/src/process/name_impl.rs
+++ implants/lib/eldritch/src/process/name_impl.rs
@@ -1,5 +1,67 @@
 use anyhow::Result;
+use sysinfo::{Pid, PidExt, ProcessExt, System, SystemExt};
 
 pub fn name(_pid: i32) -> Result<String> {
-    unimplemented!("Method unimplemented")
+    if !System::IS_SUPPORTED {
+        return Err(anyhow::anyhow!("This OS isn't supported for process functions.
+         Pleases see sysinfo docs for a full list of supported systems.
+         https://docs.rs/sysinfo/0.23.5/sysinfo/index.html#supported-oses\n\n"));
+    }
+
+    let mut sys = System::new();
+    sys.refresh_processes();
+
+    let mut res = "";
+
+    if let Some(process) = sys.process(Pid::from_u32(_pid as u32)) {
+        res = process.name()
+    }
+    
+    Ok(res.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{process::Command};
+
+    #[test]
+    fn test_process_name() -> anyhow::Result<()>{
+        let mut commandstring = "sleep 5";
+        if cfg!(target_os = "linux") || 
+                cfg!(target_os = "ios") || 
+                cfg!(target_os = "macos") || 
+                cfg!(target_os = "android") || 
+                cfg!(target_os = "freebsd") || 
+                cfg!(target_os = "openbsd") ||
+                cfg!(target_os = "netbsd") {
+            commandstring = "sleep";
+        } else if cfg!(target_os = "windows") {
+            commandstring = "timeout";
+        }
+        
+        let child = Command::new(commandstring)
+            .arg("120")
+            .spawn()?;
+
+        let mut sys = System::new();
+        sys.refresh_processes();    
+        for (pid, _) in sys.processes() {
+            if pid.as_u32() == child.id(){
+                
+                
+                assert_eq!(true, true)
+            }
+        }
+        let mut sys = System::new();
+        sys.refresh_processes();    
+        for (pid, process) in sys.processes() {
+            if pid.as_u32() == child.id() {
+                let i32_pid = pid.as_u32() as i32;
+                    let pname = name(i32_pid)?;
+                    assert_eq!(process.name().to_string(), pname)
+            }
+        }
+        return Ok(())
+    }
 }
