b5ee60a90248444c81977d442c5b3edc02f86ed6
adm1npanda <dnc295@nyu.edu>
2023-10-23 21:53:55 -0400

    fix coding style, and remove redundant code

--- implants/lib/eldritch/src/process/name_impl.rs
+++ implants/lib/eldritch/src/process/name_impl.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use sysinfo::{Pid, PidExt, ProcessExt, System, SystemExt};
 
-pub fn name(_pid: i32) -> Result<String> {
+pub fn name(pid: i32) -> Result<String> {
     if !System::IS_SUPPORTED {
         return Err(anyhow::anyhow!("This OS isn't supported for process functions.
          Pleases see sysinfo docs for a full list of supported systems.
@@ -13,7 +13,7 @@ pub fn name(_pid: i32) -> Result<String> {
 
     let mut res = "";
 
-    if let Some(process) = sys.process(Pid::from_u32(_pid as u32)) {
+    if let Some(process) = sys.process(Pid::from_u32(pid as u32)) {
         res = process.name()
     }
     
@@ -27,7 +27,7 @@ mod tests {
 
     #[test]
     fn test_process_name() -> anyhow::Result<()>{
-        let mut commandstring = "sleep 5";
+        let mut commandstring = "";
         if cfg!(target_os = "linux") || 
                 cfg!(target_os = "ios") || 
                 cfg!(target_os = "macos") || 
@@ -38,21 +38,14 @@ mod tests {
             commandstring = "sleep";
         } else if cfg!(target_os = "windows") {
             commandstring = "timeout";
+        } else {
+            return Err(anyhow::anyhow!("OS Not supported please re run on Linux, Windows, or MacOS"));
         }
         
         let child = Command::new(commandstring)
-            .arg("120")
+            .arg("4")
             .spawn()?;
 
-        let mut sys = System::new();
-        sys.refresh_processes();    
-        for (pid, _) in sys.processes() {
-            if pid.as_u32() == child.id(){
-                
-                
-                assert_eq!(true, true)
-            }
-        }
         let mut sys = System::new();
         sys.refresh_processes();    
         for (pid, process) in sys.processes() {
