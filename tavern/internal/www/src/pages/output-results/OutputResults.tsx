6849155e876ba8c6ecfbcc731e9f7437adbc21a6
Squidli <32434695+cmp5987@users.noreply.github.com>
2023-10-28 23:16:32 -0400

    Cp/rename nav items (#336)
    * fix(nav): Change menu nav items

--- tavern/internal/www/src/pages/output-results/OutputResults.tsx
+++ tavern/internal/www/src/pages/output-results/OutputResults.tsx
@@ -27,7 +27,7 @@ export const OutputResults = () => {
     return (
         <PageWrapper currNavItem={PageNavItem.results}>
             <div className="border-b border-gray-200 pb-5 sm:flex sm:items-center sm:justify-between">
-                <h3 className="text-xl font-semibold leading-6 text-gray-900">Results from quests </h3>
+                <h3 className="text-xl font-semibold leading-6 text-gray-900">Quest ouputs </h3>
             </div>
             <div className="flex flex-col justify-center items-center">
                 {formattedOutputLoading ?
