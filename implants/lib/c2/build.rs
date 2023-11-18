d315fdf268384e7770ea42b27b60339101a1f720
KCarretto <Kcarretto@gmail.com>
2023-11-18 04:10:51 +0000

    log warning if protos aren't compiled

--- implants/lib/c2/build.rs
+++ implants/lib/c2/build.rs
@@ -1,7 +1,11 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // tonic_build::configure()
-    //     .out_dir("./src")
-    //     .build_server(false)
-    //     .compile(&["c2.proto"], &["../../../tavern/internal/c2/"])?;
+    match tonic_build::configure()
+        .out_dir("./src")
+        .build_server(false)
+        .compile(&["c2.proto"], &["../../../tavern/internal/c2/"])
+    {
+        Err(err) => println!("WARNING: Failed to compile protos: {}", err),
+        Ok(_) => println!("Generating protos"),
+    }
     Ok(())
 }
