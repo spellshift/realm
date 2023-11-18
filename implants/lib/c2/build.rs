5c3ba784d9a53ef6ae1b54a9a55cb0d4f3d53889
KCarretto <Kcarretto@gmail.com>
2023-11-18 04:24:57 +0000

    Fix build.rs

--- implants/lib/c2/build.rs
+++ implants/lib/c2/build.rs
@@ -1,10 +1,27 @@
+use std::env;
+use std::path::PathBuf;
+use which::which;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    match env::var_os("PROTOC")
+        .map(PathBuf::from)
+        .or_else(|| which("protoc").ok())
+    {
+        Some(_) => println!("Found protoc, protos will be generated"),
+        None => {
+            println!("WARNING: Failed to locate protoc, protos will not be generated");
+            return Ok(());
+        }
+    }
+
     match tonic_build::configure()
         .out_dir("./src")
         .build_server(false)
         .compile(&["c2.proto"], &["../../../tavern/internal/c2/"])
     {
-        Err(err) => println!("WARNING: Failed to compile protos: {}", err),
+        Err(err) => {
+            println!("WARNING: Failed to compile protos: {}", err);
+        }
         Ok(_) => println!("Generating protos"),
     }
     Ok(())
