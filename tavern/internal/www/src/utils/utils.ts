22341110d70ce414aec5de103d170a28789b3a18
Squidli <catie@squidli.com>
2024-01-02 00:40:11 +0000

    fix(buffer): Add interval buffer

--- tavern/internal/www/src/utils/utils.ts
+++ tavern/internal/www/src/utils/utils.ts
@@ -1,3 +1,6 @@
+import {add} from "date-fns";
+import { BeaconType } from "./consts";
+
 export const safelyJsonParse = (value: string) => {
     let error = false;
     let params = [];
@@ -35,3 +38,23 @@ export function getFilterNameByTypes(typeFilters: Array<any>){
         "platform": []
     });
 };
+
+export function getOnlineBeacons(beacons: Array<BeaconType>) : Array<BeaconType>{
+    const minIntervalBuffer = 60;
+    const currentDate = new Date();
+    return beacons.filter((beacon: BeaconType)=> add(new Date(beacon.lastSeenAt),{seconds: beacon.interval + minIntervalBuffer}) >= currentDate);
+}
+export function checkIfBeaconOnline(beacon: BeaconType) : boolean{
+    const minIntervalBuffer = 60;
+    const currentDate = new Date();
+    return add(new Date(beacon.lastSeenAt),{seconds: beacon.interval + minIntervalBuffer}) >= currentDate;
+}
+
+export function isBeaconSelected(selectedBeacons: any): boolean{
+    for (let key in selectedBeacons) {
+        if (selectedBeacons[key] === true) {
+            return true;
+        }
+    }
+    return false;
+}
