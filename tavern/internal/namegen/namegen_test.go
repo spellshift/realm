956e7872c0c7894d2833c54e969d13119d08afe6
ALEXANDER MA COTE <alexcote1rocks@gmail.com>
2024-01-20 21:11:35 -0500

    add unique name selection and opportunistically generate lower comple… (#402)
    * add unique name selection and opportunistically generate lower complexity names
    
    * fix tests
    
    * batch search for collisions. and add a complexity identifier
    
    * add test for Beaconnameinstring
    
    * add tests to other random names
    
    * add requested changes.
    
    * make duplicate test use complex names
    
    * impliment one retry per request to generate a name
    
    * rename the complexity levels
    
    * seperate them out into diffrent functions, removing the enum
    
    * fix imports
    
    * add ent to namegen test
    
    ---------
    
    Co-authored-by: Hulto <7121375+hulto@users.noreply.github.com>
    Co-authored-by: KCarretto <Kcarretto@gmail.com>

--- tavern/internal/namegen/namegen_test.go
+++ tavern/internal/namegen/namegen_test.go
@@ -2,16 +2,20 @@ package namegen_test
 
 import (
 	"testing"
-
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"realm.pub/tavern/internal/namegen"
+	"realm.pub/tavern/internal/ent"
 )
 
 func TestGetRandomName(t *testing.T) {
 	t.Run("BasicName", func(t *testing.T) {
-		name := namegen.GetRandomName()
-		assert.NotEmpty(t, name)
+		name1 := namegen.GetRandomNameSimple()
+		assert.NotEmpty(t, name1)
+		name2 := namegen.GetRandomNameModerate()
+		assert.NotEmpty(t, name2)
+		name3 := namegen.GetRandomNameComplex()
+		assert.NotEmpty(t, name3)
 	})
 
 	t.Run("NoDuplicates", func(t *testing.T) {
@@ -19,7 +23,7 @@ func TestGetRandomName(t *testing.T) {
 		names := make(map[string]bool, 1000000)
 		count := 0
 		for i := 0; i < 1000000; i++ {
-			name := namegen.GetRandomName()
+			name := namegen.GetRandomNameComplex()
 			exists, ok := names[name]
 			require.False(t, ok, "Name %s already exists - after %d attempts", name, count)
 			assert.False(t, exists)
@@ -28,4 +32,42 @@ func TestGetRandomName(t *testing.T) {
 		}
 	})
 
+}
+
+// TestBeaconnameinstring tests the Beaconnameinstring function
+func TestBeaconnameinstring(t *testing.T) {
+	testCases := []struct {
+		name     string
+		beacons  []*ent.Beacon
+		str      string
+		expected bool
+	}{
+		{
+			name: "String matches a beacon name",
+			beacons: []*ent.Beacon{
+				{Name: "Alpha"},
+				{Name: "Beta"},
+			},
+			str:      "Beta",
+			expected: true,
+		},
+		{
+			name: "String does not match any beacon name",
+			beacons: []*ent.Beacon{
+				{Name: "Alpha"},
+				{Name: "Beta"},
+			},
+			str:      "Gamma",
+			expected: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := namegen.IsCollision(tc.beacons, tc.str)
+			if result != tc.expected {
+				t.Errorf("Beaconnameinstring(%v, %s) = %v; expected %v", tc.beacons, tc.str, result, tc.expected)
+			}
+		})
+	}
 }
