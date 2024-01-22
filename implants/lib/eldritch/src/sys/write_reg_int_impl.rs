70aece63a6740a6724dada8726ffa0568495799d
Nicholas O'Brien <ndo9903@rit.edu>
2024-01-22 03:00:20 +0000

    fixed all the problems

--- implants/lib/eldritch/src/sys/write_reg_int_impl.rs
+++ implants/lib/eldritch/src/sys/write_reg_int_impl.rs
@@ -1,11 +1,11 @@
 use anyhow::Result;
 
 pub fn write_reg_int(
-    reghive: String,
-    regpath: String,
-    regname: String,
-    regtype: String,
-    regvalue: u32,
+    _reghive: String,
+    _regpath: String,
+    _regname: String,
+    _regtype: String,
+    _regvalue: u32,
 ) -> Result<bool> {
     #[cfg(not(target_os = "windows"))]
     return Err(anyhow::anyhow!(
@@ -18,7 +18,7 @@ pub fn write_reg_int(
 
         //Accepted values for reghive :
         //HKEY_CLASSES_ROOT, HKEY_CURRENT_USER, HKEY_LOCAL_MACHINE, HKEY_USERS, HKEY_PERFORMANCE_DATA, HKEY_PERFORMANCE_TEXT, HKEY_PERFORMANCE_NLSTEXT, HKEY_CURRENT_CONFIG, HKEY_DYN_DATA, HKEY_CURRENT_USER_LOCAL_SETTINGS
-        let ihive: isize = match reghive.as_ref() {
+        let ihive: isize = match _reghive.as_ref() {
             "HKEY_CLASSES_ROOT" => HKEY_CLASSES_ROOT,
             "HKEY_CURRENT_USER" => HKEY_CURRENT_USER,
             "HKEY_LOCAL_MACHINE" => HKEY_LOCAL_MACHINE,
@@ -34,47 +34,47 @@ pub fn write_reg_int(
         };
 
         let hive = RegKey::predef(ihive);
-        let (nkey, _ndisp) = hive.create_subkey(regpath)?;
+        let (nkey, _ndisp) = hive.create_subkey(_regpath)?;
 
-        match regtype.as_ref() {
+        match _regtype.as_ref() {
             "REG_NONE" => {
-                nkey.set_value(regname, &regvalue)?;
+                nkey.set_value(_regname, &_regvalue)?;
             },
-            "REG_SZ" => nkey.set_value(regname, &regvalue)?,
-            "REG_EXPAND_SZ" => nkey.set_value(regname, &regvalue)?,
+            "REG_SZ" => nkey.set_value(_regname, &_regvalue)?,
+            "REG_EXPAND_SZ" => nkey.set_value(_regname, &_regvalue)?,
             "REG_BINARY" => {
-                let data = RegValue{ vtype: REG_BINARY, bytes: regvalue.to_le_bytes().to_vec()};
-                nkey.set_raw_value(regname, &data)?;
+                let data = RegValue{ vtype: REG_BINARY, bytes: _regvalue.to_le_bytes().to_vec()};
+                nkey.set_raw_value(_regname, &data)?;
             },
             "REG_DWORD" => {
-                let data = RegValue{ vtype: REG_DWORD, bytes: regvalue.to_le_bytes().to_vec()};
-                nkey.set_raw_value(regname, &data)?;
+                let data = RegValue{ vtype: REG_DWORD, bytes: _regvalue.to_le_bytes().to_vec()};
+                nkey.set_raw_value(_regname, &data)?;
             },
             "REG_DWORD_BIG_ENDIAN" => {
-                let data = RegValue{ vtype: REG_DWORD_BIG_ENDIAN, bytes: regvalue.to_be_bytes().to_vec()};
-                nkey.set_raw_value(regname, &data)?;
+                let data = RegValue{ vtype: REG_DWORD_BIG_ENDIAN, bytes: _regvalue.to_be_bytes().to_vec()};
+                nkey.set_raw_value(_regname, &data)?;
             },
             "REG_LINK" => {
-                nkey.set_value(regname, &regvalue)?;
+                nkey.set_value(_regname, &_regvalue)?;
             },
             "REG_MULTI_SZ" => {
-                nkey.set_value(regname, &regvalue)?;
+                nkey.set_value(_regname, &_regvalue)?;
             },
             "REG_RESOURCE_LIST" => {
-                let data = RegValue{ vtype: REG_RESOURCE_LIST, bytes: regvalue.to_le_bytes().to_vec()};
-                nkey.set_raw_value(regname, &data)?;
+                let data = RegValue{ vtype: REG_RESOURCE_LIST, bytes: _regvalue.to_le_bytes().to_vec()};
+                nkey.set_raw_value(_regname, &data)?;
             },
             "REG_FULL_RESOURCE_DESCRIPTOR" => {
-                let data = RegValue{ vtype: REG_FULL_RESOURCE_DESCRIPTOR, bytes: regvalue.to_le_bytes().to_vec()};
-                nkey.set_raw_value(regname, &data)?;
+                let data = RegValue{ vtype: REG_FULL_RESOURCE_DESCRIPTOR, bytes: _regvalue.to_le_bytes().to_vec()};
+                nkey.set_raw_value(_regname, &data)?;
             },
             "REG_RESOURCE_REQUIREMENTS_LIST" => {
-                let data = RegValue{ vtype: REG_RESOURCE_REQUIREMENTS_LIST, bytes: regvalue.to_le_bytes().to_vec()};
-                nkey.set_raw_value(regname, &data)?;
+                let data = RegValue{ vtype: REG_RESOURCE_REQUIREMENTS_LIST, bytes: _regvalue.to_le_bytes().to_vec()};
+                nkey.set_raw_value(_regname, &data)?;
             },
             "REG_QWORD" => {
-                let data = RegValue{ vtype: REG_QWORD, bytes: regvalue.to_le_bytes().to_vec()};
-                nkey.set_raw_value(regname, &data)?;
+                let data = RegValue{ vtype: REG_QWORD, bytes: _regvalue.to_le_bytes().to_vec()};
+                nkey.set_raw_value(_regname, &data)?;
             },
             _ => return Err(anyhow::anyhow!("RegType can only be one of the following values - REG_NONE, REG_SZ, REG_EXPAND_SZ, REG_BINARY, REG_DWORD, REG_DWORD_BIG_ENDIAN, REG_LINK, REG_MULTI_SZ, REG_RESOURCE_LIST, REG_RESOURCE_LIST, REG_FULL_RESOURCE_DESCRIPTOR, REG_QWORD. ")),
         };
@@ -85,15 +85,16 @@ pub fn write_reg_int(
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use std::str;
-    use uuid::Uuid;
 
     #[test]
     fn test_write_reg_int() -> anyhow::Result<()> {
         #[cfg(target_os = "windows")]
         {
+            use super::*;
+            use std::str;
+            use uuid::Uuid;
             use winreg::{enums::*, RegKey};
+
             let id = Uuid::new_v4();
 
             // -------------------- WRITE_REG_INT TESTS ---------------------------------------
