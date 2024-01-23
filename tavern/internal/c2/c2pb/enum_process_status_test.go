915426c975c45d98d8c565aef055856175177347
KCarretto <Kcarretto@gmail.com>
2024-01-23 18:13:32 -0500

    Added some tests for the proto enums (#499)

--- tavern/internal/c2/c2pb/enum_process_status_test.go
+++ tavern/internal/c2/c2pb/enum_process_status_test.go
@@ -1,11 +1,35 @@
 package c2pb_test
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"realm.pub/tavern/internal/c2/c2pb"
 )
+
+func TestProcessStatusValues(t *testing.T) {
+	assert.NotEmpty(t, c2pb.Process_Status(0).Values())
+}
+
+func TestProcessStatusValue(t *testing.T) {
+	val, err := c2pb.Process_Status(0).Value()
+	require.NoError(t, err)
+	require.NotNil(t, val)
+}
+
+func TestProcessStatusMarshalGraphQL(t *testing.T) {
+	var buf bytes.Buffer
+	c2pb.Process_Status(0).MarshalGQL(&buf)
+	assert.Equal(t, `"STATUS_UNSPECIFIED"`, buf.String())
+}
+
+func TestProcessStatusUnmarshalGraphQL(t *testing.T) {
+	var status c2pb.Process_Status
+	assert.NoError(t, (*c2pb.Process_Status).UnmarshalGQL(&status, `STATUS_IDLE`))
+	assert.Equal(t, c2pb.Process_STATUS_IDLE, status)
+}
 
 func TestProcessStatusScan(t *testing.T) {
 	tests := []struct {
