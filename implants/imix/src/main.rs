2cbebd15b3e0789cde028fd1965f24e4663f40ea
hulto <7121375+hulto@users.noreply.github.com>
2024-02-16 20:41:09 -0800

    Lil cleanup

--- implants/imix/src/main.rs
+++ implants/imix/src/main.rs
@@ -3,10 +3,20 @@
 #[macro_use]
 extern crate windows_service;
 
-use imix::{handle_main,win_service::handle_service_main};
+use imix::handle_main;
 
-#[cfg(feature = "win_service")]
-use windows_service::service_dispatcher;
+
+// ============= Standard ===============
+
+#[cfg(not(feature = "win_service"))]
+#[tokio::main(flavor = "multi_thread", worker_threads = 128)]
+async fn main() {
+
+    handle_main().await
+}
+
+
+// ============ Windows Service =============
 
 #[cfg(all(feature = "win_service", not(target_os = "windows")))]
 compile_error!("Feature win_service is only available on windows targets");
@@ -14,17 +24,18 @@ compile_error!("Feature win_service is only available on windows targets");
 #[cfg(feature = "win_service")]
 define_windows_service!(ffi_service_main, service_main);
 
-#[tokio::main(flavor = "multi_thread", worker_threads = 128)]
-async fn main() {
-    #[cfg(feature = "win_service")]
+#[cfg(feature = "win_service")]
+fn main() {
+    use windows_service::service_dispatcher;
     service_dispatcher::start("imix", ffi_service_main).unwrap();
-    handle_main().await
 }
 
 #[cfg(feature = "win_service")]
 #[tokio::main(flavor = "multi_thread", worker_threads = 128)]
 async fn service_main(arguments: Vec<std::ffi::OsString>) {
-    imix::win_service::handle_service_main(arguments);
+    use imix::win_service::handle_service_main;
+
+    handle_service_main(arguments);
 
     handle_main().await;
 }
