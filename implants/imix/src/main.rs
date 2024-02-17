9c77057e2136d69cda5c3a4562dbf077b7be16d4
hulto <7121375+hulto@users.noreply.github.com>
2024-02-17 09:01:49 -0800

    Allow imix to service exes to be run as regular exes

--- implants/imix/src/main.rs
+++ implants/imix/src/main.rs
@@ -3,14 +3,24 @@
 #[macro_use]
 extern crate windows_service;
 
-use imix::handle_main;
+use imix::{handle_main};
 
 
 // ============= Standard ===============
 
-#[cfg(not(feature = "win_service"))]
 #[tokio::main(flavor = "multi_thread", worker_threads = 128)]
 async fn main() {
+    #[cfg(debug_assertions)]
+    imix::init_logging();
+
+    #[cfg(feature = "win_service")]
+    match windows_service::service_dispatcher::start("imix", ffi_service_main) {
+        Ok(_) => {},
+        Err(_err) => {
+            #[cfg(debug_assertions)]
+            log::error!("Failed to start service (running as exe?): {_err}");
+        },
+    }    
 
     handle_main().await
 }
@@ -25,18 +35,9 @@ compile_error!("Feature win_service is only available on windows targets");
 define_windows_service!(ffi_service_main, service_main);
 
 #[cfg(feature = "win_service")]
-fn main() {
-    use windows_service::service_dispatcher;
-    service_dispatcher::start("imix", ffi_service_main).unwrap();
-}
-
-#[cfg(feature = "win_service")]
 #[tokio::main(flavor = "multi_thread", worker_threads = 128)]
 async fn service_main(arguments: Vec<std::ffi::OsString>) {
-    use imix::win_service::handle_service_main;
-
-    handle_service_main(arguments);
-
+    imix::win_service::handle_service_main(arguments);
     handle_main().await;
 }
 
