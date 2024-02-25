43346fea179cdce864bbe47e1082931ee939bb6c
cmp5987 <32434695+cmp5987@users.noreply.github.com>
2024-02-25 21:31:57 +0000

    feat(import-repo): Add import repository workflow

---
+++ tavern/internal/www/src/components/tavern-base-ui/Modal.tsx
@@ -0,0 +1,59 @@
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import React, { FC, Fragment } from "react";
+import { Dialog, Transition } from '@headlessui/react';
+
+type ModalProps = {
+    isOpen: boolean,
+    setOpen: (arg: any) => any,
+    children: React.ReactElement
+}
+
+const Modal: FC<ModalProps> = ({ isOpen, setOpen, children }) => {
+    return (
+        <Transition.Root show={isOpen} as={Fragment}>
+            <Dialog as="div" className="relative z-10" onClose={setOpen}>
+                <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+                <div className="fixed inset-0 overflow-hidden">
+                    <div className="absolute inset-0 overflow-hidden">
+                        <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-4 ">
+                            <Transition.Child
+                                as={Fragment}
+                                enter="transform transition ease-in-out duration-500 sm:duration-700"
+                                enterFrom="translate-x-full"
+                                enterTo="translate-x-0"
+                                leave="transform transition ease-in-out duration-500 sm:duration-700"
+                                leaveFrom="translate-x-0"
+                                leaveTo="translate-x-full"
+                            >
+                                <Dialog.Panel className="pointer-events-auto w-screen max-w-xs md:max-w-md lg:max-w-4xl">
+                                    <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
+                                        <div className="px-4 sm:px-6">
+                                            <div className="flex w-full justify-end">
+
+                                                <div className="ml-3 flex h-7 items-center">
+                                                    <button
+                                                        type="button"
+                                                        className="relative rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                                                        onClick={() => setOpen(false)}
+                                                    >
+                                                        <span className="absolute -inset-2.5" />
+                                                        <span className="sr-only">Close panel</span>
+                                                        <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="relative mt-6 flex-1 px-4 sm:px-6 flex flex-col gap-4">
+                                            {children}
+                                        </div>
+                                    </div>
+                                </Dialog.Panel>
+                            </Transition.Child>
+                        </div>
+                    </div>
+                </div>
+            </Dialog>
+        </Transition.Root>
+    );
+};
+export default Modal;
