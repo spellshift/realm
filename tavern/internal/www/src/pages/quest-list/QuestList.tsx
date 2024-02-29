c5c33fe96230028b8d43140da3e12a36380f3078
cmp5987 <32434695+cmp5987@users.noreply.github.com>
2024-02-29 02:22:42 +0000

    fix(style): Fix styling of quest and tomes

--- tavern/internal/www/src/pages/quest-list/QuestList.tsx
+++ tavern/internal/www/src/pages/quest-list/QuestList.tsx
@@ -20,7 +20,7 @@ export const QuestList = () => {
 
     return (
         <PageWrapper currNavItem={PageNavItem.quests}>
-            <div className="border-b border-gray-200 pb-5 sm:flex sm:items-center sm:justify-between">
+            <div className="border-b border-gray-200 pb-5 flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
                 <div className="flex-1 flex flex-col gap-2">
                     <h3 className="text-xl font-semibold leading-6 text-gray-900">Quests</h3>
                     <div className="max-w-2xl text-sm">
