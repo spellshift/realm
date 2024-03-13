91c2e41ee75de09ef985c6d27dfa9e1faa2869d3
KCarretto <Kcarretto@gmail.com>
2024-03-13 02:28:43 +0000

    built UI

--- tavern/internal/www/src/pages/shell/Shell.tsx
+++ tavern/internal/www/src/pages/shell/Shell.tsx
@@ -26,7 +26,7 @@ const Shell = () => {
     return (
         <PageWrapper currNavItem={PageNavItem.dashboard}>
             <div className="border-b border-gray-200 pb-6 sm:flex sm:items-center sm:justify-between">
-                <h3 className="text-xl font-semibold leading-6 text-gray-900">Shell</h3>
+                <h3 className="text-xl font-semibold leading-6 text-gray-900">Shell <i>[BETA]</i></h3>
             </div>
             <div id="terminal" className="w-full bg-gray-500 h-48" ref={terminalRef}></div>
         </PageWrapper>
