ea2a40e5f7943189b683ef3b71e478290b85576c
cmp5987 <32434695+cmp5987@users.noreply.github.com>
2024-04-12 23:44:08 +0000

    fix(bug): Fix oveflow of output

--- tavern/internal/www/src/features/task-card/components/TaskResults.tsx
+++ tavern/internal/www/src/features/task-card/components/TaskResults.tsx
@@ -13,32 +13,34 @@ const TaskResults: FC<TaskResultsType> = ({
     return (
         <div className="flex flex-row gap-4">
             <BookOpenIcon className="h-5 w-5 mt-1" />
-            <div className="flex flex-col gap-1 ">
+            <div className="flex flex-col gap-1 w-full">
                 <div className="text-gray-600">
                     Tome: {quest?.tome?.name}
                 </div>
-                <div className="flex flex-col gap-2 text-sm max-h-80 overflow-y-scroll">
-                    {error ? (
-                        <CodeBlock
-                            className="-ml-2"
-                            text={error}
-                            language={""}
-                            showLineNumbers={false}
-                            theme={tomorrow}
-                            codeBlock
-                        />
-                    ) : output && output?.length > 0 ? (
-                        <CodeBlock
-                            className="-ml-2"
-                            text={output}
-                            language={""}
-                            showLineNumbers={false}
-                            theme={tomorrow}
-                            codeBlock
-                        />
-                    ) : (
-                        <div className="mt-2 text-gray-600">No output available</div>
-                    )}
+                <div className="flex flex-col gap-2 text-sm max-h-80 overflow-y-scroll overflow-x-scroll">
+                    <div className="max-w-fit">
+                        {error ? (
+                            <CodeBlock
+                                className="-ml-2"
+                                text={error}
+                                language={""}
+                                showLineNumbers={false}
+                                theme={tomorrow}
+                                codeBlock
+                            />
+                        ) : output && output?.length > 0 ? (
+                            <CodeBlock
+                                className="-ml-2"
+                                text={output + "Where can I get some? There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."}
+                                language={""}
+                                showLineNumbers={false}
+                                theme={tomorrow}
+                                codeBlock
+                            />
+                        ) : (
+                            <div className="mt-2 text-gray-600">No output available</div>
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
