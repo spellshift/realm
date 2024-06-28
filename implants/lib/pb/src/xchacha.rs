6a8ebeb9444e4265a293a56aba13facb67e7f08f
Hulto <7121375+hulto@users.noreply.github.com>
2024-06-28 23:15:19 +0000

    Claenup

--- implants/lib/pb/src/xchacha.rs
+++ implants/lib/pb/src/xchacha.rs
@@ -1,17 +1,15 @@
-use anyhow::Result;
-use base64::{engine::general_purpose, Engine};
+use anyhow::{Context, Result};
 use bytes::{Buf, BufMut};
 use chacha20poly1305::{aead::generic_array::GenericArray, aead::Aead, AeadCore, KeyInit};
+use const_decoder::Decoder as const_decode;
 use prost::Message;
 use rand::rngs::OsRng;
 use rand_chacha::rand_core::SeedableRng;
-use sha2::{Digest, Sha256};
 use std::{
     collections::HashMap,
-    hash::Hash,
     io::{Read, Write},
     marker::PhantomData,
-    sync::{Arc, Mutex, OnceLock, RwLock},
+    sync::{Mutex, OnceLock},
 };
 use tonic::{
     codec::{Codec, DecodeBuf, Decoder, EncodeBuf, Encoder},
@@ -19,11 +17,10 @@ use tonic::{
 };
 use x25519_dalek::{EphemeralSecret, PublicKey};
 
-#[derive(Debug, Clone)]
-pub struct ChaChaSvc {
-    key: Vec<u8>,
-    priv_key_history: HashMap<[u8; 32], [u8; 32]>,
-}
+const SERVER_PUBKEY_STR: &str = env!("SERVER_PUBKEY");
+const SERVER_PUBKEY: [u8; 32] = const_decode::Base64.decode(SERVER_PUBKEY_STR.as_bytes());
+
+// ------------
 
 fn key_history() -> &'static Mutex<HashMap<[u8; 32], [u8; 32]>> {
     static ARRAY: OnceLock<Mutex<HashMap<[u8; 32], [u8; 32]>>> = OnceLock::new();
@@ -31,35 +28,26 @@ fn key_history() -> &'static Mutex<HashMap<[u8; 32], [u8; 32]>> {
 }
 
 fn add_key_history(pub_key: [u8; 32], shared_secret: [u8; 32]) {
-    key_history().lock().unwrap().insert(pub_key, shared_secret);
+    key_history().lock().unwrap().insert(pub_key, shared_secret); // Mutex's must unwrap
 }
 
-fn get_key(pub_key: [u8; 32]) -> [u8; 32] {
-    *key_history().lock().unwrap().get(&pub_key).unwrap()
+fn get_key(pub_key: [u8; 32]) -> Result<[u8; 32]> {
+    let res = *key_history()
+        .lock()
+        .unwrap() // Mutex's must unwrap
+        .get(&pub_key)
+        .context("Key not found")?;
+    Ok(res)
 }
 
-// !!!! DANGER !!!!!
-// This is the default static key set to ensure compatability between server and client
-// in debug builds. This key must be changed in order to protect data. We're still in the
-// process of implementing app layer crypto and this is temporary. Do not rely on the
-// app layer crypto without changing this default.
-const IMIX_ENCRYPT_KEY_DEFAULT: &str = "I Don't care how small the room is I cast fireball";
-const IMIX_ENCRYPT_KEY: Option<&'static str> = option_env!("IMIX_ENCRYPT_KEY",);
+// ------------
 
-const SERVER_PUB_KEY: [u8; 32] = [
-    115, 191, 95, 40, 160, 88, 16, 88, 37, 61, 11, 49, 240, 4, 125, 171, 127, 117, 245, 6, 242,
-    215, 197, 247, 122, 187, 197, 169, 155, 249, 205, 61,
-];
+#[derive(Debug, Clone)]
+pub struct ChaChaSvc {}
 
 impl Default for ChaChaSvc {
     fn default() -> Self {
-        Self {
-            key: match IMIX_ENCRYPT_KEY {
-                Some(res) => res.into(),
-                None => IMIX_ENCRYPT_KEY_DEFAULT.into(),
-            },
-            priv_key_history: HashMap::new(),
-        }
+        Self {}
     }
 }
 
@@ -114,33 +102,25 @@ where
         }
 
         // Store server pubkey
-        let server_public_bytes = SERVER_PUB_KEY;
-        let server_public = PublicKey::from(server_public_bytes);
+        let server_public = PublicKey::from(SERVER_PUBKEY);
 
         // Generate ephemeral keys
         let rng = rand_chacha::ChaCha20Rng::from_entropy();
         let client_secret = EphemeralSecret::random_from_rng(rng);
         let client_public = PublicKey::from(&client_secret);
-        log::debug!("client_public: {:?}", client_public);
 
         // Generate shared secret
         let shared_secret = client_secret.diffie_hellman(&server_public);
         add_key_history(*client_public.as_bytes(), *shared_secret.as_bytes());
 
-        // let key = shared_secret.as_bytes();
-        // let mut hasher = Sha256::new();
-        // hasher.update(IMIX_ENCRYPT_KEY_DEFAULT);
-        // let key = hasher.finalize();
-        // log::debug!("[DEBUG] {:?}", key);
-        log::debug!("[DEBUG] derived key {:?}", shared_secret.as_bytes());
-
+        // Generate nonce
         let cipher = chacha20poly1305::XChaCha20Poly1305::new(GenericArray::from_slice(
             shared_secret.as_bytes(),
         ));
         let nonce = chacha20poly1305::XChaCha20Poly1305::generate_nonce(&mut OsRng);
 
+        // Encrypt data
         let pt_vec = item.encode_to_vec();
-
         let ciphertext = match cipher.encrypt(&nonce, pt_vec.as_slice()) {
             Ok(ct) => ct,
             Err(err) => {
@@ -150,8 +130,9 @@ where
             }
         };
 
-        let _ = buf.writer().write_all(client_public.as_bytes());
-        let _ = buf.writer().write_all(nonce.as_slice());
+        // Write pubkey + nonce + cipher text
+        buf.writer().write_all(client_public.as_bytes());
+        buf.writer().write_all(nonce.as_slice());
         buf.writer().write_all(ciphertext.as_slice())?;
 
         Ok(())
@@ -174,6 +155,7 @@ where
     type Error = Status;
 
     fn decode(&mut self, buf: &mut DecodeBuf<'_>) -> Result<Option<Self::Item>, Self::Error> {
+        // public key + xchacha nonce + ciphertext
         let mut reader = buf.reader();
         let mut bytes_in = vec![0; DEFAULT_CODEC_BUFFER_SIZE];
         let bytes_read = match reader.read(&mut bytes_in) {
@@ -186,27 +168,46 @@ where
         };
 
         // TODO validate buffer size to avoid index out of bounds accesses
-        let buf = bytes_in.get(0..bytes_read).unwrap();
-        let msg_pub_key_ref = buf.get(0..32).unwrap();
-        let nonce = buf.get(32..56).unwrap();
-        let ciphertext = buf.get(56..).unwrap();
+        let buf = bytes_in
+            .get(0..bytes_read)
+            .context("Bytes read doesn't match buffer size")
+            .map_err(from_anyhow_error)?;
 
-        log::debug!("msg_pub_key: {:?}", buf);
-        let msg_pub_key: [u8; 32] = msg_pub_key_ref.to_vec().try_into().unwrap();
+        let client_public = buf
+            .get(0..32)
+            .context("Input buffer doesn't have enough bytes for public key")
+            .map_err(from_anyhow_error)?;
 
-        let key = get_key(msg_pub_key);
-        log::debug!("priv_key_history: {:?}", key);
-        let cipher = chacha20poly1305::XChaCha20Poly1305::new(GenericArray::from_slice(&key));
+        let nonce = buf
+            .get(32..56)
+            .context("Input buffer doesn't have enough bytes for nonce")
+            .map_err(from_anyhow_error)?;
 
+        let ciphertext = buf
+            .get(56..)
+            .context("Input buffer doesn't have enough bytes for ciphertext")
+            .map_err(from_anyhow_error)?;
+
+        // Get private key based on messages public key
+        let tmp_client_public_bytes = client_public.to_vec();
+        let client_public_bytes = tmp_client_public_bytes.try_into().unwrap(); // Bruh idk how to not unwrap this :sob:
+
+        let client_private_bytes = get_key(client_public_bytes).map_err(from_anyhow_error)?;
+        let cipher = chacha20poly1305::XChaCha20Poly1305::new(GenericArray::from_slice(
+            &client_private_bytes,
+        ));
+
+        // Decrypt message
         let plaintext = match cipher.decrypt(GenericArray::from_slice(nonce), ciphertext.as_ref()) {
             Ok(pt) => pt,
             Err(err) => {
                 #[cfg(debug_assertions)]
-                log::debug!("err: {:?}", err);
+                log::debug!("Error decrypting response: {:?}", err);
                 return Err(Status::new(tonic::Code::Internal, err.to_string()));
             }
         };
 
+        // Serialize
         let item = Message::decode(bytes::Bytes::from(plaintext))
             .map(Option::Some)
             .map_err(from_decode_error)?;
@@ -215,12 +216,12 @@ where
     }
 }
 
+fn from_anyhow_error(error: anyhow::Error) -> Status {
+    Status::new(tonic::Code::Internal, error.to_string())
+}
+
 fn from_decode_error(error: prost::DecodeError) -> Status {
     // Map Protobuf parse errors to an INTERNAL status code, as per
     // https://github.com/grpc/grpc/blob/master/doc/statuscodes.md
     Status::new(tonic::Code::Internal, error.to_string())
-}
-
-pub fn decode_b64(content: String) -> Result<Vec<u8>> {
-    Ok(general_purpose::STANDARD.decode(content.as_bytes())?)
 }
--- tavern/internal/cryptocodec/cryptocodec.go
+++ tavern/internal/cryptocodec/cryptocodec.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"crypto/ecdh"
 	"crypto/rand"
-	"crypto/sha256"
 	"errors"
 	"fmt"
 	"log"
@@ -17,12 +16,14 @@ import (
 	"google.golang.org/grpc/encoding"
 )
 
-// var last_seen_client_pub_key []byte
+// TODO: Switch to a gomap and mutex.
 var session_pub_keys = sync.Map{}
 
+// TODO: Should we make this a random long byte array in case it gets used anywhere to avoid encrypting data with a week key? - Sliver handles errors in this way.
+var FAILURE_BYTES = []byte{}
+
 func init() {
-	log.Println("Loading xchacha20-poly1305")
-	// Probably want to use the `ForceServerCodec` option instead.
+	log.Println("[INFO] Loading xchacha20-poly1305")
 	encoding.RegisterCodec(StreamDecryptCodec{})
 }
 
@@ -34,6 +35,148 @@ func NewStreamDecryptCodec() StreamDecryptCodec {
 	return StreamDecryptCodec{}
 }
 
+func (s StreamDecryptCodec) Marshal(v any) ([]byte, error) {
+	id, err := goid()
+	if err != nil {
+		log.Println("[ERROR] Unable to find GOID ", id)
+		return FAILURE_BYTES, err
+	}
+	proto := encoding.GetCodec("proto")
+	res, err := proto.Marshal(v)
+	enc_res := s.Csvc.Encrypt(res)
+	return enc_res, err
+}
+
+func (s StreamDecryptCodec) Unmarshal(buf []byte, v any) error {
+	id, err := goid()
+	if err != nil {
+		log.Println("[ERROR] Unable to find GOID ", id)
+		return err
+	}
+	dec_buf, pub_key := s.Csvc.Decrypt(buf)
+	s.Csvc.SetAgentPubkey(pub_key)
+	proto := encoding.GetCodec("proto")
+	return proto.Unmarshal(dec_buf, v)
+}
+
+func (s StreamDecryptCodec) Name() string {
+	return "xchacha20-poly1305"
+}
+
+type CryptoSvc struct {
+	// Aead cipher.AEAD
+	priv_key *ecdh.PrivateKey
+}
+
+func NewCryptoSvc(priv_key *ecdh.PrivateKey) CryptoSvc {
+	return CryptoSvc{
+		priv_key: priv_key,
+	}
+}
+
+func (csvc *CryptoSvc) GetAgentPubkey() []byte {
+	id, err := goid()
+	if err != nil {
+		log.Println("[ERROR] Unable to find GOID ", id)
+		return FAILURE_BYTES
+	}
+	res, ok := session_pub_keys.Load(id)
+	if !ok {
+		log.Println("[ERROR] Public key not found")
+	}
+	return res.([]byte)
+}
+
+func (csvc *CryptoSvc) SetAgentPubkey(client_pub_key []byte) {
+	id, err := goid()
+	if err != nil {
+		log.Println("[ERROR] Failed to get goid")
+	}
+	session_pub_keys.Store(id, client_pub_key)
+}
+
+func (csvc *CryptoSvc) generate_shared_key(client_pub_key_bytes []byte) []byte {
+	fmt.Println("[DEBUG] client_pub_key_bytes: ", client_pub_key_bytes)
+	x22519_curve := ecdh.X25519()
+	client_pub_key, err := x22519_curve.NewPublicKey(client_pub_key_bytes)
+	if err != nil {
+		log.Printf("[ERROR] Failed to create public key %v", err)
+		return FAILURE_BYTES
+	}
+
+	shared_key, err := csvc.priv_key.ECDH(client_pub_key)
+	if err != nil {
+		log.Printf("[ERROR] Failed to get shared secret %v", err)
+		return FAILURE_BYTES
+	}
+
+	return shared_key
+}
+
+func (csvc *CryptoSvc) Decrypt(in_arr []byte) ([]byte, []byte) {
+	// Read in pub key
+	if len(in_arr) < x25519.Size {
+		fmt.Printf("Input bytes to short %d expected at least %d\n", len(in_arr), x25519.Size)
+		return FAILURE_BYTES, FAILURE_BYTES
+	}
+
+	client_pub_key_bytes := in_arr[:x25519.Size]
+	csvc.SetAgentPubkey(client_pub_key_bytes)
+
+	// Generate shared secret
+	derived_key := csvc.generate_shared_key(client_pub_key_bytes)
+
+	log.Println("[DEBUG] derived_key: ", derived_key)
+	aead, err := chacha20poly1305.NewX(derived_key)
+	if err != nil {
+		log.Printf("[ERROR] Failed to create xchacha key %v", err)
+		return FAILURE_BYTES, FAILURE_BYTES
+	}
+
+	// Progress in_arr buf
+	in_arr = in_arr[x25519.Size:]
+
+	// Read nonce
+	if len(in_arr) < aead.NonceSize() {
+		fmt.Printf("Input bytes to short %d expected at least %d\n", len(in_arr), aead.NonceSize())
+		return FAILURE_BYTES, FAILURE_BYTES
+	}
+	nonce, ciphertext := in_arr[:aead.NonceSize()], in_arr[aead.NonceSize():]
+
+	// Decrypt
+	plaintext, err := aead.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		fmt.Printf("Failed to decrypt %v\n", err)
+		return FAILURE_BYTES, FAILURE_BYTES
+	}
+
+	return plaintext, client_pub_key_bytes
+}
+
+// TODO: Don't use [] ref.
+func (csvc *CryptoSvc) Encrypt(in_arr []byte) []byte {
+	// Get the client pub key?
+	client_pub_key_bytes := csvc.GetAgentPubkey()
+	fmt.Println("[DEBUG] Got pub key: ", client_pub_key_bytes)
+
+	// Generate shared secret
+	shared_key := csvc.generate_shared_key(client_pub_key_bytes)
+	aead, err := chacha20poly1305.NewX(shared_key)
+	if err != nil {
+		log.Printf("[ERROR] Failed to create xchacha key %v", err)
+		return FAILURE_BYTES
+	}
+
+	nonce := make([]byte, aead.NonceSize(), aead.NonceSize()+len(in_arr)+aead.Overhead())
+	if _, err := rand.Read(nonce); err != nil {
+		fmt.Printf("Failed to encrypt %v\n", err)
+		return FAILURE_BYTES
+	}
+	encryptedMsg := aead.Seal(nonce, nonce, in_arr, nil)
+	return append(client_pub_key_bytes, encryptedMsg...)
+}
+
+// TODO: Find a better way
 // This is terrible, slow, and should never be used.
 func goid() (int, error) {
 	buf := make([]byte, 32)
@@ -53,161 +196,4 @@ func goid() (int, error) {
 	}
 
 	return strconv.Atoi(string(buf[:i]))
-}
-
-func (s StreamDecryptCodec) Marshal(v any) ([]byte, error) {
-	id, _ := goid()
-	fmt.Printf("GO_ID: %d Marshal\n", id)
-	proto := encoding.GetCodec("proto")
-	res, err := proto.Marshal(v)
-	enc_res := s.Csvc.Encrypt(res)
-	return enc_res, err
-}
-
-func (s StreamDecryptCodec) Unmarshal(buf []byte, v any) error {
-	id, _ := goid()
-	fmt.Printf("GO_ID: %d Unmarshal\n", id)
-	dec_buf, pub_key := s.Csvc.Decrypt(buf)
-	s.Csvc.SetAgentPubkey(pub_key)
-	proto := encoding.GetCodec("proto")
-	return proto.Unmarshal(dec_buf, v)
-}
-
-func (s StreamDecryptCodec) Name() string {
-	return "xchacha20-poly1305"
-}
-
-type CryptoSvc struct {
-	// Aead cipher.AEAD
-	priv_key *ecdh.PrivateKey
-}
-
-func hashitup(in_key []byte) []byte {
-	hasher := sha256.New()
-	hasher.Write(in_key)
-	key := hasher.Sum(nil)
-	// aead, err := chacha20poly1305.NewX(key)
-
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// nonce := make([]byte, aead.NonceSize())
-	// if _, err := rand.Read(nonce); err != nil {
-	// 	panic(err)
-	// }
-	return key
-
-}
-
-func NewCryptoSvc(priv_key *ecdh.PrivateKey) CryptoSvc {
-
-	res := CryptoSvc{
-		// Aead: aead,
-		priv_key: priv_key,
-	}
-
-	return res
-
-}
-
-func (csvc *CryptoSvc) GetAgentPubkey() []byte {
-	id, _ := goid()
-	res, _ := session_pub_keys.Load(id)
-	return res.([]byte)
-}
-
-func (csvc *CryptoSvc) SetAgentPubkey(client_pub_key []byte) {
-	// fmt.Printf("Old last_seen_client_pub_key: %v\n", last_seen_client_pub_key)
-	// last_seen_client_pub_key = client_pub_key
-	// fmt.Printf("New last_seen_client_pub_key: %v\n", last_seen_client_pub_key)
-	id, _ := goid()
-	session_pub_keys.Store(id, client_pub_key)
-}
-
-func (csvc *CryptoSvc) generate_shared_key(client_pub_key_bytes []byte) []byte {
-	fmt.Println("[DEBUG] client_pub_key_bytes: ", client_pub_key_bytes)
-	x22519_curve := ecdh.X25519()
-	client_pub_key, err := x22519_curve.NewPublicKey(client_pub_key_bytes)
-	if err != nil {
-		log.Printf("[ERROR] Failed to create public key %v", err)
-		return []byte{}
-	}
-
-	shared_key, err := csvc.priv_key.ECDH(client_pub_key)
-	if err != nil {
-		log.Printf("[ERROR] Failed to get shared secret %v", err)
-		return []byte{}
-	}
-
-	return shared_key
-}
-
-func (csvc *CryptoSvc) Decrypt(in_arr []byte) ([]byte, []byte) {
-	// Read in pub key
-	if len(in_arr) < x25519.Size {
-		fmt.Printf("Input bytes to short %d expected at least %d\n", len(in_arr), x25519.Size)
-		return []byte{}, []byte{}
-	}
-
-	client_pub_key_bytes := in_arr[:x25519.Size]
-	csvc.SetAgentPubkey(client_pub_key_bytes)
-
-	// // Generate shared secret
-	// log.Printf("[DEBUG] shared_key: %v\n", shared_key)
-	// aead, err := chacha20poly1305.NewX(shared_key)
-	// derived_key := hashitup([]byte("I Don't care how small the room is I cast fireball"))
-
-	derived_key := csvc.generate_shared_key(client_pub_key_bytes)
-
-	log.Println("[DEBUG] derived_key: ", derived_key)
-	aead, err := chacha20poly1305.NewX(derived_key)
-	if err != nil {
-		log.Printf("[ERROR] Failed to create xchacha key %v", err)
-		return []byte{}, []byte{}
-	}
-
-	// // Progress in_arr buf
-	// fmt.Println("[DEBUG] in_arr 1:", in_arr)
-	in_arr = in_arr[x25519.Size:]
-	// fmt.Println("[DEBUG] in_arr 2:", in_arr)
-
-	// Read nonce
-	if len(in_arr) < aead.NonceSize() {
-		fmt.Printf("Input bytes to short %d expected at least %d\n", len(in_arr), aead.NonceSize())
-		return []byte{}, []byte{}
-	}
-	nonce, ciphertext := in_arr[:aead.NonceSize()], in_arr[aead.NonceSize():]
-
-	// Decrypt
-	plaintext, err := aead.Open(nil, nonce, ciphertext, nil)
-	if err != nil {
-		fmt.Printf("Failed to decrypt %v\n", err)
-		return []byte{}, []byte{}
-	}
-
-	return plaintext, client_pub_key_bytes
-}
-
-// TODO: Don't use [] ref.
-func (csvc *CryptoSvc) Encrypt(in_arr []byte) []byte {
-	// Get the client pub key?
-	client_pub_key_bytes := csvc.GetAgentPubkey()
-	fmt.Println("[DEBUG] Got pub key: ", client_pub_key_bytes)
-
-	// Generate shared secret
-	shared_key := csvc.generate_shared_key(client_pub_key_bytes)
-	aead, err := chacha20poly1305.NewX(shared_key)
-	if err != nil {
-		log.Printf("[ERROR] Failed to create xchacha key %v", err)
-		return []byte{}
-	}
-
-	nonce := make([]byte, aead.NonceSize(), aead.NonceSize()+len(in_arr)+aead.Overhead())
-	if _, err := rand.Read(nonce); err != nil {
-		fmt.Printf("Failed to encrypt %v\n", err)
-		return []byte{}
-	}
-	encryptedMsg := aead.Seal(nonce, nonce, in_arr, nil)
-	return append(client_pub_key_bytes, encryptedMsg...)
 }
