3784c67a66f46fd5991a1b180da7d6115fd84053
Hulto <7121375+hulto@users.noreply.github.com>
2024-06-29 18:34:18 -0500

    App layer crypto codec asym (#784)
    * Rough POC
    
    * Update key
    
    * Debug
    
    * Works on my computer.
    
    * Claenup
    
    * Unused edps

--- implants/lib/pb/src/xchacha.rs
+++ implants/lib/pb/src/xchacha.rs
@@ -1,38 +1,53 @@
+use anyhow::{Context, Result};
 use bytes::{Buf, BufMut};
 use chacha20poly1305::{aead::generic_array::GenericArray, aead::Aead, AeadCore, KeyInit};
+use const_decoder::Decoder as const_decode;
 use prost::Message;
 use rand::rngs::OsRng;
-use sha2::{Digest, Sha256};
+use rand_chacha::rand_core::SeedableRng;
 use std::{
+    collections::HashMap,
     io::{Read, Write},
     marker::PhantomData,
+    sync::{Mutex, OnceLock},
 };
 use tonic::{
     codec::{Codec, DecodeBuf, Decoder, EncodeBuf, Encoder},
     Status,
 };
+use x25519_dalek::{EphemeralSecret, PublicKey};
 
-#[derive(Debug, Clone)]
-pub struct ChaChaSvc {
-    key: Vec<u8>,
+const SERVER_PUBKEY_STR: &str = env!("SERVER_PUBKEY");
+const SERVER_PUBKEY: [u8; 32] = const_decode::Base64.decode(SERVER_PUBKEY_STR.as_bytes());
+
+// ------------
+
+fn key_history() -> &'static Mutex<HashMap<[u8; 32], [u8; 32]>> {
+    static ARRAY: OnceLock<Mutex<HashMap<[u8; 32], [u8; 32]>>> = OnceLock::new();
+    ARRAY.get_or_init(|| Mutex::new(HashMap::new()))
 }
 
-// !!!! DANGER !!!!!
-// This is the default static key set to ensure compatability between server and client
-// in debug builds. This key must be changed in order to protect data. We're still in the
-// process of implementing app layer crypto and this is temporary. Do not rely on the
-// app layer crypto without changing this default.
-const IMIX_ENCRYPT_KEY_DEFAULT: &str = "I Don't care how small the room is I cast fireball";
-const IMIX_ENCRYPT_KEY: Option<&'static str> = option_env!("IMIX_ENCRYPT_KEY",);
+fn add_key_history(pub_key: [u8; 32], shared_secret: [u8; 32]) {
+    key_history().lock().unwrap().insert(pub_key, shared_secret); // Mutex's must unwrap
+}
+
+fn get_key(pub_key: [u8; 32]) -> Result<[u8; 32]> {
+    let res = *key_history()
+        .lock()
+        .unwrap() // Mutex's must unwrap
+        .get(&pub_key)
+        .context("Key not found")?;
+    Ok(res)
+}
+
+// ------------
+
+#[derive(Debug, Clone)]
+pub struct ChaChaSvc {}
 
 impl Default for ChaChaSvc {
     fn default() -> Self {
-        Self {
-            key: match IMIX_ENCRYPT_KEY {
-                Some(res) => res.into(),
-                None => IMIX_ENCRYPT_KEY_DEFAULT.into(),
-            },
-        }
+        Self {}
     }
 }
 
@@ -86,16 +101,26 @@ where
             log::debug!("DANGER can't add to the buffer.");
         }
 
-        let key = self.1.key.as_slice();
-        let mut hasher = Sha256::new();
-        hasher.update(key);
-        let key_hash = hasher.finalize();
+        // Store server pubkey
+        let server_public = PublicKey::from(SERVER_PUBKEY);
 
-        let cipher = chacha20poly1305::XChaCha20Poly1305::new(GenericArray::from_slice(&key_hash));
+        // Generate ephemeral keys
+        let rng = rand_chacha::ChaCha20Rng::from_entropy();
+        let client_secret = EphemeralSecret::random_from_rng(rng);
+        let client_public = PublicKey::from(&client_secret);
+
+        // Generate shared secret
+        let shared_secret = client_secret.diffie_hellman(&server_public);
+        add_key_history(*client_public.as_bytes(), *shared_secret.as_bytes());
+
+        // Generate nonce
+        let cipher = chacha20poly1305::XChaCha20Poly1305::new(GenericArray::from_slice(
+            shared_secret.as_bytes(),
+        ));
         let nonce = chacha20poly1305::XChaCha20Poly1305::generate_nonce(&mut OsRng);
 
+        // Encrypt data
         let pt_vec = item.encode_to_vec();
-
         let ciphertext = match cipher.encrypt(&nonce, pt_vec.as_slice()) {
             Ok(ct) => ct,
             Err(err) => {
@@ -105,7 +130,9 @@ where
             }
         };
 
-        let _ = buf.writer().write_all(nonce.as_slice());
+        // Write pubkey + nonce + cipher text
+        buf.writer().write_all(client_public.as_bytes());
+        buf.writer().write_all(nonce.as_slice());
         buf.writer().write_all(ciphertext.as_slice())?;
 
         Ok(())
@@ -128,6 +155,7 @@ where
     type Error = Status;
 
     fn decode(&mut self, buf: &mut DecodeBuf<'_>) -> Result<Option<Self::Item>, Self::Error> {
+        // public key + xchacha nonce + ciphertext
         let mut reader = buf.reader();
         let mut bytes_in = vec![0; DEFAULT_CODEC_BUFFER_SIZE];
         let bytes_read = match reader.read(&mut bytes_in) {
@@ -139,31 +167,57 @@ where
             }
         };
 
-        let ciphertext = &bytes_in[0..bytes_read];
-        let nonce = &ciphertext[0..24];
-        let ciphertext = &ciphertext[24..];
+        // TODO validate buffer size to avoid index out of bounds accesses
+        let buf = bytes_in
+            .get(0..bytes_read)
+            .context("Bytes read doesn't match buffer size")
+            .map_err(from_anyhow_error)?;
 
-        let key = self.1.key.as_slice();
-        let mut hasher = Sha256::new();
-        hasher.update(key);
-        let key_hash = hasher.finalize();
-        let cipher = chacha20poly1305::XChaCha20Poly1305::new(GenericArray::from_slice(&key_hash));
+        let client_public = buf
+            .get(0..32)
+            .context("Input buffer doesn't have enough bytes for public key")
+            .map_err(from_anyhow_error)?;
 
+        let nonce = buf
+            .get(32..56)
+            .context("Input buffer doesn't have enough bytes for nonce")
+            .map_err(from_anyhow_error)?;
+
+        let ciphertext = buf
+            .get(56..)
+            .context("Input buffer doesn't have enough bytes for ciphertext")
+            .map_err(from_anyhow_error)?;
+
+        // Get private key based on messages public key
+        let tmp_client_public_bytes = client_public.to_vec();
+        let client_public_bytes = tmp_client_public_bytes.try_into().unwrap(); // Bruh idk how to not unwrap this :sob:
+
+        let client_private_bytes = get_key(client_public_bytes).map_err(from_anyhow_error)?;
+        let cipher = chacha20poly1305::XChaCha20Poly1305::new(GenericArray::from_slice(
+            &client_private_bytes,
+        ));
+
+        // Decrypt message
         let plaintext = match cipher.decrypt(GenericArray::from_slice(nonce), ciphertext.as_ref()) {
             Ok(pt) => pt,
             Err(err) => {
                 #[cfg(debug_assertions)]
-                log::debug!("err: {:?}", err);
+                log::debug!("Error decrypting response: {:?}", err);
                 return Err(Status::new(tonic::Code::Internal, err.to_string()));
             }
         };
 
+        // Serialize
         let item = Message::decode(bytes::Bytes::from(plaintext))
             .map(Option::Some)
             .map_err(from_decode_error)?;
 
         Ok(item)
     }
+}
+
+fn from_anyhow_error(error: anyhow::Error) -> Status {
+    Status::new(tonic::Code::Internal, error.to_string())
 }
 
 fn from_decode_error(error: prost::DecodeError) -> Status {
