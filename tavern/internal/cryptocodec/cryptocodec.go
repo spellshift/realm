d9bf87dae7cb60d3d5122376416ab51a05117f30
Hulto <7121375+hulto@users.noreply.github.com>
2024-06-29 18:34:18 -0500

    App layer crypto codec asym (#784)
    * Rough POC
    
    * Update key
    
    * Debug
    
    * Works on my computer.
    
    * Claenup
    
    * Unused edps

--- tavern/internal/cryptocodec/cryptocodec.go
+++ tavern/internal/cryptocodec/cryptocodec.go
@@ -1,19 +1,29 @@
 package cryptocodec
 
 import (
-	"crypto/cipher"
+	"bytes"
+	"crypto/ecdh"
 	"crypto/rand"
-	"crypto/sha256"
+	"errors"
 	"fmt"
 	"log"
+	"runtime"
+	"strconv"
+	"sync"
 
+	"github.com/cloudflare/circl/dh/x25519"
 	"golang.org/x/crypto/chacha20poly1305"
 	"google.golang.org/grpc/encoding"
 )
 
+// TODO: Switch to a gomap and mutex.
+var session_pub_keys = sync.Map{}
+
+// TODO: Should we make this a random long byte array in case it gets used anywhere to avoid encrypting data with a week key? - Sliver handles errors in this way.
+var FAILURE_BYTES = []byte{}
+
 func init() {
-	log.Println("Loading xchacha20-poly1305")
-	// Probably want to use the `ForceServerCodec` option instead.
+	log.Println("[INFO] Loading xchacha20-poly1305")
 	encoding.RegisterCodec(StreamDecryptCodec{})
 }
 
@@ -26,6 +36,11 @@ func NewStreamDecryptCodec() StreamDecryptCodec {
 }
 
 func (s StreamDecryptCodec) Marshal(v any) ([]byte, error) {
+	id, err := goid()
+	if err != nil {
+		log.Println("[ERROR] Unable to find GOID ", id)
+		return FAILURE_BYTES, err
+	}
 	proto := encoding.GetCodec("proto")
 	res, err := proto.Marshal(v)
 	enc_res := s.Csvc.Encrypt(res)
@@ -33,7 +48,13 @@ func (s StreamDecryptCodec) Marshal(v any) ([]byte, error) {
 }
 
 func (s StreamDecryptCodec) Unmarshal(buf []byte, v any) error {
-	dec_buf := s.Csvc.Decrypt(buf)
+	id, err := goid()
+	if err != nil {
+		log.Println("[ERROR] Unable to find GOID ", id)
+		return err
+	}
+	dec_buf, pub_key := s.Csvc.Decrypt(buf)
+	s.Csvc.SetAgentPubkey(pub_key)
 	proto := encoding.GetCodec("proto")
 	return proto.Unmarshal(dec_buf, v)
 }
@@ -43,54 +64,136 @@ func (s StreamDecryptCodec) Name() string {
 }
 
 type CryptoSvc struct {
-	Aead cipher.AEAD
+	// Aead cipher.AEAD
+	priv_key *ecdh.PrivateKey
 }
 
-func NewCryptoSvc(key []byte) CryptoSvc {
-	hasher := sha256.New()
-	hasher.Write(key)
-	key = hasher.Sum(nil)
-	aead, err := chacha20poly1305.NewX(key)
+func NewCryptoSvc(priv_key *ecdh.PrivateKey) CryptoSvc {
+	return CryptoSvc{
+		priv_key: priv_key,
+	}
+}
 
+func (csvc *CryptoSvc) GetAgentPubkey() []byte {
+	id, err := goid()
 	if err != nil {
-		panic(err)
+		log.Println("[ERROR] Unable to find GOID ", id)
+		return FAILURE_BYTES
 	}
-
-	nonce := make([]byte, aead.NonceSize())
-	if _, err := rand.Read(nonce); err != nil {
-		panic(err)
+	res, ok := session_pub_keys.Load(id)
+	if !ok {
+		log.Println("[ERROR] Public key not found")
 	}
-
-	res := CryptoSvc{
-		Aead: aead,
-	}
-
-	return res
-
+	return res.([]byte)
 }
 
-func (csvc *CryptoSvc) Decrypt(in_arr []byte) []byte {
-	if len(in_arr) < csvc.Aead.NonceSize() {
-		fmt.Printf("Input bytes to short %d expected %d\n", len(in_arr), csvc.Aead.NonceSize())
-		return []byte{}
+func (csvc *CryptoSvc) SetAgentPubkey(client_pub_key []byte) {
+	id, err := goid()
+	if err != nil {
+		log.Println("[ERROR] Failed to get goid")
+	}
+	session_pub_keys.Store(id, client_pub_key)
+}
+
+func (csvc *CryptoSvc) generate_shared_key(client_pub_key_bytes []byte) []byte {
+	fmt.Println("[DEBUG] client_pub_key_bytes: ", client_pub_key_bytes)
+	x22519_curve := ecdh.X25519()
+	client_pub_key, err := x22519_curve.NewPublicKey(client_pub_key_bytes)
+	if err != nil {
+		log.Printf("[ERROR] Failed to create public key %v", err)
+		return FAILURE_BYTES
 	}
 
-	nonce, ciphertext := in_arr[:csvc.Aead.NonceSize()], in_arr[csvc.Aead.NonceSize():]
-	plaintext, err := csvc.Aead.Open(nil, nonce, ciphertext, nil)
+	shared_key, err := csvc.priv_key.ECDH(client_pub_key)
+	if err != nil {
+		log.Printf("[ERROR] Failed to get shared secret %v", err)
+		return FAILURE_BYTES
+	}
+
+	return shared_key
+}
+
+func (csvc *CryptoSvc) Decrypt(in_arr []byte) ([]byte, []byte) {
+	// Read in pub key
+	if len(in_arr) < x25519.Size {
+		fmt.Printf("Input bytes to short %d expected at least %d\n", len(in_arr), x25519.Size)
+		return FAILURE_BYTES, FAILURE_BYTES
+	}
+
+	client_pub_key_bytes := in_arr[:x25519.Size]
+	csvc.SetAgentPubkey(client_pub_key_bytes)
+
+	// Generate shared secret
+	derived_key := csvc.generate_shared_key(client_pub_key_bytes)
+
+	log.Println("[DEBUG] derived_key: ", derived_key)
+	aead, err := chacha20poly1305.NewX(derived_key)
+	if err != nil {
+		log.Printf("[ERROR] Failed to create xchacha key %v", err)
+		return FAILURE_BYTES, FAILURE_BYTES
+	}
+
+	// Progress in_arr buf
+	in_arr = in_arr[x25519.Size:]
+
+	// Read nonce
+	if len(in_arr) < aead.NonceSize() {
+		fmt.Printf("Input bytes to short %d expected at least %d\n", len(in_arr), aead.NonceSize())
+		return FAILURE_BYTES, FAILURE_BYTES
+	}
+	nonce, ciphertext := in_arr[:aead.NonceSize()], in_arr[aead.NonceSize():]
+
+	// Decrypt
+	plaintext, err := aead.Open(nil, nonce, ciphertext, nil)
 	if err != nil {
 		fmt.Printf("Failed to decrypt %v\n", err)
-		return []byte{}
+		return FAILURE_BYTES, FAILURE_BYTES
 	}
 
-	return plaintext
+	return plaintext, client_pub_key_bytes
 }
 
+// TODO: Don't use [] ref.
 func (csvc *CryptoSvc) Encrypt(in_arr []byte) []byte {
-	nonce := make([]byte, csvc.Aead.NonceSize(), csvc.Aead.NonceSize()+len(in_arr)+csvc.Aead.Overhead())
+	// Get the client pub key?
+	client_pub_key_bytes := csvc.GetAgentPubkey()
+	fmt.Println("[DEBUG] Got pub key: ", client_pub_key_bytes)
+
+	// Generate shared secret
+	shared_key := csvc.generate_shared_key(client_pub_key_bytes)
+	aead, err := chacha20poly1305.NewX(shared_key)
+	if err != nil {
+		log.Printf("[ERROR] Failed to create xchacha key %v", err)
+		return FAILURE_BYTES
+	}
+
+	nonce := make([]byte, aead.NonceSize(), aead.NonceSize()+len(in_arr)+aead.Overhead())
 	if _, err := rand.Read(nonce); err != nil {
 		fmt.Printf("Failed to encrypt %v\n", err)
-		return []byte{}
+		return FAILURE_BYTES
 	}
-	encryptedMsg := csvc.Aead.Seal(nonce, nonce, in_arr, nil)
-	return encryptedMsg
+	encryptedMsg := aead.Seal(nonce, nonce, in_arr, nil)
+	return append(client_pub_key_bytes, encryptedMsg...)
+}
+
+// TODO: Find a better way
+// This is terrible, slow, and should never be used.
+func goid() (int, error) {
+	buf := make([]byte, 32)
+	n := runtime.Stack(buf, false)
+	buf = buf[:n]
+	// goroutine 1 [running]: ...
+	var goroutinePrefix = []byte("goroutine ")
+	var errBadStack = errors.New("invalid runtime.Stack output")
+	buf, ok := bytes.CutPrefix(buf, goroutinePrefix)
+	if !ok {
+		return 0, errBadStack
+	}
+
+	i := bytes.IndexByte(buf, ' ')
+	if i < 0 {
+		return 0, errBadStack
+	}
+
+	return strconv.Atoi(string(buf[:i]))
 }
