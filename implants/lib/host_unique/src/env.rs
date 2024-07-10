0530e11c928c2ca8b19b3de180938047356d51fa
Hulto <7121375+hulto@users.noreply.github.com>
2024-07-10 01:45:58 +0000

    Fix env unwrap

--- implants/lib/host_unique/src/env.rs
+++ implants/lib/host_unique/src/env.rs
@@ -17,7 +17,14 @@ impl HostIDSelector for Env {
     }
 
     fn get_host_id(&self) -> Option<uuid::Uuid> {
-        let host_id_env = env::var("IMIX_HOST_ID").unwrap();
+        let host_id_env = match env::var("IMIX_HOST_ID") {
+            Ok(res) => res,
+            Err(_err) => {
+                #[cfg(debug_assertions)]
+                log::debug!("No environment variable set {:?}", _err);
+                return None;
+            }
+        };
         match Uuid::parse_str(&host_id_env) {
             Ok(res) => Some(res),
             Err(_err) => {
