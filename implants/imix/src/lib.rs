3a96be6fb27f2d58e04760b07d272c5c5c0a73c4
Hulto <7121375+hulto@users.noreply.github.com>
2024-11-03 22:44:14 -0500

    Seems to work (#796)

--- implants/imix/src/lib.rs
+++ implants/imix/src/lib.rs
@@ -6,22 +6,11 @@ mod install;
 mod run;
 mod task;
 mod version;
-#[cfg(feature = "win_service")]
-pub mod win_service;
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 128)]
 pub async fn lib_entry() {
     #[cfg(debug_assertions)]
     run::init_logging();
-
-    #[cfg(feature = "win_service")]
-    match windows_service::service_dispatcher::start("imix", ffi_service_main) {
-        Ok(_) => {}
-        Err(_err) => {
-            #[cfg(debug_assertions)]
-            log::error!("Failed to start service (running as exe?): {_err}");
-        }
-    }
 
     run::handle_main().await
 }
