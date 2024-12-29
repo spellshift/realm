ce06a3554f84d29ed9f08a4a9fa2422c3334cf41
Joe Abbate <joe@joeabbate.me>
2024-12-28 19:22:49 -0500

    Fix Tests

--- implants/lib/eldritch/src/random/int_impl.rs
+++ implants/lib/eldritch/src/random/int_impl.rs
@@ -14,9 +14,19 @@ pub fn int(min: i32, max: i32) -> Result<i32> {
 mod tests {
     use super::*;
 
+    /*
+    * Statistics of a Uniform Distribution from a to b
+    * mean = (a+b)/2
+    * standard deviation = sqrt(((b-a)^2)/12)
+    * a = 0, b = 1000
+    * mean = 500, std dev = 288.675
+    * 95% Confidence Interval where n = 50000 = (496.675, 503.325)
+    */
     const NUM_ITERATION: i32 = 50000;
     const MIN_VALUE: i32 = 0;
     const MAX_VALUE: i32 = 1000;
+    const CI_99_MIN: f32 = 496.675;
+    const CI_99_MAX: f32 = 503.325;
 
     #[test]
     fn test_random_int() -> anyhow::Result<()> {
@@ -27,24 +37,20 @@ mod tests {
 
     #[test]
     fn test_random_int_uniform() -> anyhow::Result<()> {
-        let mut counts = vec![0; MAX_VALUE as usize];
+        let mut total = 0;
         for _ in 0..NUM_ITERATION {
             let random_number = int(MIN_VALUE, MAX_VALUE)?;
-            counts[random_number as usize] += 1;
+            total += random_number;
         }
 
-        let lower_bound = 0.90 * NUM_ITERATION as f64 / MAX_VALUE as f64;
-        let upper_bound = 1.10 * NUM_ITERATION as f64 / MAX_VALUE as f64;
+        let avg = total as f32 / NUM_ITERATION as f32;
 
-        for count in counts {
-            assert!(
-                count as f64 >= lower_bound && count as f64 <= upper_bound,
-                "Count {} is not within the acceptable bounds of ({},{})",
-                count,
-                lower_bound,
-                upper_bound
-            );
-        }
+        assert!(
+            avg >= CI_99_MIN && avg <= CI_99_MAX,
+            "Average of {} Random Numbers not within 99% Confidence Interval",
+            NUM_ITERATION
+        );
+
         Ok(())
     }
 }
