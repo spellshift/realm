e72e33424e5c481845efa327ef26e39274dc7d4c
Chase Killorin <112667412+chase1k@users.noreply.github.com>
2024-12-30 15:46:03 -0500

    Random.int added (#773)
    * Random.int added
    
    * Random.int test fix
    
    * Random.int make test more leanent
    
    * Typo fix and increasing test iterations
    
    * Cargo fmt
    
    * Double Import Error
    
    * Fix Tests
    
    * Fmt
    
    * Typo
    
    * Better Testing for Uniform
    
    * Should only fail 1% of the time now
    
    ---------
    
    Co-authored-by: Chase Killorin <chase.k@metactf.com>
    Co-authored-by: Hulto <7121375+hulto@users.noreply.github.com>
    Co-authored-by: Joe Abbate <40615740+jabbate19@users.noreply.github.com>
    Co-authored-by: Joe Abbate <joe@joeabbate.me>

--- implants/lib/eldritch/src/random/mod.rs
+++ implants/lib/eldritch/src/random/mod.rs
@@ -1,4 +1,5 @@
 mod bool_impl;
+mod int_impl;
 mod string_impl;
 
 use starlark::{environment::MethodsBuilder, starlark_module, values::starlark_value};
@@ -19,6 +20,11 @@ fn methods(builder: &mut MethodsBuilder) {
     #[allow(unused_variables)]
     fn bool<'v>(this: &RandomLibrary) -> anyhow::Result<bool> {
         bool_impl::bool()
+    }
+
+    #[allow(unused_variables)]
+    fn int<'v>(this: &RandomLibrary, min: i32, max: i32) -> anyhow::Result<i32> {
+        int_impl::int(min, max)
     }
 
     #[allow(unused_variables)]
