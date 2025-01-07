6872f5cff3f5778f115b777d09e0a8791d5608cc
KCarretto <Kcarretto@gmail.com>
2025-01-06 23:10:52 -0500

    fix regression caused by git conflict (#828)

--- tavern/config.go
+++ tavern/config.go
@@ -197,9 +197,9 @@ func (cfg *Config) NewShellMuxes(ctx context.Context) (wsMux *stream.Mux, grpcMu
 		shellOutputTopic := client.Topic(strings.TrimPrefix(topicShellOutput, gcpTopicPrefix))
 
 		// Overwrite env var specification with newly created GCP PubSub Subscriptions
-		subShellInput = fmt.Sprintf("gcppubsub://%s", createGCPSubscription(ctx, shellInputTopic))
+		subShellInput = fmt.Sprintf("gcppubsub://projects/%s/subscriptions/%s", projectID, createGCPSubscription(ctx, shellInputTopic))
 		slog.DebugContext(ctx, "created GCP PubSub subscription for shell input", "subscription_name", subShellInput)
-		subShellOutput = fmt.Sprintf("gcppubsub://%s", createGCPSubscription(ctx, shellOutputTopic))
+		subShellOutput = fmt.Sprintf("gcppubsub://projects/%s/subscriptions/%s", projectID, createGCPSubscription(ctx, shellOutputTopic))
 		slog.DebugContext(ctx, "created GCP PubSub subscription for shell output", "subscription_name", subShellOutput)
 	}
 
