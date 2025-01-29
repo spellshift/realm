5de9d5dd7fa5ebfc2441cf19ebe9ef2c0b998b97
Hulto <7121375+hulto@users.noreply.github.com>
2025-01-28 21:53:10 -0500

    Cred poster

---
+++ bin/cred-publishing/creds.py
@@ -0,0 +1,147 @@
+import argparse
+import os
+import re
+import requests
+from pprint import pprint
+import json
+from dataclasses import dataclass
+
+
+@dataclass
+class CredPost:
+    graphql_url: str
+    auth_session: str
+    known_hosts: dict
+
+    def make_graphql_request(self, query, variables):
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        }
+
+        data = {"query": query, "variables": variables}
+        cookies = {
+            "auth-session": self.auth_session,
+        }
+
+        response = requests.post(self.graphql_url, json=data, headers=headers, cookies=cookies)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Error {response.status_code}: {response.text}")
+            return None
+
+
+    def get_hosts(self, ip: str):
+        graphql_query = """
+query getHosts($where:HostWhereInput){
+    hosts(where:$where) {
+        id
+        primaryIP
+        name
+    }
+}    """
+
+        graphql_variables = {
+  "where": {
+    "primaryIP": ip
+  }
+}
+        res = self.make_graphql_request(graphql_query, graphql_variables)
+        if 'errors' in res:
+            return -1
+        else:
+            return res
+
+    def create_cred(self, principal: str, secret: str, kind: str, host_id: int):
+        graphql_query = """
+mutation CreateCreds($input:CreateHostCredentialInput!) {
+  createCredential(input:$input) {
+    principal
+    secret
+    kind
+    host {
+      id
+    }
+    task {
+      id
+    }
+  }
+}
+"""
+        graphql_variables = {
+  "input": {
+    "principal": principal,
+    "secret": secret,
+    "kind": kind,
+    "hostID": host_id
+  }
+}
+        res = self.make_graphql_request(graphql_query, graphql_variables)
+        if 'errors' in res:
+            pprint(res)
+            return -1
+        else:
+            return res['data']['createTag']['id']
+
+    def add_hosts(self, tag_id: str, hosts: list):
+        graphql_query = """
+mutation updateTag($input:UpdateTagInput!, $tagid:ID!){
+    updateTag(input:$input, tagID:$tagid) {
+        id
+    }
+}    """
+        print(hosts)
+        graphql_variables = {"input":{"addHostIDs":hosts},"tagid":tag_id}
+        res = self.make_graphql_request(graphql_query, graphql_variables)
+        if 'errors' in res:
+            pprint(res)
+            return -1
+        else:
+            return res['data']['updateTag']['id']
+
+    def get_host_id(self, ip: str) -> int:
+        if ip in self.known_hosts:
+            return self.known_hosts[ip]
+
+        self.known_hosts[ip] = self.get_hosts(ip)
+        return self.known_hosts[ip]
+
+    def run(self, log_file):
+        with open(log_file) as file:
+            for line in file:
+                line_arr = line.strip().split(":")
+                ip = line_arr[0]
+                user = line_arr[1]
+                password = line_arr[2]
+                host_id = self.get_host_id(ip)
+                self.create_cred(
+                    user,
+                    password,
+                    "KIND_PASSWORD",
+                    host_id
+                )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="CCDC Cred post",
+        description="Parse cred logs in form `ip:user:password` and post them to tavern",
+    )
+
+    parser.add_argument("tavern_url")
+    parser.add_argument("log_file")
+
+    args = parser.parse_args()
+
+    auth_session = os.environ.get("TAVERN_AUTH_SESSION")
+
+    if auth_session is None:
+        print(
+            "No auth-session cookie found. Please set it using the environment variable TAVERN_AUTH_SESSION"
+        )
+        exit(1)
+
+    graphql_url = f"{args.tavern_url}/graphql".replace("//","/")
+    poster = CredPost(graphql_url, auth_session)
+    poster.run(args.log_file)
