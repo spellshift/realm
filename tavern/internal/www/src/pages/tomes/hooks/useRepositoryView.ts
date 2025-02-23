80094e0a07c39a4bfe09935c41c754ba80bc8d9b
Joe Abbate <joe@joeabbate.me>
2025-02-23 00:44:37 -0500

    Show Tomes with No Repo

--- tavern/internal/www/src/pages/tomes/hooks/useRepositoryView.ts
+++ tavern/internal/www/src/pages/tomes/hooks/useRepositoryView.ts
@@ -1,43 +1,42 @@
 import { useQuery } from "@apollo/client";
 import { useEffect, useState } from "react";
 import { RepositoryRow } from "../../../utils/consts";
-import { GET_REPOSITORY_QUERY, GET_TOMES_QUERY } from "../../../utils/queries";
+import { GET_ALL_TOMES_QUERY } from "../../../utils/queries";
 
 export const useRepositoryView = () => {
     const [firstParty, setFirstParty] = useState<RepositoryRow | null>(null);
+    const [noRepo, setNoRepo] = useState<RepositoryRow | null>(null);
     const [repositories, setRepositories] = useState<Array<RepositoryRow>>([]);
-    const { loading: firstPartyTomeLoading, data: firstPartyTome, error: firstPartyTomeError } = useQuery(GET_TOMES_QUERY, {
-        variables:
-        {
-            "where": {
-              "supportModel": "FIRST_PARTY"
-            },
-        }
-    });
 
-    const {loading, data, error} = useQuery(GET_REPOSITORY_QUERY, {
-        variables:
-        {
-            "orderBy": [{
-                "direction": "DESC",
-                "field": "LAST_MODIFIED_AT"
-            }]
-        }
-    });
+    const {loading, data, error} = useQuery(GET_ALL_TOMES_QUERY);
 
     useEffect(()=> {
-        if(!firstParty && firstPartyTome && firstPartyTome?.tomes?.length > 0){
+        if(!firstParty && data && data.first_party && data.first_party?.length > 0){
             const firstPartyRepo =
             {node:{
                 url: "https://github.com/spellshift/realm/tree/main/tavern/tomes",
                 repoType: "FIRST_PARTY",
-                tomes: firstPartyTome?.tomes
+                tomes: data.first_party
             }}
             setFirstParty(
                 firstPartyRepo
             );
         }
-    },[firstPartyTome, firstParty]);
+    },[data, firstParty]);
+
+    useEffect(()=> {
+        if(!noRepo && data && data.no_repo && data.no_repo?.length > 0){
+            const noRepoData =
+            {node:{
+                url: "No Repository",
+                repoType: "",
+                tomes: data.no_repo
+            }}
+            setNoRepo(
+                noRepoData
+            );
+        }
+    },[data, noRepo]);
 
     useEffect(()=>{
         const repos = [] as Array<RepositoryRow>;
@@ -49,14 +48,17 @@ export const useRepositoryView = () => {
                 ...data?.repositories?.edges
             );
         }
+        if(noRepo){
+            repos.push(noRepo);
+        }
         setRepositories(
             repos
         );
-    },[data, firstParty]);
+    },[data, firstParty, noRepo]);
 
     return {
-        loading: firstPartyTomeLoading || loading,
-        repositories: repositories,
-        error: firstPartyTomeError || error
+        loading,
+        repositories,
+        error
     };
 }
