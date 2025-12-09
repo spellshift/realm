89560d96812d38366f45579f306bcf38fb7ef90f
Hulto <7121375+hulto@users.noreply.github.com>
2025-12-08 19:01:37 -0500

    update rust fix failing CI (#1184)
    * Update rust version to 1.91.1

--- implants/lib/eldritch/src/sys/exec_impl.rs
+++ implants/lib/eldritch/src/sys/exec_impl.rs
@@ -17,12 +17,12 @@ use {
 };
 
 pub fn exec(
-    starlark_heap: &Heap,
+    starlark_heap: &'_ Heap,
     path: String,
     args: Vec<String>,
     disown: Option<bool>,
     env_vars: Option<SmallMap<String, String>>,
-) -> Result<Dict> {
+) -> Result<Dict<'_>> {
     let mut env_vars_map = HashMap::new();
     if let Some(e) = env_vars {
         for (k, v) in e {
