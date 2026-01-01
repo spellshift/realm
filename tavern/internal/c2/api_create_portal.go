e3ff7417fe055edb116fe8b47721edefd17056f3
KCarretto <Kcarretto@gmail.com>
2026-01-01 22:29:29 +0000

    minor cleanup

--- tavern/internal/c2/api_create_portal.go
+++ tavern/internal/c2/api_create_portal.go
@@ -38,7 +38,6 @@ func (srv *Server) CreatePortal(gstream c2pb.C2_CreatePortalServer) error {
 	defer cleanup()
 
 	portalInTopic := srv.portalMux.TopicIn(portalID)
-	// portalInSub := srv.portalMux.SubName(portalInTopic)
 	recv, cleanup := srv.portalMux.Subscribe(portalInTopic)
 	defer cleanup()
 
