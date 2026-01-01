8056d3d9ec022be0e7b32861b295938399fcf01f
Catherine Poggioli <catie@squidli.com>
2026-01-01 18:09:23 +0000

    fix(filter): Fix create quest filter by hostname or beacon

--- tavern/internal/www/src/components/BeaconTile.tsx
+++ tavern/internal/www/src/components/BeaconTile.tsx
@@ -1,35 +1,50 @@
+import { BugAntIcon } from "@heroicons/react/24/outline";
 import { BeaconNode, TagEdge } from "../utils/interfacesQuery";
 import { checkIfBeaconOffline } from "../utils/utils";
 import Badge from "./tavern-base-ui/badge/Badge";
 import { Globe, Network } from "lucide-react";
 
+
 type Props = {
-    beaconData: BeaconNode
+    beacon: BeaconNode;
+    isBeaconIconVisible?: boolean;
 }
 const BeaconTile = (props: Props) => {
-    const { beaconData } = props;
-    const beaconOffline = checkIfBeaconOffline(beaconData);
+    const { beacon, isBeaconIconVisible } = props;
+    const {
+        host,
+        principal,
+        name
+    } = beacon;
+    const beaconOffline = checkIfBeaconOffline(beacon);
+
     return (
-        <div className="flex flex-col gap-1">
-            <div className="flex flex-row gap-4">{beaconData.name}</div>
-            <div className="flex flex-row flex-wrap gap-1">
-                {(beaconData.principal && beaconData.principal !== "") &&
-                    <Badge>{beaconData.principal}</Badge>
-                }
-                <Badge>{beaconData?.host?.name}</Badge>
-                {beaconData?.host?.primaryIP && (
-                    <Badge leftIcon={<Network className="h-3 w-3" />}>{beaconData?.host?.primaryIP}</Badge>
-                )}
-                {beaconData?.host?.externalIP && (
-                    <Badge leftIcon={<Globe className="h-3 w-3" />}>{beaconData?.host?.externalIP}</Badge>
-                )}
-                <Badge>{beaconData?.host?.platform}</Badge>
-                {beaconData?.host?.tags && beaconData?.host?.tags?.edges.map((tag: TagEdge) => {
-                    return <Badge key={tag.node.id}>{tag.node.name}</Badge>
-                })}
-                {beaconOffline && <Badge>Offline</Badge>}
+        <div className=" flex flex-row gap-4">
+            {isBeaconIconVisible && <BugAntIcon className="h-5 w-5 mt-2" />}
+            <div className="flex flex-col gap-1 ">
+                <div className="text-gray-600">
+                    {name}@{host?.name}
+                </div>
+                <div className="flex flex-row gap-2 flex-wrap">
+                    {(principal && principal !== "") &&
+                        <Badge>{principal}</Badge>
+                    }
+                    {host?.primaryIP && (
+                        <Badge leftIcon={<Network className="h-3 w-3" />}>{host?.primaryIP}</Badge>
+                    )}
+                    {host?.externalIP && (
+                        <Badge leftIcon={<Globe className="h-3 w-3" />}>{host?.externalIP}</Badge>
+                    )}
+                    {host?.platform &&
+                        <Badge>{host?.platform}</Badge>
+                    }
+                    {host?.tags && host?.tags?.edges?.map((tag: TagEdge) => {
+                        return <Badge key={tag.node.id}>{tag.node.name}</Badge>
+                    })}
+                    {beaconOffline && <Badge>Offline</Badge>}
+                </div>
             </div>
         </div>
     );
-}
+};
 export default BeaconTile;
