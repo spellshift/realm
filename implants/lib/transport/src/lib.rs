1f59672a1e1224b3f934a5e20404909b30169a63
KCarretto <Kcarretto@gmail.com>
2026-01-03 01:24:32 +0000

    cleanup

--- implants/lib/transport/src/lib.rs
+++ implants/lib/transport/src/lib.rs
@@ -226,7 +226,7 @@ impl Transport for ActiveTransport {
         }
     }
 
-    fn get_type(&mut self) -> beacon::Transport {
+    fn get_type(&mut self) -> active_transport::Type {
         match self {
             #[cfg(feature = "grpc")]
             Self::Grpc(t) => t.get_type(),
