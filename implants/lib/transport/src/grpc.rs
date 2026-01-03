c23dc5c6fbe06e5f4a5c564f63be288836a1763c
Hulto <7121375+hulto@users.noreply.github.com>
2026-01-03 02:15:12 -0500

    initial stubbing and renaming (#1512)

--- implants/lib/transport/src/grpc.rs
+++ implants/lib/transport/src/grpc.rs
@@ -255,8 +255,8 @@ impl Transport for GRPC {
         Ok(())
     }
 
-    fn get_type(&mut self) -> pb::c2::beacon::Transport {
-        return pb::c2::beacon::Transport::Grpc;
+    fn get_type(&mut self) -> pb::c2::active_transport::Type {
+        return pb::c2::active_transport::Type::TransportGrpc;
     }
 
     fn is_active(&self) -> bool {
