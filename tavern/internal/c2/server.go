b03ed40bbd6f0bb1b49b39207d13a8e9ac3b0bb0
Hulto <7121375+hulto@users.noreply.github.com>
2026-02-07 19:15:44 -0500

    Fix JWT validation error handling and logging

--- tavern/internal/c2/server.go
+++ tavern/internal/c2/server.go
@@ -111,19 +111,14 @@ func (srv *Server) ValidateJWT(jwttoken string) error {
 	token, err := jwt.Parse(jwttoken, func(token *jwt.Token) (any, error) {
 		// 1. Verify the signing method is EdDSA
 		if _, ok := token.Method.(*jwt.SigningMethodEd25519); !ok {
-			// TODO: Uncomment with imixv1 delete
-			// return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-			slog.Warn(fmt.Sprintf("unexpected signing method: %v", token.Header["alg"]))
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
 		// 2. Return the PUBLIC key for verification
 		return srv.jwtPublicKey, nil
 	})
 
 	if err != nil || !token.Valid {
-		// TODO: Uncomment with imixv1 delete
-		// return status.Errorf(codes.PermissionDenied, "invalid token: %v", err)
-		slog.Warn(fmt.Sprintf("invalid token: %v", err))
-		return nil
+		return status.Errorf(codes.PermissionDenied, "invalid token: %v", err)
 	}
 
 	slog.Info(fmt.Sprintf("received valid JWT: %s", jwttoken))
