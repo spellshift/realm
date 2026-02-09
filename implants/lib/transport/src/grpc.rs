d71bfb87507d0c257f32d8f2b52113c33956d968
hulto <7121375+hulto@users.noreply.github.com>
2026-02-09 02:31:18 +0000

    Refactor ignore certs

--- implants/lib/transport/src/grpc.rs
+++ implants/lib/transport/src/grpc.rs
@@ -11,10 +11,7 @@ use tonic::Request;
 use crate::dns_resolver::doh::DohProvider;
 use crate::Transport;
 
-use rustls::client::danger::{HandshakeSignatureValid, ServerCertVerified, ServerCertVerifier};
-use rustls::pki_types::{CertificateDer, ServerName, UnixTime};
-use rustls::DigitallySignedStruct;
-use rustls::SignatureScheme;
+use crate::tls_utils::AcceptAllCertVerifier;
 use std::sync::Arc;
 use std::time::Duration;
 
@@ -53,54 +50,6 @@ where
         } else {
             self.inner.call(uri)
         }
-    }
-}
-
-#[derive(Debug)]
-struct AcceptAllCertVerifier;
-
-impl ServerCertVerifier for AcceptAllCertVerifier {
-    fn verify_server_cert(
-        &self,
-        _end_entity: &CertificateDer<'_>,
-        _intermediates: &[CertificateDer<'_>],
-        _server_name: &ServerName<'_>,
-        _ocsp_response: &[u8],
-        _now: UnixTime,
-    ) -> Result<ServerCertVerified, rustls::Error> {
-        Ok(ServerCertVerified::assertion())
-    }
-
-    fn verify_tls12_signature(
-        &self,
-        _message: &[u8],
-        _cert: &CertificateDer<'_>,
-        _dss: &DigitallySignedStruct,
-    ) -> Result<HandshakeSignatureValid, rustls::Error> {
-        Ok(HandshakeSignatureValid::assertion())
-    }
-
-    fn verify_tls13_signature(
-        &self,
-        _message: &[u8],
-        _cert: &CertificateDer<'_>,
-        _dss: &DigitallySignedStruct,
-    ) -> Result<HandshakeSignatureValid, rustls::Error> {
-        Ok(HandshakeSignatureValid::assertion())
-    }
-
-    fn supported_verify_schemes(&self) -> Vec<SignatureScheme> {
-        vec![
-            SignatureScheme::ECDSA_NISTP384_SHA384,
-            SignatureScheme::ECDSA_NISTP256_SHA256,
-            SignatureScheme::ED25519,
-            SignatureScheme::RSA_PSS_SHA512,
-            SignatureScheme::RSA_PSS_SHA384,
-            SignatureScheme::RSA_PSS_SHA256,
-            SignatureScheme::RSA_PKCS1_SHA512,
-            SignatureScheme::RSA_PKCS1_SHA384,
-            SignatureScheme::RSA_PKCS1_SHA256,
-        ]
     }
 }
 
