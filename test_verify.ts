263893063dbdfae7d53d5f12bd78486093f14b4b
KCarretto <Kcarretto@gmail.com>
2026-03-04 18:27:05 +0000

    small cleanup

--- test_verify.ts
+++
@@ -1,2 +0,0 @@
-import { highlightPythonSyntax } from "./tavern/internal/www/src/pages/shellv2/hooks/shellUtils";
-console.log(highlightPythonSyntax('x = f"Hello {name} {age + 1} and {{escaped}}"'));
