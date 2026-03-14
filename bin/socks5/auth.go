481479c7358076207fb4d5d10c0c0f63d46c73da
Hulto <7121375+hulto@users.noreply.github.com>
2026-03-14 00:19:40 -0400

    Update auth.go

--- bin/socks5/auth.go
+++ bin/socks5/auth.go
@@ -11,7 +11,7 @@ import (
 )
 
 // EnvAPIKey is the name of the environment variable to optionally provide an API key
-const EnvAPIKey = "TAVERN_API_KEY"
+const EnvAPIKey = "TAVERN_API_TOKEN"
 
 func getAuthToken(ctx context.Context, tavernURL, cachePath string) (auth.Token, error) {
 	return auth.Authenticate(
